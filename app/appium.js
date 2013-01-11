2d9b582bc51ac6751aa21fb4db807d3d3ad51d27
Adam Christian <adam.christian@gmail.com>
2013-01-11 00:17:28 -0800

    More re-org, adding some new commands

--- app/appium.js
+++ app/appium.js
@@ -12,6 +12,16 @@ Appium.prototype.start = function(err, cb) {
   console.log('The appium client start function has been called!');
 };
 
+Appium.prototype.stop = function(err, cb) {
+  console.log('The appium client stop function has been called!');
+};
+
+Appium.prototype.proxy = function(err, command, cb) {
+  // was thinking we should use a queue for commands instead of writing to a file
+  session.queue.push(command);
+  console.log('Pushed command to appium work queue.' + command);
+};
+
 module.exports = function(app, uuid, version) {
   return new Appium(app, uuid, version);
 }; 
--- app/routing.js
+++ app/routing.js
@@ -2,4 +2,8 @@ var controller = require('./controller');
 
 module.exports = function(app) {
   app.get('/wd/hub/status', controller.status);
+  app.post('/wd/hub/session', controller.createSession);
+  app.get('/wd/hub/session/:sessionId?', controller.getSession);
+  app.delete('/wd/hub/session/:sessionId?', controller.deleteSession);
+  app.post('/wd/hub/session/:sessionId?/execute', controller.executeScript);
 };

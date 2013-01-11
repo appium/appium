f77e2e44d989f6165a9443b61f93c87e52537397
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2013-01-10 18:54:42 -0800

    Keep conventions for function vs. ctor straight.

--- app/appium.js
+++ app/appium.js
@@ -1,10 +1,17 @@
-var appium = module.exports = function(app, uuid, verbose) {
+// Appium webserver controller methods
+// https://github.com/hugs/appium/blob/master/appium/appium.py
+
+var Appium = function(app, uuid, verbose) {
   this.app = app;
   this.uuid = uuid;
   this.verbose = verbose;
   this.instrumentsProcess = null;
 };
 
-appium.prototype.start = function(err, cb) {
+Appium.prototype.start = function(err, cb) {
   console.log('The appium client start function has been called!');
 };
+
+module.exports = function(app, uuid, version) {
+  return new Appium(app, uuid, version);
+}; 

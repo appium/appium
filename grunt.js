85d2954670b0fdb1cf7d78cc9a6e1e54255ec19b
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2013-01-13 21:08:28 -0800

    Lint changes to maintain high level of code quality.

--- grunt.js
+++ grunt.js
@@ -1,7 +1,7 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     lint: {
-      all: ['*.js', 'app/*.js', 'instruments/instruments.js', 'instruments/example.js', 'instruments/build.js']
+      all: ['*.js', 'app/*.js', 'app/test/unit/*.js', 'instruments/instruments.js', 'instruments/example.js', 'instruments/build.js']
     }
     , jshint: {
       all: {

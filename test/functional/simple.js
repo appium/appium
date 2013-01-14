844e0590964f02369501875dbeb9b438c274d4b5
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2013-01-13 18:17:09 -0800

    First simple functional test. Basically port of webdriver-test.py.

---
+++ test/functional/simple.js
@@ -0,0 +1,49 @@
+var wd = require('wd')
+  , assert = require("assert")
+  , caps = {
+      browserName: 'iOS'
+      , platform: 'Mac'
+      , version: '6.0'
+    };
+
+describe('load calc app', function() {
+
+  var values = [];
+  var populate = function(driver, cb) {
+    driver.elementsByTagName('textField', function(err, elems) {
+      var next = function(num) {
+        if (num >= elems.length) {
+          return cb(elems);
+        }
+        var val = Math.round(Math.random()*10);
+        values.push(val);
+        var elem = elems[num++];
+        elem.sendKeys(val, function() {
+          next(num);
+        });
+      };
+      next(0);
+    });
+  };
+
+  var driver = wd.remote('127.0.0.1', 8080);
+  return it('should fill two fields with numbers', function(done) {
+    driver.init(caps, function(err, sessionId) {
+      populate(driver, function(elems) {
+        driver.elementsByTagName('button', function(err, buttons) {
+          buttons[0].click(function() {
+            driver.elementsByTagName('staticText', function(err, elems) {
+              elems[0].text(function(err, text) {
+                var sum = values[0] + values[1];
+                assert.equal(parseInt(text, 10), sum);
+                driver.quit(function() {
+                  done();
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+});

7f71f0e1a3cdeb6fb71aed432325f81ad5dab634
Santiago Suarez Ordoñez <santiycr@gmail.com>
2013-01-15 15:23:59 -0800

    moving capabilities to the device, to keep responsibilities clear

--- app/appium.js
+++ app/appium.js
@@ -11,16 +11,6 @@ var Appium = function(args) {
   this.active = null;
   this.device = null;
   this.sessionId = null;
-  this.capabilities = {
-      version: '6.0'
-      , webStorageEnabled: false
-      , locationContextEnabled: false
-      , browserName: 'iOS'
-      , platform: 'MAC'
-      , javascriptEnabled: true
-      , databaseEnabled: false
-      , takesScreenshot: false
-  };
 };
 
 Appium.prototype.attachTo = function(rest, cb) {

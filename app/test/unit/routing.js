bda7b3cc80086de63b42bdd2d984e85ca63f9434
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2013-01-15 12:14:11 -0800

    Added linter options in head of file.

--- app/test/unit/routing.js
+++ app/test/unit/routing.js
@@ -1,7 +1,7 @@
+/*global describe:true, it:true */
 "use strict";
 
-var assert = require('assert')
-  , rest = require('express')()
+var rest = require('express')()
   , appium = require('../../appium');
 
 describe('Appium', function() {

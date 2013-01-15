ef51b712a81fff65bafb0047143195ac52bce1ff
Jonathan Lipps <jlipps@gmail.com>
2013-01-15 11:03:04 -0800

    rewrite of instruments to use sockets rather than HTTP

---
+++ instruments/client.js
@@ -0,0 +1,29 @@
+var net = require('net')
+  , ap = require('argparse').ArgumentParser;
+
+var main = function() {
+  var parser = new ap({
+    version: '0.0.1',
+  });
+  parser.addArgument(['-r', '--result'], {defaultValue: null, required: false});
+  parser.addArgument(['-s', '--socket'], {defaultValue: '/tmp/instruments_sock', required: false});
+  var args = parser.parseArgs();
+  var client = net.connect({path: args.socket}, function() {
+    var data = {event: "cmd"};
+    if (args.result) {
+      data['result'] = JSON.parse(args.result);
+    }
+    var data = JSON.stringify(data);
+    client.write(data);
+  });
+  client.on('data', function(data) {
+    data = JSON.parse(data);
+    process.stdout.write(data.nextCommand);
+    client.end();
+    process.exit(0);
+  });
+};
+
+if (module === require.main) {
+  main();
+}
--- instruments/example.js
+++ instruments/example.js
@@ -1,18 +1,10 @@
 var http = require('http')
-  , express = require('express')
-  , app = express()
   , path = require('path')
-  , server = http.createServer(app)
   , spawn = require('child_process').spawn
   , build = require('../build')
   , fs = require('fs')
   , instruments = require('./instruments')
   , rimraf = require('rimraf');
-
-app.configure(function() {
-  app.use(express.bodyParser()); // this is required
-  app.use(app.router);
-});
 
 var appRoot = path.resolve(__dirname, '../sample-code/apps/TestApp/');
 var simApp = path.resolve(appRoot, 'build/Release-iphonesimulator/TestApp.app');
@@ -23,35 +15,41 @@ build(appRoot, function(err) {
     process.exit(1);
   }
 
-  app.listen(4723, 'localhost', function() {
-    console.log("Instruments REST server running");
-    console.log("Launching instruments...");
-    var inst = instruments(
-      app
-      , simApp
-      , null
-      , path.resolve(__dirname, '../app/uiauto/bootstrap.js')
-      , path.resolve(__dirname, '../app/uiauto/Automation.tracetemplate')
-    );
-
-    inst.launch(function() {
-      inst.setDebug(true);
-      inst.sendCommand("mainWindow.textFields()[0].setValue('3');", function() {
-        inst.sendCommand("mainWindow.textFields()[1].setValue('5');", function() {
-          inst.sendCommand("mainWindow.buttons()[0].tap();", function() {
-            inst.sendCommand("mainWindow.staticTexts()[0].value()", function(sum) {
-              console.log("Sum should be 8 and is " + sum);
-              inst.shutdown(function(traceDir) {
-                rimraf(traceDir, function() {
-                  process.exit();
-                });
-              });
-            });
+  console.log("Launching instruments...");
+  var onLaunch = function(inst) {
+    inst.setDebug(true);
+    inst.sendCommand("mainWindow.textFields()[0].setValue('3');", function() {
+      inst.sendCommand("mainWindow.textFields()[1].setValue('5');", function() {
+        inst.sendCommand("mainWindow.buttons()[0].tap();", function() {
+          inst.sendCommand("mainWindow.staticTexts()[0].value()", function(sum) {
+            console.log("Sum should be 8 and is " + sum);
+            inst.shutdown();
           });
         });
       });
-    }, function(code) {
-      console.log("Instruments exited " + (code === 0 ? "cleanly" : "with code " + code));
     });
-  });
+  };
+
+  var onExit = function(code, traceDir) {
+    console.log("Instruments exited " + (code == 0 ? "cleanly" : "with code " + code));
+    if (traceDir) {
+      rimraf(traceDir, function() {
+        console.log("Deleted tracedir");
+        process.kill(code);
+      });
+    } else {
+      console.log("Could not delete tracedir");
+      process.kill(code);
+    }
+  };
+
+  var inst = instruments(
+    simApp
+    , null
+    , path.resolve(__dirname, 'bootstrap.js')
+    , path.resolve(__dirname, '../app/uiauto/Automation.tracetemplate')
+    , '/tmp/instruments_sock'
+    , onLaunch
+    , onExit
+  );
 });

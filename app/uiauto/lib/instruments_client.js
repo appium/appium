5d39b975c481e2f8f1d6f7c4bd851c97cad47cd8
Jonathan Lipps <jlipps@gmail.com>
2013-01-15 12:35:19 -0800

    cleaned up instruments socket client

--- app/uiauto/lib/instruments_client.js
+++ app/uiauto/lib/instruments_client.js
@@ -1,14 +1,16 @@
+"use strict";
+
 var system = UIATarget.localTarget().host();
 // clientPath is relative to where you run the appium server from
 var clientPath = 'instruments/client.js';
 
 var sendResultAndGetNext = function(result) {
-  args = [clientPath, '-s', '/tmp/instruments_sock'];
+  var args = [clientPath, '-s', '/tmp/instruments_sock'];
   if (typeof result !== "undefined") {
     args = args.concat(['-r', JSON.stringify(result)]);
   }
   var res = system.performTaskWithPathArgumentsTimeout('/usr/local/bin/node', args, 20);
-  if (res.status !== 0) {
+  if (res.exitCode !== 0) {
     console.log("Error talking with instruments client, here's stderr:");
     console.log(res.stderr);
   }
@@ -17,4 +19,4 @@ var sendResultAndGetNext = function(result) {
 
 var getFirstCommand = function() {
   return sendResultAndGetNext();
-}
+};

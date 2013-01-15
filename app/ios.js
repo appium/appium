c7ed067c8cba82b111460a2e79ca50c965d682aa
berni <berni@extensa.pl>
2013-01-15 23:49:19 +0100

    fixing the build: syntax err

--- app/ios.js
+++ app/ios.js
@@ -3,7 +3,7 @@ var path = require('path')
   , rimraf = require('rimraf')
   , UUID = require('uuid-js')
   , fs = require('fs')
-  , sock = '/tmp/instruments_sock';
+  , sock = '/tmp/instruments_sock'
   , instruments = require('../instruments/instruments');
 
 

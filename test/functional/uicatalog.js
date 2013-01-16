42c0c896492cac3c1b48df3fe5ba0dc8d10c0337
Jonathan Lipps <jlipps@gmail.com>
2013-01-15 16:50:46 -0800

    comment out that test for now, it doesn't play nice in the current system

--- test/functional/uicatalog.js
+++ test/functional/uicatalog.js
@@ -1,56 +1,56 @@
 /*global describe:true, it:true, beforeEach:true */
 "use strict";
 
-var wd = require('wd')
-  , server = require("../../server.js").run
-  , assert = require("assert")
-  , request = require("request")
-  , caps = {
-      browserName: 'iOS'
-      , platform: 'Mac'
-      , version: '6.0'
-    };
+//var wd = require('wd')
+  //, server = require("../../server.js").run
+  //, assert = require("assert")
+  //, request = require("request")
+  //, caps = {
+      //browserName: 'iOS'
+      //, platform: 'Mac'
+      //, version: '6.0'
+    //};
 
-var appium = null;
+//var appium = null;
 
-describe('Tests with server', function() {
-  this.timeout(5000);
-  beforeEach(function(done) {
-    if (appium === null) {
-      appium = server({
-        //app: 'sample-code/apps/UICatalog/build/Release-iphonesimulator/UICatalog.app'
-        app: 'sample-code/apps/TestApp/build/Release-iphonesimulator/TestApp.app'
-        , udid: null
-        , verbose: true
-        , remove: true
-        , port: 4723
-        , address: '127.0.0.1' }
-        , done
-        , function() {}
-      );
-    } else {
-      done();
-    }
-  });
+//describe('Tests with server', function() {
+  //this.timeout(5000);
+  //beforeEach(function(done) {
+    //if (appium === null) {
+      //appium = server({
+        ////app: 'sample-code/apps/UICatalog/build/Release-iphonesimulator/UICatalog.app'
+        //app: 'sample-code/apps/TestApp/build/Release-iphonesimulator/TestApp.app'
+        //, udid: null
+        //, verbose: true
+        //, remove: true
+        //, port: 4723
+        //, address: '127.0.0.1' }
+        //, done
+        //, function() {}
+      //);
+    //} else {
+      //done();
+    //}
+  //});
 
-  var driver = wd.remote('127.0.0.1', 4723);
+  //var driver = wd.remote('127.0.0.1', 4723);
 
-  it('should work', function(done) {
-    driver.init(caps, function(err, sessionId) {
-      appium.proxy('mainWindow.logElementTree()', function(err, json) {
-        console.log(json);
-        driver.quit(done);
-      });
-    });
-  });
+  //it('should work', function(done) {
+    //driver.init(caps, function(err, sessionId) {
+      //appium.proxy('mainWindow.logElementTree()', function(err, json) {
+        //console.log(json);
+        //driver.quit(done);
+      //});
+    //});
+  //});
 
-  it('should work again!', function(done) {
-    driver.init(caps, function(err, sessionId) {
-      appium.proxy('mainWindow.logElementTree()', function(err, json) {
-        console.log(json);
-        driver.quit(done);
-      });
-    });
-  });
-});
+  //it('should work again!', function(done) {
+    //driver.init(caps, function(err, sessionId) {
+      //appium.proxy('mainWindow.logElementTree()', function(err, json) {
+        //console.log(json);
+        //driver.quit(done);
+      //});
+    //});
+  //});
+//});
 

7df780112e86c4fd8dc444e00b8fd810c64c822e
Jonathan Lipps <jlipps@gmail.com>
2013-01-17 13:53:56 -0800

    ported findElement.js to use driverBlock

--- test/functional/findElement.js
+++ test/functional/findElement.js
@@ -1,38 +1,25 @@
 /*global describe:true, it:true */
 "use strict";
 
-var wd = require('wd')
-  , assert = require('assert')
-  , caps = {
-      browserName: 'iOS'
-      , platform: 'Mac'
-      , version: '6.0'
-    };
+var driverBlock = require("../helpers/driverblock.js")
+  , assert = require('assert');
 
-describe('check findElement', function() {
-  var driver = wd.remote('127.0.0.1', 4723);
-  return it('should find a single element on the app', function(done) {
-    driver.init(caps, function(err, sessionId) {
-      driver.elementByTagName('button', function(err, element) {
+driverBlock(function(h) {
+  describe('findElement', function() {
+    return it('should find a single element on the app', function(done) {
+      h.driver.elementByTagName('button', function(err, element) {
         assert.ok(element.value);
-        driver.quit(function() {
-          done();
-        });
+        done();
       });
     });
   });
-});
 
-describe('check findElements', function() {
-  var driver = wd.remote('127.0.0.1', 4723);
-  return it('should find both elements on the app', function(done) {
-    driver.init(caps, function(err, sessionId) {
-      driver.elementsByTagName('button', function(err, elements) {
+  return describe('findElements', function() {
+    return it('findElements should find both elements on the app', function(done) {
+      h.driver.elementsByTagName('button', function(err, elements) {
         assert.equal(elements.length, 2);
         assert.ok(elements[0].value);
-        driver.quit(function() {
-          done();
-        });
+        done();
       });
     });
   });

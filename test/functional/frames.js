e567350601b98865e6b1d14e1943c9093ebcb21b
Jonathan Lipps <jlipps@gmail.com>
2013-01-17 13:56:36 -0800

    ported frames.js to use driverBlock

--- test/functional/frames.js
+++ test/functional/frames.js
@@ -3,27 +3,17 @@
 /*global describe:true, it:true */
 "use strict";
 
-var wd = require('wd')
-  , should = require("should")
-  , request = require("request")
-  , caps = {
-      browserName: 'iOS'
-      , platform: 'Mac'
-      , version: '6.0'
-    };
+var driverBlock = require("../helpers/driverblock.js")
+  , should = require("should");
 
 describe('check frame command', function() {
-  var driver = wd.remote('127.0.0.1', 4723);
-  return it('should actually switch frames', function(done) {
-    driver.init(caps, function(err, sessionId) {
-      should.not.exist(err);
-      driver.frame(null, function(err) {
+  driverBlock(function(h) {
+    return it('should actually switch frames', function(done) {
+      h.driver.frame(null, function(err) {
         // TODO: make a test that proves frame switching actuall worked once we
         // move to testing with UI Catalog app
-        driver.quit(function() {
-          should.not.exist(err);
-          done();
-        });
+        should.not.exist(err);
+        done();
       });
     });
   });

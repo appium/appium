17ddb0d3e16033d6964025aea7ded0da0499803b
Jonathan Lipps <jlipps@gmail.com>
2013-01-17 14:22:11 -0800

    ported timeouts.js to use describeWd

--- test/functional/timeouts.js
+++ test/functional/timeouts.js
@@ -1,4 +1,4 @@
-/*global describe:true, it:true */
+/*global it:true */
 "use strict";
 
 /*
@@ -7,31 +7,21 @@
  * this test
  */
 
-// var wd = require('wd')
-//   , assert = require('assert')
-//   , caps = {
-//       browserName: 'iOS'
-//       , platform: 'Mac'
-//       , version: '6.0'
-//     };
-// 
-// describe('check implicit wait', function() {
-//   var driver = wd.remote('127.0.0.1', 4723);
-//   return it('should set the implicit wait for finding elements', function(done) {
-//     driver.init(caps, function(err, sessionId) {
-//       driver.setImplicitWaitTimeout(10 * 1000, function(err) {
-//         var before = new Date().getTime() / 1000;
-//         console.log(before);
-//         driver.elementsByTagName('notgonnabethere', function(err, missing) {
-//           var after = new Date().getTime() / 1000;
-//           console.log(after);
-//           assert.ok(after - before < 12);
-//           assert.ok(after - before > 10);
-//           driver.quit(function() {
-//             done();
-//           });
-//         });
-//       });
-//     });
-//   });
-// });
+//var describeWd = require('../helpers/driverblock.js').describe
+  //, assert = require('assert');
+
+ //describeWd('check implicit wait', function(h) {
+   //return it('should set the implicit wait for finding elements', function(done) {
+     //h.driver.setImplicitWaitTimeout(10 * 1000, function(err) {
+       //var before = new Date().getTime() / 1000;
+       //console.log(before);
+       //h.driver.elementsByTagName('notgonnabethere', function(err, missing) {
+         //var after = new Date().getTime() / 1000;
+         //console.log(after);
+         //assert.ok(after - before < 12);
+         //assert.ok(after - before > 10);
+         //done();
+       //});
+     //});
+   //});
+ //});

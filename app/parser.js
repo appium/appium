00b8f5b9b3e8ade2c4167ee0e26eacf00c984e2c
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2013-01-18 15:05:26 -0800

    Fixed udid parser arg.

--- app/parser.js
+++ app/parser.js
@@ -14,7 +14,7 @@ module.exports = function() {
   });
 
   parser.addArgument([ '-V', '--verbose' ], { required: false, help: 'verbose mode' });
-  parser.addArgument([ '-U', '--UDID' ]
+  parser.addArgument([ '-U', '--udid' ]
     , { required: false, help: 'unique device identifier of the SUT'
   });
 

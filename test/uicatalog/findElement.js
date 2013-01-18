62762e8b2eda49022f2ee8ac7e24f5a067c1bc18
Jonathan Lipps <jlipps@gmail.com>
2013-01-17 18:19:24 -0800

    add uicatalog tests

---
+++ test/uicatalog/findElement.js
@@ -0,0 +1,51 @@
+/*global it:true */
+"use strict";
+
+var describeWd = require("../helpers/driverblock.js").describe
+  , should = require('should');
+
+describeWd('findElementFromElement', function(h) {
+  it('should find an element within itself', function(done) {
+    h.driver.elementByTagName('tableView', function(err, element) {
+      should.exist(element.value);
+      element.elementByTagName('tableCell', function(err, label) {
+        should.exist(label.value);
+        label.text(function(err, text) {
+          text.should.equal("Buttons, Various uses of UIButton");
+          done();
+        });
+      });
+    });
+  });
+  it('should not find an element not within itself', function(done) {
+    h.driver.elementByTagName('tableView', function(err, element) {
+      should.exist(element.value);
+      element.elementByTagName('navigationBar', function(err, label) {
+        should.exist(err);
+        done();
+      });
+    });
+  });
+});
+
+
+describeWd('findElementsFromElement', function(h) {
+  it('should find some elements within itself', function(done) {
+    h.driver.elementByTagName('tableView', function(err, element) {
+      should.exist(element.value);
+      element.elementsByTagName('tableCell', function(err, els) {
+        els.length.should.equal(12);
+        done();
+      });
+    });
+  });
+  it('should not find elements not within itself', function(done) {
+    h.driver.elementByTagName('tableView', function(err, element) {
+      should.exist(element.value);
+      element.elementsByTagName('navigationBar', function(err, els) {
+        els.length.should.equal(0);
+        done();
+      });
+    });
+  });
+});

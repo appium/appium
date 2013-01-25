e09f3f092c4220dc83f9f87684eb053c9a3b1826
Jonathan Lipps <jlipps@gmail.com>
2013-01-25 10:10:46 -0800

    create better test organization, say which tests need --app and which don't

--- test/functional/testapp/frames.js
+++ test/functional/testapp/frames.js
@@ -3,7 +3,7 @@
 /*global it:true */
 "use strict";
 
-var describeWd = require("../helpers/driverblock.js").describeForApp('TestApp')
+var describeWd = require("../../helpers/driverblock.js").describeForApp('TestApp')
   , should = require("should");
 
 describeWd('check frame command', function(h) {

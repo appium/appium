82e833fd022a5e172143187d8334b2ceb232aa42
Jonathan Lipps <jlipps@gmail.com>
2013-01-29 08:20:34 -0800

    follow JSONWP in allowing not implemented methods and responding with 501

--- test/functional/appium/jsonwp.js
+++ test/functional/appium/jsonwp.js
@@ -3,9 +3,10 @@
 
 var should = require("should")
   , serverUrl = 'http://localhost:4723'
+  , serverHub = serverUrl + '/wd/hub/session'
   , request = require('request');
 
-describe('http request', function() {
+describe('JSONWP request', function() {
   describe('to a non-existent url', function() {
     it('should get 404 with text/plain body', function(done) {
       request.get(serverUrl + '/a/bad/path', function(err, res, body) {
@@ -13,6 +14,27 @@ describe('http request', function() {
         res.headers['content-type'].should.equal('text/plain');
         res.statusCode.should.equal(404);
         should.ok(body);
+        done();
+      });
+    });
+  });
+  describe('to a not-yet-implemented url', function() {
+    it('should respond with 501 Not Implemented', function(done) {
+      var url = serverHub + '/fakesessid/ime/deactivate';
+      request.post(url, function(err, res, body) {
+        should.not.exist(err);
+        res.statusCode.should.equal(501);
+        body.should.equal("Not Implemented");
+        done();
+      });
+    });
+  });
+  describe('to a variable resource that doesnt exist', function() {
+    it('should respond with a 404', function(done) {
+      var url = serverHub + '/fakesessid';
+      request.get(url, function(err, res) {
+        should.not.exist(err);
+        res.statusCode.should.equal(404);
         done();
       });
     });

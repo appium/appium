fdfdb1d72d095dc90a3df8c6033de705fd89c8ae
Jonathan Lipps <jlipps@gmail.com>
2013-02-01 16:08:35 -0800

    make grunt buildApp handle more SDKs, update readme

--- grunt-helpers.js
+++ grunt-helpers.js
@@ -165,3 +165,26 @@ module.exports.authorize = function(grunt, cb) {
     });
   });
 };
+
+module.exports.build = function(appRoot, cb, sdk) {
+  if (typeof sdk == "undefined") {
+    sdk = 'iphonesimulator6.0';
+  }
+  console.log("Building app...");
+  var args = ['-sdk', sdk];
+  var xcode = spawn('xcodebuild', args, {
+    cwd: appRoot
+  });
+  var output = '';
+  var collect = function(data) { output += data; };
+  xcode.stdout.on('data', collect);
+  xcode.stderr.on('data', collect);
+  xcode.on('exit', function(code) {
+    if (code === 0) {
+      cb(null);
+    } else {
+      console.log("Failed building app");
+      cb(output);
+    }
+  });
+};

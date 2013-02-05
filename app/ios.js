0810bdbb2d0c611678c9ff43be4d66752d392ceb
Jonathan Lipps <jlipps@gmail.com>
2013-02-04 18:03:27 -0800

    add nav to url function for web view

--- app/ios.js
+++ app/ios.js
@@ -491,10 +491,19 @@ IOS.prototype.flickElement = function(elementId, xoffset, yoffset, speed, cb) {
   this.proxy(command, cb);
 };
 
-IOS.prototype.url = function(cb) {
-  // in the future, detect whether we have a UIWebView that we can use to
-  // make sense of this command. For now, and otherwise, it's a no-op
-  cb(null, {status: status.codes.Success.code, value: ''});
+IOS.prototype.url = function(url, cb) {
+  if (this.curWindowHandle) {
+    this.remote.navToUrl(url, function() {
+      cb(null, {
+        status: status.codes.Success.code
+        , value: ''
+      });
+    });
+  } else {
+    // in the future, detect whether we have a UIWebView that we can use to
+    // make sense of this command. For now, and otherwise, it's a no-op
+    cb(null, {status: status.codes.Success.code, value: ''});
+  }
 };
 
 IOS.prototype.active = function(cb) {

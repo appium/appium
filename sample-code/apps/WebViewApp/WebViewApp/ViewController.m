fd9ea78eeae6327bfbce2d05550626f704962ea2
Jonathan Lipps <jlipps@gmail.com>
2013-02-05 01:56:28 -0800

    add webview app

---
+++ sample-code/apps/WebViewApp/WebViewApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WebViewApp
+//
+//  Created by Jonathan Lipps on 2/5/13.
+//  Copyright (c) 2013 Appium Committers. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

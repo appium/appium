d8181b0b2a22f14890763b849a9c81238c863638
Jonathan Lipps <jlipps@gmail.com>
2013-02-06 12:06:28 -0800

    move element-related functions from base into element

--- app/uiauto/appium/base.js
+++ app/uiauto/appium/base.js
@@ -1,3 +1,5 @@
+"use strict";
+
  /**
  *	Copyright 2012 Appium Committers
  *
@@ -25,184 +27,10 @@
 #import "element.js"
 #import "app.js"
 #import "binding.js"
-// TODO: rewrite this entire file using helper methods from mechanic?
-
-
-
-UIAElementNil.prototype.type = function() {
-    return "UIAElementNil";
-}
-
-UIAElement.prototype.type = function() {
-    var type = this.toString();
-    return type.substring(8, type.length - 1);
-}
-
-UIAElement.prototype.hasChildren = function() {
-    var type = this.type();
-    // NOTE: UIALink/UIAImage/UIAElement can have children
-    return !(type === "UIAStaticText" || type === "UIATextField"
-            || type === "UIASecureTextField" || type === "UIAButton"
-            || type === "UIASwitch" || type === "UIAElementNil");
-}
-
-UIAElement.prototype.matchesTagName = function(tagName) {
-    var type = this.type();
-    // i.e. "UIALink" matches "link:
-    return type.substring(3).toLowerCase() === tagName.toLowerCase();
-}
-
-UIAElement.prototype.matchesBy = function(tagName, text) {
-    if (!this.matchesTagName(tagName))
-        return false;
-    if (text === '')
-        return true;
-    var name = this.name();
-    if (name)
-        name = name.trim();
-    if (name === text)
-        return true;
-    var value = this.value();
-    if (value)
-        value = String(value).trim();
-    return value === text;
-}
-
-// Finding elements
-
-// @param by "type[/text]"
-UIAElement.prototype.findElements = function(by) {
-    var tagName;
-    var text;
-    var sep = by.indexOf('/');
-    if (sep != -1) {
-        tagName = by.substring(0, sep);
-        text = by.substring(sep + 1);
-    } else {
-        tagName = by;
-        text = '';
-    }
-
-    var foundElements = new Array();
-    var findElements = function(element, tagName, text) {
-        var children = element.elements();
-        var numChildren = children.length;
-        for ( var i = 0; i < numChildren; i++) {
-            var child = children[i];
-            if (child.matchesBy(tagName, text))
-                foundElements.push(child);
-            if (child.hasChildren()) // big optimization
-                findElements(child, tagName, text);
-        }
-    };
-    findElements(this, tagName, text);
-    return foundElements;
-};
-
-// @param by "type[/text]"
-UIAElement.prototype.findElement = function(by) {
-    var tagName
-    , text
-    , sep = by.indexOf('/');
-    if (sep != -1) {
-        tagName = by.substring(0, sep);
-        text = by.substring(sep + 1);
-    } else {
-        tagName = by;
-        text = '';
-    }
-
-    var foundElement;
-    var findElement = function(element, tagName, text) {
-        var children = element.elements();
-        var numChildren = children.length;
-        var found;
-        for (var i = 0; i < numChildren; i++) {
-            var child = children[i];
-            if (child.matchesBy(tagName, text)) {
-                return child;
-            }
-            else if (child.hasChildren()) { // big optimization
-                found = findElement(child, tagName, text);
-                if (found)
-                    return found;
-            }
-        }
-    };
-    return findElement(this, tagName, text);
-};
-
-var elements = new Array();
-var globalElementCounter = 0;
-
-UIAElement.prototype.getPageSource = function() {
-  target.pushTimeout(0);
-  var output = this.getTree();
-  target.popTimeout();
-  return JSON.stringify(output);
-}
-
-UIAElement.prototype.getTree = function() {
-  var target = UIATarget.localTarget();
-  target.pushTimeout(0);
-  var getTree = function(element) {
-    var subtree = {
-      name: element.name()
-      , type: element.type()
-      , label: element.label()
-      , value: element.value()
-      , rect: element.rect()
-      , dom: typeof element.dom === "function" ? element.dom() : null
-      , enabled: element.isEnabled() ? true : false
-      , valid: element.isValid() ? true : false
-      , visible: element.isVisible() ? true : false
-      , children: []
-    };
-    var children = element.elements();
-    var numChildren = children.length;
-    for ( var i = 0; i < numChildren; i++) {
-      var child = children[i];
-      subtree.children.push(getTree(child));
-    }
-    return subtree;
-  };
-  var tree = getTree(this);
-  target.popTimeout();
-  return tree
-};
-
-
-
-// getText
-
-UIAElement.prototype.getText = function() {
-    // TODO: tune as more cases are found
-    var text;
-    var type = this.type();
-    if (type === "UIATextField" || type === "UIASecureTextField"
-            || type === "UIATextView") {
-        // value takes precedence for text fields
-        text = this.value();
-        if (!text)
-            text = this.name();
-        if (!text)
-            text = "";
-    } else {
-        // name takes preference for others
-        // i.e. <h1>title</h1> becomes: name="title", value="1"
-        text = this.name();
-        if (!text)
-            text = this.value();
-    }
-    return {
-      status: codes.Success.code,
-      value: text
-    };
-}
 
 // Keyboard
 
-sendKeysToActiveElement = function(keys) {
+var sendKeysToActiveElement = function(keys) {
     if (hasSpecialKeys(keys)) {
         return sendKeysToActiveElementSpecial(keys);
     } else {
@@ -213,17 +41,17 @@ sendKeysToActiveElement = function(keys) {
       status: codes.Success.code,
       value: null
     };
-}
+};
 
-hasSpecialKeys = function(keys) {
+var hasSpecialKeys = function(keys) {
     var numChars = keys.length;
     for ( var i = 0; i < numChars; i++)
         if (isSpecialKey(keys.charAt(i)))
             return true;
     return false;
-}
+};
 
-sendKeysToActiveElementSpecial = function(keys) {
+var sendKeysToActiveElementSpecial = function(keys) {
     var keyboard = UIATarget.localTarget().frontMostApp().keyboard();
     var numChars = keys.length;
     for ( var i = 0; i < numChars; i++)
@@ -232,19 +60,19 @@ sendKeysToActiveElementSpecial = function(keys) {
       status: codes.Success.code,
       value: null
     };
-}
+};
 
 // handles some of the special keys in org.openqa.selenium.Keys
 
-isSpecialKey = function(k) {
+var isSpecialKey = function(k) {
     if (k === '\uE003') // DELETE
         return true;
     else if (k === '\uE006' || k === '\uE007') // RETURN ENTER
         return true;
     return false;
-}
+};
 
-typeKey = function(keyboard, k) {
+var typeKey = function(keyboard, k) {
     if (k === '\uE003') { // DELETE
       keyboard.keys()["Delete"].tap();
     } else if (k === '\uE006' || k === '\uE007') {// RETURN ENTER
@@ -252,36 +80,11 @@ typeKey = function(keyboard, k) {
     } else {
       keyboard.typeString(String(k)); // regular key
     }
-}
-
-// location/size/...
-
-UIAElement.prototype.getElementLocation = function() {
-    return {
-      status: codes.Success.code,
-      value: this.rect().origin
-    };
-}
-
-UIAElement.prototype.getElementSize = function() {
-    return {
-      status: codes.Success.code,
-      value: this.rect().size
-    };
-}
-
-UIAElement.prototype.isDisplayed = function() {
-  return {
-    status: codes.Success.code,
-    value: this.isVisible() == 1
-  };
-}
-
-// touch
+};
 
 // does a flick in the middle of the screen of size 1/4 of screen
 // using the direction corresponding to xSpeed/ySpeed
-touchFlickFromSpeed = function(xSpeed, ySpeed) {
+var touchFlickFromSpeed = function(xSpeed, ySpeed) {
     // get x, y of vector that provides the direction given by xSpeed/ySpeed and
     // has length .25
     var mult = Math.sqrt((0.25 * 0.25) / (xSpeed * xSpeed + ySpeed * ySpeed));
@@ -306,12 +109,12 @@ touchFlickFromSpeed = function(xSpeed, ySpeed) {
       status: codes.Success.code,
       value: null
     };
-}
+};
 
 // similar to flick but does a longer movement in the direction of the swipe
 // does a swipe in the middle of the screen of size 1/2 of screen
 // using the direction corresponding to xSpeed/ySpeed
-touchSwipeFromSpeed = function(xSpeed, ySpeed) {
+var touchSwipeFromSpeed = function(xSpeed, ySpeed) {
     // get x, y of vector that provides the direction given by xSpeed/ySpeed and
     // has length .50
     var mult = Math.sqrt((0.5 * 0.5) / (xSpeed * xSpeed + ySpeed * ySpeed));
@@ -337,27 +140,7 @@ touchSwipeFromSpeed = function(xSpeed, ySpeed) {
       status: codes.Success.code,
       value: null
     };
-}
-
-// does a flick from a center of a specified element (use case: sliders)
-UIAElement.prototype.touchFlick = function(xoffset, yoffset) {
-  var options = {
-        startOffset : {
-            x : 0.5,
-            y : 0.5
-        },
-        endOffset : {
-            x : 0.5 + xoffset,
-            y : 0.5 + yoffset
-        }
-    };
-
-  this.flickInsideWithOptions(options);
-  return {
-    status: codes.Success.code,
-    value: null
-  };
-}
+};
 
 // alerts
 
@@ -366,7 +149,7 @@ var getAlertText = function() {
     status: codes.Success.code,
     value: target.frontMostApp().alert().name()
   };
-}
+};
 
 var acceptAlert = function() {
   target.frontMostApp().alert().defaultButton().tap()
@@ -374,7 +157,7 @@ var acceptAlert = function() {
     status: codes.Success.code,
     value: null
   };
-}
+};
 
 var dismissAlert = function() {
   target.frontMostApp().alert().cancelButton().tap()
@@ -382,4 +165,4 @@ var dismissAlert = function() {
     status: codes.Success.code,
     value: null
   };
-}
+};
--- app/uiauto/appium/element.js
+++ app/uiauto/appium/element.js
@@ -1,46 +1,156 @@
-/*global au:true $:true codes:true */
+/*global au:true $:true codes:true UIAElementNil:true UIATarget:true */
 "use strict";
 
+UIAElementNil.prototype.type = function() {
+    return "UIAElementNil";
+};
+
 $.extend($.fn, {
-  getActiveElement: function() {
-    var foundElement = null;
-    var checkAll = function(element) {
-      var children = $(element).children();
-      children.each(function(e, child) {
-        var focused = $(child).isFocused();
-        if(focused === true || focused === 1) {
-          return child;
-        }
-        if (child.hasChildren()) { // big optimization
-          checkAll(child);
-        }
-      });
+    type: function() {
+      var type = this.toString();
+      return type.substring(8, type.length - 1);
+    }
 
-      return null;
-    };
-    // try au.cache in the array first
-    for (var key in au.cache) {
-      var elemFocused = $(au.cache[key]).isFocused();
-      if (elemFocused === true || elemFocused === 1) {
-        return {
-          status: codes.Success.code,
-          value: {ELEMENT: key}
-        };
+  , getActiveElement: function() {
+      var foundElement = null;
+      var checkAll = function(element) {
+        var children = $(element).children();
+        children.each(function(e, child) {
+          var focused = $(child).isFocused();
+          if(focused === true || focused === 1) {
+            return child;
+          }
+          if (child.hasChildren()) { // big optimization
+            checkAll(child);
+          }
+        });
+
+        return null;
+      };
+      // try au.cache in the array first
+      for (var key in au.cache) {
+        var elemFocused = $(au.cache[key]).isFocused();
+        if (elemFocused === true || elemFocused === 1) {
+          return {
+            status: codes.Success.code,
+            value: {ELEMENT: key}
+          };
+        }
       }
-    }
-    foundElement = checkAll(this);
+      foundElement = checkAll(this);
 
-    if (foundElement) {
-        var varName = $(foundElement).name();
-        return {
-          status: codes.Success.code,
-          value: {ELEMENT: varName}
+      if (foundElement) {
+          var varName = $(foundElement).name();
+          return {
+            status: codes.Success.code,
+            value: {ELEMENT: varName}
+          };
+      }
+
+      return {
+        status: codes.NoSuchElement.code,
+        value: null,
+      };
+    }
+
+  , hasChildren: function() {
+      var type = this.type();
+      // NOTE: UIALink/UIAImage/UIAElement can have children
+      return !(type === "UIAStaticText" || type === "UIATextField" ||
+               type === "UIASecureTextField" || type === "UIAButton" ||
+               type === "UIASwitch" || type === "UIAElementNil");
+    }
+
+  , matchesTagName: function(tagName) {
+      var type = this.type();
+      // i.e. "UIALink" matches "link:
+      return type.substring(3).toLowerCase() === tagName.toLowerCase();
+    }
+
+
+  , matchesBy: function(tagName, text) {
+      if (!this.matchesTagName(tagName))
+        return false;
+      if (text === '')
+        return true;
+      var name = this.name();
+      if (name)
+        name = name.trim();
+      if (name === text)
+        return true;
+      var value = this.value();
+      if (value)
+        value = String(value).trim();
+      return value === text;
+    }
+
+  , getTree: function() {
+      var target = UIATarget.localTarget();
+      target.pushTimeout(0);
+      var getTree = function(element) {
+        var subtree = {
+          name: element.name()
+          , type: element.type()
+          , label: element.label()
+          , value: element.value()
+          , rect: element.rect()
+          , dom: typeof element.dom === "function" ? element.dom() : null
+          , enabled: element.isEnabled() ? true : false
+          , valid: element.isValid() ? true : false
+          , visible: element.isVisible() ? true : false
+          , children: []
         };
+        var children = element.elements();
+        var numChildren = children.length;
+        for ( var i = 0; i < numChildren; i++) {
+          var child = children[i];
+          subtree.children.push(getTree(child));
+        }
+        return subtree;
+      };
+      var tree = getTree(this);
+      target.popTimeout();
+      return tree;
     }
 
-    return {
-      status: codes.NoSuchElement.code,
-      value: null,
-    };
-  }
+  , getElementLocation: function() {
+      return {
+        status: codes.Success.code,
+        value: this.rect().origin
+      };
+    }
+
+  , getElementSize: function() {
+      return {
+        status: codes.Success.code,
+        value: this.rect().size
+      };
+    }
+
+  , isDisplayed: function() {
+      return {
+        status: codes.Success.code,
+        value: this.isVisible() == 1
+      };
+    }
+
+    // does a flick from a center of a specified element (use case: sliders)
+  , touchFlick: function(xoffset, yoffset) {
+      var options = {
+            startOffset : {
+                x : 0.5,
+                y : 0.5
+            },
+            endOffset : {
+                x : 0.5 + xoffset,
+                y : 0.5 + yoffset
+            }
+        };
+
+      this.flickInsideWithOptions(options);
+      return {
+        status: codes.Success.code,
+        value: null
+      };
+    }
 });

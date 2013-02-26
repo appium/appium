b33f1985a3ab0f15e82f39e7932c3baae64d1760
Jonathan Lipps <jlipps@gmail.com>
2013-02-25 16:52:34 -0800

    add skeleton for android bootstrap

---
+++ uiautomator/bootstrap/src/io/appium/android/bootstrap/Bootstrap.java
@@ -0,0 +1,15 @@
+package io.appium.android.bootstrap;
+
+import com.android.uiautomator.core.UiObject;
+import com.android.uiautomator.core.UiObjectNotFoundException;
+import com.android.uiautomator.core.UiScrollable;
+import com.android.uiautomator.core.UiSelector;
+import com.android.uiautomator.testrunner.UiAutomatorTestCase;
+
+public class Bootstrap extends UiAutomatorTestCase {
+
+	public void testRunServer() {
+		assertTrue(true);
+	}
+
+}

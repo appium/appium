02639c310caaa2e210541c070ee08eb5e5ddaa4a
Jonathan Lipps <jlipps@gmail.com>
2013-02-27 10:02:59 -0800

    uninstall/install app and go to home screen

--- uiautomator/example.js
+++ uiautomator/example.js
@@ -4,30 +4,38 @@ var makeAdb = require('./adb');
 var adbOpts = {
   sdkRoot: process.env.ANDROID_HOME
   , avd: "jlipps1"
+  , appPackage: "com.example.android.apis"
+  , appActivity: "ApiDemos"
+  , apkPath: "/Users/jlipps/Code/eclipse/ApiDemos/bin/ApiDemos-debug.apk"
 };
-var apkPath = "/Users/jlipps/Code/eclipse/ApiDemos/bin/ApiDemos-debug.apk";
 
 
 makeAdb(adbOpts, function(err, adb) {
   adb.getConnectedDevices(function(err, devices) {
     if (devices.length) {
-      adb.waitForDevice(function(err) {
-        adb.pushAppium(function(err) {
-          adb.forwardPort(function(err) {
-            adb.installApp(apkPath, function(err) {
-              adb.startApp("com.example.android.apis", "ApiDemos", function(err) {
-                var onReady = function() {
-                  console.log("Ready for new commands");
-                  adb.sendCommand(function(data) {
-                    console.log("Got the data all the way from android");
-                    console.log(data);
-                  });
-                };
-                var onExit = function() {
-                  console.log("Process exited");
-                };
-                adb.runBootstrap(onReady, onExit);
+      adb.waitForDevice(function() {
+        adb.pushAppium(function() {
+          adb.forwardPort(function() {
+            adb.uninstallApp(function() {
+              adb.installApp(function() {
+                adb.startApp(function() {
+                  var onReady = function() {
+                    console.log("Ready for new commands");
+                    adb.sendShutdownCommand(function(data) {
+                      console.log("Got the data all the way from android");
+                      console.log(data);
+                    });
+                  };
+                  var onExit = function() {
+                    console.log("Process exited");
+                    adb.goToHome(function() {});
+                  };
+                  var go = function() {
+                    adb.runBootstrap(onReady, onExit);
+                  };
+                  setTimeout(go, 3000);
 
+                });
               });
             });
           });

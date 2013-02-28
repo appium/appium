72e8a05fb05dcfd1ede9599b9812006ecc718c31
Jonathan Lipps <jlipps@gmail.com>
2013-02-28 10:33:57 -0800

    break out element tests

---
+++ test/functional/apidemos/findElement.js
@@ -0,0 +1,21 @@
+/*global it:true */
+"use strict";
+
+var path = require('path')
+  , appPath = path.resolve(__dirname, "../../../sample-code/apps/ApiDemos/bin/ApiDemos-debug.apk")
+  , appPkg = "com.example.android.apis"
+  , appAct = "ApiDemos"
+  , describeWd = require("../../helpers/driverblock.js").describeForApp(appPath,
+      "android", appPkg, appAct)
+  , should = require('should');
+
+describeWd('app', function(h) {
+  it('should find a single element by tag name', function(done) {
+    h.driver.elementByTagName("text", function(err, el) {
+      should.not.exist(err);
+      should.exist(el);
+      done();
+    });
+  });
+});
+

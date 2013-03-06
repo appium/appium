37213a04b67dd7edcfc24b8c64367da818e59d28
bootstraponline <code@bootstraponline.com>
2013-03-06 18:35:10 -0500

    Add Android to reset

--- reset.sh
+++ reset.sh
@@ -3,6 +3,9 @@ echo "Clearing dev version of WD"
 rm -rf node_modules/wd
 echo "Installing WD and new NPM modules"
 npm install .
+echo "Building Android bootstrap"
+grunt configAndroidBootstrap
+grunt buildAndroidBootstrap
 echo "Rebuilding test apps"
 grunt buildApp:TestApp
 grunt buildApp:UICatalog

c1de952540328d532f9f77f2c439492729453001
Jonathan Lipps <jlipps@gmail.com>
2013-03-07 12:13:37 -0800

    add android app rebuilding and uicatalog downloading to reset.sh

--- reset.sh
+++ reset.sh
@@ -6,10 +6,16 @@ npm install .
 echo "Building Android bootstrap"
 grunt configAndroidBootstrap
 grunt buildAndroidBootstrap
+if [ ! -d "./sample-code/apps/UICatalog" ]; then
+    echo "Downloading UICatalog app"
+    grunt downloadApp
+fi
 echo "Rebuilding test apps"
 grunt buildApp:TestApp
 grunt buildApp:UICatalog
 grunt buildApp:WebViewApp
+grunt configAndroidApp:ApiDemos
+grunt buildAndroidApp:ApiDemos
 echo "Cleaning temp files"
 rm -rf /tmp/instruments_sock
 rm -rf *.trace

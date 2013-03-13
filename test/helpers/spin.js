bcc50adeb448356675d32a34e28a49bfd7b2ba96
Jonathan Lipps <jlipps@gmail.com>
2013-03-12 19:57:13 -0700

    stub out spin helpers

---
+++ test/helpers/spin.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var waitForCondition = require("../../app/device").waitForCondition;
+
+exports.spin = waitForCondition;
+
+exports.spinTillResEquals = function() {};
+
+exports.spinTillNoError = function() {};
+
+exports.spinTillError = function() {};

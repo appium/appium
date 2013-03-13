1bcbb78fd4374e97b5d0a4fe8e81053abd874cc7
Jonathan Lipps <jlipps@gmail.com>
2013-03-12 20:32:09 -0700

    use spinWait for this test that now goes faster

--- test/helpers/spin.js
+++ test/helpers/spin.js
@@ -1,8 +1,27 @@
 "use strict";
 
-var waitForCondition = require("../../app/device").waitForCondition;
-
-exports.spin = waitForCondition;
+exports.spinWait = function(spinFn, cb, waitMs, intMs) {
+  if (typeof waitMs === "undefined") {
+    waitMs = 10000;
+  }
+  if (typeof intMs === "undefined") {
+    intMs = 500;
+  }
+  var begunAt = Date.now();
+  var endAt = begunAt + waitMs;
+  var spin = function() {
+    spinFn(function(err) {
+      if (err && Date.now() < endAt) {
+        setTimeout(spin, intMs);
+      } else if (err) {
+        throw err;
+      } else {
+        cb.apply(null, Array.prototype.slice.call(arguments));
+      }
+    });
+  };
+  spin();
+};
 
 exports.spinTillResEquals = function() {};
 

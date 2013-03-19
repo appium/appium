dfaa53874d50e161597c6ae9b6987debf00f4b1c
Jason Carr <jason.s.carr@gmail.com>
2013-03-19 15:42:23 -0700

    Support and test for submit() method in webviews.

--- app/routing.js
+++ app/routing.js
@@ -58,6 +58,7 @@ module.exports = function(appium) {
   rest.get('/wd/hub/session/:sessionId?/window/:windowhandle?/size', controller.getWindowSize);
   rest.post('/wd/hub/session/:sessionId?/execute', controller.execute);
   rest.get('/wd/hub/session/:sessionId?/title', controller.title);
+  rest.post('/wd/hub/session/:sessionId?/element/:elementId?/submit', controller.submit);
 
   // these are for testing purposes only
   rest.post('/wd/hub/produce_error', controller.produceError);
@@ -104,13 +105,12 @@ var routeNotYetImplemented = function(rest) {
   rest.post('/wd/hub/session/:sessionId?/cookie', controller.notYetImplemented);
   rest.delete('/wd/hub/session/:sessionId?/cookie', controller.notYetImplemented);
   rest.delete('/wd/hub/session/:sessionId?/cookie/:name', controller.notYetImplemented);
-  rest.get('/wd/hub/session/:sessionId?/element/:elementid?', controller.notYetImplemented);
-  rest.post('/wd/hub/session/:sessionId?/element/:elementid?/submit', controller.notYetImplemented);
-  rest.get('/wd/hub/session/:sessionId?/element/:elementid?/name', controller.notYetImplemented);
-  rest.get('/wd/hub/session/:sessionId?/element/:elementid?/selected', controller.notYetImplemented);
-  rest.get('/wd/hub/session/:sessionId?/element/:elementid?/equals/:other', controller.notYetImplemented);
-  rest.get('/wd/hub/session/:sessionId?/element/:elementid?/location_in_view', controller.notYetImplemented);
-  rest.get('/wd/hub/session/:sessionId?/element/:elementid?/css/:propertyname', controller.notYetImplemented);
+  rest.get('/wd/hub/session/:sessionId?/element/:elementId?', controller.notYetImplemented);
+  rest.get('/wd/hub/session/:sessionId?/element/:elementId?/name', controller.notYetImplemented);
+  rest.get('/wd/hub/session/:sessionId?/element/:elementId?/selected', controller.notYetImplemented);
+  rest.get('/wd/hub/session/:sessionId?/element/:elementId?/equals/:other', controller.notYetImplemented);
+  rest.get('/wd/hub/session/:sessionId?/element/:elementId?/location_in_view', controller.notYetImplemented);
+  rest.get('/wd/hub/session/:sessionId?/element/:elementId?/css/:propertyname', controller.notYetImplemented);
   rest.post('/wd/hub/session/:sessionId?/moveto', controller.notYetImplemented);
   rest.post('/wd/hub/session/:sessionId?/click', controller.notYetImplemented);
   rest.post('/wd/hub/session/:sessionId?/buttondown', controller.notYetImplemented);

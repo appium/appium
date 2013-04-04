0683f51914c4b73491deb0442718f4dafe1171b7
Jonathan Lipps <jlipps@gmail.com>
2013-04-04 10:47:12 -0700

    it's in git history forever. no need to keep it commented

--- test/functional/safari/screenshot.js
+++ test/functional/safari/screenshot.js
@@ -3,13 +3,7 @@
 
 var desc = require("../../helpers/driverblock.js").describeForSafari()
   , _ = require("underscore")
-  //, fs = require("fs")
   , should = require('should');
-
-//var writeImg = function(imgPath, b64Data, cb) {
-  //var data = new Buffer(b64Data, 'base64').toString('binary');
-  //fs.writeFile(imgPath, data, "binary", cb);
-//};
 
 _.each(["iPhone", "iPad"], function(device) {
   desc('screenshots (' + device + ')', function(h) {
@@ -24,19 +18,15 @@ _.each(["iPhone", "iPad"], function(device) {
       h.driver.takeScreenshot(function(err, screenshot){
         should.not.exist(err);
         should.exist(screenshot);
-        //writeImg("/tmp/screen1.png", screenshot, function() {
-          h.driver.setOrientation("LANDSCAPE", function(err) {
+        h.driver.setOrientation("LANDSCAPE", function(err) {
+          should.not.exist(err);
+          h.driver.takeScreenshot(function(err, screenshot2) {
             should.not.exist(err);
-            h.driver.takeScreenshot(function(err, screenshot2) {
-              should.not.exist(err);
-              should.exist(screenshot2);
-              screenshot2.should.not.eql(screenshot);
-              //writeImg("/tmp/screen2.png", screenshot2, function() {
-                done();
-              //});
-            });
+            should.exist(screenshot2);
+            screenshot2.should.not.eql(screenshot);
+            done();
           });
-        //});
+        });
       });
     });
   }, null, null, {device: device + ' Simulator'});

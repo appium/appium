eee179f7c1d4bd0d36fb35f3a51b5463deef5fa6
Jonathan Lipps <jlipps@gmail.com>
2013-04-08 18:58:51 -0700

    minor js fixes

--- test/functional/prefs/prefs.js
+++ test/functional/prefs/prefs.js
@@ -22,26 +22,27 @@ describe('preferences app', function() {
       // have modified the plist of your preferences.app to run iphone
       // instead of ipad by default (this test is for iphone)
       it('should turn off autocomplete', function(done) {
-        h.driver.elementsByTagName('tableCell', function(err, els) {
-          els[1].click(function(err) {
-            h.driver.elementsByTagName('tableCell', function(err, els) {
-              els[1].click(function(err) {
-                h.driver.elementByXPath('//switch[@name="Auto-Correction"]', function(err, switchEl) {
-                  switchEl.getValue(function(err, checked) {
-                    if (checked === 1) {
-                      // was checked, click it off
-                      switchEl.click(function(err) {
-                        done();
-                      });
-                    } else {
-                      // was unchecked, do nothing
+        var p = {strategy: "tag name", selector: "tableCell", index: 1};
+        h.driver.execute("mobile: findAndAct", [p], function(err) {
+          should.not.exist(err);
+          setTimeout(function() {
+            h.driver.execute("mobile: findAndAct", [p], function(err) {
+              should.not.exist(err);
+              h.driver.elementByXPath('//switch[@name="Auto-Correction"]', function(err, switchEl) {
+                switchEl.getValue(function(err, checked) {
+                  if (checked === 1) {
+                    // was checked, click it off
+                    switchEl.click(function(err) {
                       done();
-                    }
-                  });
+                    });
+                  } else {
+                    // was unchecked, do nothing
+                    done();
+                  }
                 });
               });
             });
-          });
+          }, 1000);
         });
       });
     });

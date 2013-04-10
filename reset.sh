85caf8a4aff7b2a6ab0541dc3e19480d31be9634
Jonathan Lipps <jlipps@gmail.com>
2013-04-10 14:22:22 -0700

    rewrite reset.sh so it can be run with --android or --ios flags Fix #423

--- reset.sh
+++ reset.sh
@@ -1,45 +1,95 @@
-echo "Resetting / Initializing Appium"
-echo "Clearing dev version of WD"
-set +e  # we expect next command might fail without sudo privs
-rm -rf node_modules/wd
-rm_status=$?
-set -e  # turn error checking back on so we can exit if sudo branch doesn't work
-if [ $rm_status -gt 0 ]; then
-    echo "rm failed. Trying again with sudo."
-    sudo rm -rf node_modules/wd
-fi
-echo "Installing WD and new NPM modules"
-set +e
-npm install .
-install_status=$?
+#!/bin/bash
+#
+#   reset.sh: INSTALL OR RESET APPIUM
+#   This script should ensure that after pulling the most recent code,
+#   you will be in a state where you can run tests and use appium
+#
 set -e
-if [ $install_status -gt 0 ]; then
-    echo "install failed. Trying again with sudo."
-    sudo npm install .
+should_reset_android=0
+should_reset_ios=0
+
+while test $# != 0
+do
+    echo $1
+    case "$1" in
+        "--android") should_reset_android=1;;
+        "--ios") should_reset_ios=1;;
+    esac
+    shift
+done
+
+if [[ $should_reset_android -eq 0 ]] && [[ $should_reset_ios -eq 0 ]]; then
+    should_reset_android=1
+    should_reset_ios=1
 fi
-echo "Downloading/updating instruments-without-delay"
-git submodule update --init submodules/instruments-without-delay
-echo "Building instruments-without-delay"
-pushd submodules/instruments-without-delay
-./build.sh
-popd
-echo "Downloading/updating AndroidApiDemos"
-git submodule update --init submodules/ApiDemos
-rm -rf sample-code/apps/ApiDemos
-ln -s $(pwd)/submodules/ApiDemos $(pwd)/sample-code/apps/ApiDemos
-echo "Building Android bootstrap"
-grunt configAndroidBootstrap
-grunt buildAndroidBootstrap
-if [ ! -d "./sample-code/apps/UICatalog" ]; then
-    echo "Downloading UICatalog app"
-    grunt downloadApp
+
+reset_general() {
+    echo "---- RESETTING NPM ----"
+    echo "Clearing dev version of wd.js"
+    set +e  # we expect next command might fail without sudo privs
+    rm -rf node_modules/wd
+    rm_status=$?
+    set -e  # turn error checking back on so we can exit if sudo branch doesn't work
+    if [ $rm_status -gt 0 ]; then
+        echo "rm failed. Trying again with sudo."
+        sudo rm -rf node_modules/wd
+    fi
+    echo "Installing wd.js from master and new or updated NPM modules"
+    set +e
+    npm install .
+    install_status=$?
+    set -e
+    if [ $install_status -gt 0 ]; then
+        echo "install failed. Trying again with sudo."
+        sudo npm install .
+    fi
+}
+
+reset_ios() {
+    echo "---- RESETTING IOS ----"
+    echo "Downloading/updating instruments-without-delay"
+    git submodule update --init submodules/instruments-without-delay
+    echo "Building instruments-without-delay"
+    pushd submodules/instruments-without-delay
+    ./build.sh
+    popd
+    if [ ! -d "./sample-code/apps/UICatalog" ]; then
+        echo "Downloading UICatalog app"
+        grunt downloadApp
+    fi
+    echo "Rebuilding iOS test apps"
+    grunt buildApp:TestApp
+    grunt buildApp:UICatalog
+    grunt buildApp:WebViewApp
+}
+
+reset_android() {
+    echo "---- RESETTING ANDROID ----"
+    echo "Downloading/updating AndroidApiDemos"
+    git submodule update --init submodules/ApiDemos
+    rm -rf sample-code/apps/ApiDemos
+    ln -s $(pwd)/submodules/ApiDemos $(pwd)/sample-code/apps/ApiDemos
+    echo "Building Android bootstrap"
+    grunt configAndroidBootstrap
+    grunt buildAndroidBootstrap
+    echo "Configuring and rebuilding Android test apps"
+    grunt configAndroidApp:ApiDemos
+    grunt buildAndroidApp:ApiDemos
+}
+
+cleanup() {
+    echo "---- CLEANING UP ----"
+    echo "Cleaning temp files"
+    rm -rf /tmp/instruments_sock
+    rm -rf *.trace
+}
+
+echo "Resetting / Initializing Appium"
+reset_general
+if [ $should_reset_ios -eq 1 ]; then
+    reset_ios
 fi
-echo "Rebuilding test apps"
-grunt buildApp:TestApp
-grunt buildApp:UICatalog
-grunt buildApp:WebViewApp
-grunt configAndroidApp:ApiDemos
-grunt buildAndroidApp:ApiDemos
-echo "Cleaning temp files"
-rm -rf /tmp/instruments_sock
-rm -rf *.trace
+if [ $should_reset_android -eq 1 ]; then
+    reset_android
+fi
+cleanup

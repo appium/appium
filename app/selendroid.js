f2da217f183aa16a7fde360a45a94a84c0a868bf
Jonathan Lipps <jlipps@gmail.com>
2013-04-10 18:22:23 -0700

    stub out selendroid.js

---
+++ app/selendroid.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var errors = require('./errors')
+  , adb = require('../uiautomator/adb')
+  , _ = require('underscore')
+  , logger = require('../logger').get('appium')
+  , status = require("./uiauto/lib/status")
+  , exec = require('child_process').exec
+  , fs = require('fs')
+  , async = require('async')
+  , path = require('path')
+  , UnknownError = errors.UnknownError;
+
+var Selendroid = function(opts) {
+  this.apkPath = opts.apkPath;
+  this.udid = opts.udid;
+  this.appPackage = opts.appPackage;
+  this.appActivity = opts.appActivity;
+  this.verbose = opts.verbose;
+  this.onStop = function() {};
+  this.adb = null;
+  this.isProxy = true;
+};
+
+Selendroid.prototype.getProxiedSession = function(cb) {
+};
+
+module.exports = function(opts) {
+  return new Selendroid(opts);
+};
+

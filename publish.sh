e2954460477d5fdd0c957ef737a524affb1c5db4
Jonathan Lipps <jlipps@gmail.com>
2013-05-08 11:42:43 -0700

    ensure working directory is clean before publishing

--- publish.sh
+++ publish.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
+git status | grep "nothing to commit (working directory clean)" >/dev/null
+if [ $? -gt 0 ]; then
+    echo "Working directory isn't clean, commit/clean then publish"
+    exit 1
+fi
 version=$(cat package.json | underscore extract version | sed 's/\"//g')
+rm -rf build/selendroid/selendroid.mod.apk
 npm publish
 git tag -a "v$version" -m "tag appium@$version for npm publish"
 git push --tags upstream master

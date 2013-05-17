9bb37c441482cf24c0f208355069df026822b987
Swapna Chennadi <Swapna.Chennadi@Gamesys.co.uk>
2013-05-17 12:40:21 +0100

    Fixed travis errors Updated code based on review comments Used 'request' instead of 'http'

--- grid_register.js
+++ grid_register.js
@@ -1,60 +1,49 @@
-var http = require('http')
+"use strict";
+var request = require('request')
   , fs = require('fs')
   , logger = require('./logger').get('appium');
 
 exports.registerNode = function (configFile) {
   fs.readFile(configFile, 'utf-8', function (err, data, cb) {
     if (err) {
-      logger.error("Unable to load node configuration file to register with grid");
-      cb("Unable to load node configuration file to register with grid");
-      code = code || 1;
+    logger.error("Unable to load node configuration file to register with grid");
+    cb("Unable to load node configuration file to register with grid");
+    code = code || 1;
     }
     // Check presence of data before posting  it to the selenium grid
     if (data) {
       postRequest(data);
-    }
-    else {
+    } else {
       logger.error("No data found in the node configuration  file to send to the grid");
       cb("No data found in the node configuration  file to send to the grid");
       code = code || 1;
     }
   });
-}
+};
 
 function postRequest(data) {
   //parse json to get hub host and port
   var jsonObject = JSON.parse(data);
-  hubHost = jsonObject.configuration.hubHost;
   // prepare the header
-  var postheaders = {
-    'Content-Type': 'application/json',
-    'Content-Length': data.length
+  var post_headers = {
+    'Content-Type'  : 'application/json'
+    , 'Content-Length': data.length
   };
   // the post options
-  var optionspost = {
-    host: jsonObject.configuration.hubHost,
-    port: jsonObject.configuration.hubPort,
-    path: '/grid/register',
-    method: 'POST',
-    headers: postheaders
+  var options_post = {
+    url       : 'https://'+jsonObject.configuration.hubHost+':'+jsonObject.configuration.hubPort+'/grid/register'
+    , method  : 'POST'
+    , body    : data
+    , headers : post_headers
   };
   // make the http POST to the grid for registration
-  var reqPost = http.request(optionspost, function (res) {
-    if (res.statusCode == 200) {
+  var reqPost = request(options_post, function (error, response, body) {
+    if (error !== null || response.statusCode !== 200) {
+      logger.error("Request to register with grid was Unsuccessful...");
+    } else {
       var logMessage = "Appium successfully registered with the grid on " + jsonObject.configuration.hubHost + ":" + jsonObject.configuration.hubPort;
       logger.info(logMessage.cyan);
-    } else {
-      logger.err("Request to register with grid was Unsuccessful...");
     }
-    res.setEncoding('utf-8');
-    res.on('data', function (d) {
-    });
-  });
-  // write the json data  to the request
-  reqPost.write(data);
-  reqPost.end();
-  reqPost.on('error', function (e) {
-  console.error(e);
   });
 }
 

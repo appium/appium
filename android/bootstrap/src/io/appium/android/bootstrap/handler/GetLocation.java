c8d74fca5725de2d46fcd14f747ae17e6bdba072
Jonathan Lipps <jlipps@gmail.com>
2013-05-30 08:34:34 -0700

    make exception check broader

--- android/bootstrap/src/io/appium/android/bootstrap/handler/GetLocation.java
+++ android/bootstrap/src/io/appium/android/bootstrap/handler/GetLocation.java
@@ -5,14 +5,11 @@ import io.appium.android.bootstrap.AndroidCommandResult;
 import io.appium.android.bootstrap.AndroidElement;
 import io.appium.android.bootstrap.CommandHandler;
 import io.appium.android.bootstrap.WDStatus;
-import io.appium.android.bootstrap.exceptions.ElementNotInHashException;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.graphics.Rect;
-
-import com.android.uiautomator.core.UiObjectNotFoundException;
 
 /**
  * This handler is used to get the text of elements that support it.
@@ -44,9 +41,7 @@ public class GetLocation extends CommandHandler {
       res.put("x", bounds.left);
       res.put("y", bounds.top);
       return getSuccessResult(res);
-    } catch (final UiObjectNotFoundException e) {
-      return new AndroidCommandResult(WDStatus.NO_SUCH_ELEMENT, e.getMessage());
-    } catch (final ElementNotInHashException e) {
+    } catch (final Exception e) {
       return new AndroidCommandResult(WDStatus.NO_SUCH_ELEMENT, e.getMessage());
     }
   }

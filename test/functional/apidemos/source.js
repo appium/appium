2b66b8c0fbd5733c34ead7fea9bb98a7363d586d
Jonathan Lipps <jlipps@gmail.com>
2013-06-04 12:49:26 -0700

    add basic test for android page source

---
+++ test/functional/apidemos/source.js
@@ -0,0 +1,23 @@
+/*global it:true */
+"use strict";
+
+var path = require('path')
+  , appPath = path.resolve(__dirname, "../../../sample-code/apps/ApiDemos/bin/ApiDemos-debug.apk")
+  , appPkg = "com.example.android.apis"
+  , appAct = "ApiDemos"
+  , describeWd = require("../../helpers/driverblock.js").describeForApp(appPath,
+      "android", appPkg, appAct)
+  , should = require('should');
+
+describeWd('get source', function(h) {
+  return it('should return the page source', function(done) {
+    h.driver.source(function(err, source){
+      var obj = JSON.parse(source);
+      should.not.exist(err);
+      should.ok(obj);
+      obj.hierarchy.node['@class'].should.equal("android.widget.FrameLayout");
+      obj.hierarchy.node.node.node[0].node['@class'].should.equal("android.view.View");
+      done();
+    });
+  });
+});

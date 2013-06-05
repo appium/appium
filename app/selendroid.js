8532e35869b9e7d3a99e85fe7d0af1dd85d999ea
Swapna Chennadi <Swapna.Chennadi@Gamesys.co.uk>
2013-06-05 16:35:53 +0100

    fixed spacing

--- app/selendroid.js
+++ app/selendroid.js
@@ -21,7 +21,7 @@ var Selendroid = function(opts) {
   this.adb = null;
   this.isProxy = true;
   this.proxyHost = 'localhost';
-  if(opts.selendroidPort === -1){
+  if (opts.selendroidPort === -1) {
     this.proxyPort = 8080;
   } else {
     this.proxyPort = opts.selendroidPort;

1a40c3db5cf1fb44fc4b83c42be7771cc9b2f3cd
Patrick Näf <herzbube@herzbube.ch>
2013-06-21 17:51:35 +0200

    look for ~/.profile in sysExec()

--- app/uiauto/lib/instruments_client.js
+++ app/uiauto/lib/instruments_client.js
@@ -22,11 +22,15 @@ var fileExists = function(filename) {
 var sysExec = function(cmd) {
   var params = [];
   if (user !== null) {
-    if (fileExists('/Users/' + user + '/.bash_profile')) {
-      params = params.concat(['--rcfile', '/Users/' + user + '/.bash_profile']);
+    if (fileExists('/Users/' + user + '/.profile')) {
+      params = params.concat(['--rcfile', '/Users/' + user + '/.profile']);
     } else {
-      if (fileExists('/Users/' + user + '/.bashrc')) {
-        params = params.concat(['--rcfile', '/Users/' + user + '/.bashrc']);
+      if (fileExists('/Users/' + user + '/.bash_profile')) {
+        params = params.concat(['--rcfile', '/Users/' + user + '/.bash_profile']);
+      } else {
+        if (fileExists('/Users/' + user + '/.bashrc')) {
+          params = params.concat(['--rcfile', '/Users/' + user + '/.bashrc']);
+        }
       }
     }
   }

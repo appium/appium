3d40d4be75887d908616b527a7fb61721abe5e93
Amit Kumar <kumar.amit@thinksys.com>
2013-07-19 12:20:04 +0530

    Changes Indentation in test suite

--- test/functional/testapp/pinchGesture.js
+++ test/functional/testapp/pinchGesture.js
@@ -7,23 +7,23 @@ var assert = require("assert")
 
 describeWd('pinchOpen and pinchClose gesture', function(h) {
   return it('should pinchOpen and pinchClose map after tapping Test Gesture', function(done) {
-      var driver = h.driver;
-      driver.elementsByTagName('button', function(err, buttons) {
-        buttons[3].click(function(err) {
-            should.not.exist(err);
+    var driver = h.driver;
+    driver.elementsByTagName('button', function(err, buttons) {
+      buttons[3].click(function(err) {
+        should.not.exist(err);
+        driver.elementByXPath('//window[1]/UIAMapView[1]', function(err) {
+          should.not.exist(err);
+          driver.execute("mobile: pinchOpen", [{startX: 114.0, startY: 198.0, endX: 257.0, endY: 256.0, duration: 5.0}], function(err) {
             driver.elementByXPath('//window[1]/UIAMapView[1]', function(err) {
+              should.not.exist(err);
+              driver.execute("mobile: pinchClose", [{startX: 114.0, startY: 198.0, endX: 257.0, endY: 256.0, duration: 5.0}], function(err) {
                 should.not.exist(err);
-                driver.execute("mobile: pinchOpen", [{startX: 114.0, startY: 198.0, endX: 257.0, endY: 256.0, duration: 5.0}], function(err) {
-                    driver.elementByXPath('//window[1]/UIAMapView[1]', function(err) {
-                        should.not.exist(err);
-                            driver.execute("mobile: pinchClose", [{startX: 114.0, startY: 198.0, endX: 257.0, endY: 256.0, duration: 5.0}], function(err) {
-                                should.not.exist(err);
-                                done();
-                            });
-                        });
-                    });
-                });
+                done();
+			  });
             });
+          });
         });
+      });
     });
+  });
 });
--- test/functional/testapp/rotationGesture.js
+++ test/functional/testapp/rotationGesture.js
@@ -9,15 +9,15 @@ describeWd('rotation gesture', function(h) {
   return it('should rotate map after tapping Test Gesture', function(done) {
     var driver = h.driver;
     driver.elementsByTagName('button', function(err, buttons) {
-        buttons[3].click();
-        should.not.exist(err);
+      buttons[3].click();
+      should.not.exist(err);
     });
     driver.elementsByTagName('Map', function(err) {
+      should.not.exist(err);
+      driver.execute("mobile: rotate", [{x: 114, y: 198, duration: 5, radius: 3, rotation: 220, touchCount: 2}], function(err) {
         should.not.exist(err);
-        driver.execute("mobile: rotate", [{x: 114, y: 198, duration: 5, radius: 3, rotation: 220, touchCount: 2}], function(err) {
-            should.not.exist(err);
-            done();
-        });
+        done();
+      });
     });
   });
 });

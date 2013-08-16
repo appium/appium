df451c5cc1132390f811c8ed7f5aa4c6a3f9107a
bootstraponline <code@bootstraponline.com>
2013-08-16 16:56:20 -0400

    mobile: localScreenshot

--- test/functional/safari/screenshot.js
+++ test/functional/safari/screenshot.js
@@ -3,10 +3,24 @@
 
 var desc = require("../../helpers/driverblock.js").describeForSafari()
   , _ = require("underscore")
+  , fs = require('fs')
   , should = require('should');
 
 _.each(["iPhone", "iPad"], function(device) {
   desc('screenshots (' + device + ')', function(h) {
+    it('should get a local screenshot', function(done) {
+      var localScreenshotFile = '/tmp/test_screenshot_appium.png';
+      if (fs.existsSync(localScreenshotFile)) {
+        fs.unlinkSync(localScreenshotFile);
+      }
+      h.driver.execute("mobile: localScreenshot", [{file: localScreenshotFile}], function(err) {
+        should.not.exist(err);
+        var screenshot = fs.readFileSync(localScreenshotFile);
+        should.exist(screenshot);
+        screenshot.length.should.be.above(1000);
+        done();
+      });
+    });
     it('should get an app screenshot', function(done){
       h.driver.takeScreenshot(function(err, screenshot){
         should.not.exist(err);

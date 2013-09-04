acefc0f6c6fa6126288ffa2f08f4fd99a35e30a9
bootstraponline <code@bootstraponline.com>
2013-09-04 13:24:49 -0400

    mobile: getStrings

---
+++ android/bootstrap/src/io/appium/android/bootstrap/handler/GetStrings.java
@@ -0,0 +1,27 @@
+package io.appium.android.bootstrap.handler;
+
+import io.appium.android.bootstrap.AndroidCommand;
+import io.appium.android.bootstrap.AndroidCommandResult;
+import io.appium.android.bootstrap.CommandHandler;
+
+/**
+ * This handler is used to get the apk strings.
+ * 
+ */
+public class GetStrings extends CommandHandler {
+
+  /*
+   * @param command The {@link AndroidCommand} used for this handler.
+   * 
+   * @return {@link AndroidCommandResult}
+   * 
+   * @throws JSONException
+   * 
+   * @see io.appium.android.bootstrap.CommandHandler#execute(io.appium.android.
+   * bootstrap.AndroidCommand)
+   */
+  @Override
+  public AndroidCommandResult execute(final AndroidCommand command) {
+    return getSuccessResult(Find.apkStrings);
+  }
+}

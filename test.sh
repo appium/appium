5629097cde00aff95cd85cb30b4c8f07fc7476f9
Jonathan Lipps <jlipps@gmail.com>
2013-09-15 14:04:49 +0200

    make running one set of platform tests easier with test.sh

--- test.sh
+++ test.sh
@@ -1,18 +1,31 @@
 #!/bin/sh
 set +e
 mocha_args=""
+ios_only=false
+android_only=false
+all_tests=true
 for arg in "$@"; do
-    if [[ "$arg" =~ " " ]]; then
+    if [[ "$arg" = "--ios" ]]; then
+        ios_only=true
+        all_tests=false
+    elif [[ "$arg" = "--android" ]]; then
+        android_only=true
+        all_tests=false
+    elif [[ "$arg" =~ " " ]]; then
         mocha_args="$mocha_args \"$arg\""
     else
         mocha_args="$mocha_args $arg"
     fi
 done
 alias appium_mocha="mocha -t 60000 -R spec $mocha_args"
-appium_mocha test/functional/apidemos
-appium_mocha test/functional/prefs
-appium_mocha test/functional/safari
-appium_mocha test/functional/selendroid
-appium_mocha test/functional/testapp
-appium_mocha test/functional/uicatalog
-appium_mocha test/functional/webview
+if [[ $ios_only || $all_tests ]]; then
+    appium_mocha test/functional/prefs
+    appium_mocha test/functional/safari
+    appium_mocha test/functional/testapp
+    appium_mocha test/functional/uicatalog
+    appium_mocha test/functional/webview
+fi
+if [[ $android_only || $all_tests ]]; then
+    appium_mocha test/functional/apidemos
+    appium_mocha test/functional/selendroid
+fi

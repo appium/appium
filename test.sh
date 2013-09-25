66e87f8cc02e29028db96a08591dfe70278ce05c
Jonathan Lipps <jlipps@gmail.com>
2013-09-25 14:22:49 +0100

    use strings instead of arrays for args in test.sh

--- test.sh
+++ test.sh
@@ -19,6 +19,7 @@ function join_testfiles {
     touch $outfile
     for indir in $indirs; do
         for infile in ./test/functional/$indir/*.js; do
+            echo "Collating test/functional/$indir/$infile..."
             echo "$pre\n" >> $outfile
             cat $infile >> $outfile
             echo "\n$post\n" >> $outfile
@@ -51,7 +52,7 @@ if $ios_only || $all_tests; then
     echo "RUNNING IOS 6.1 TESTS"
     echo "---------------------"
     ios_testfile="./test/functional/_joined/ios.js"
-    ios_dirs=(prefs safari testapp uicatalog webview)
+    ios_dirs="prefs safari testapp uicatalog webview"
     join_testfiles $ios_testfile $ios_dirs
     if test -d /Applications/Xcode-6.1.app; then
         echo "Found Xcode for iOS 6.1, switching to it"
@@ -67,7 +68,7 @@ if $ios7_only || $all_tests; then
     echo "RUNNING IOS 7.0 TESTS"
     echo "---------------------"
     ios7_testfile="./test/functional/_joined/ios7.js"
-    ios7_dirs=(testapp uicatalog webview)
+    ios7_dirs="testapp uicatalog webview"
     join_testfiles $ios7_testfile $ios7_dirs
     if test -d /Applications/Xcode-7.0.app; then
         echo "Found Xcode for iOS 7.0, switching to it"
@@ -88,7 +89,7 @@ if $android_only || $all_tests; then
     echo "RUNNING ANDROID TESTS"
     echo "---------------------"
     android_testfile="./test/functional/_joined/android.js"
-    android_dirs=(apidemos selendroid)
+    android_dirs="apidemos selendroid"
     join_testfiles $android_testfile $android_dirs
     $appium_mocha $android_testfile
 fi

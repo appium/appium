592f6c394a89508638b45f48bebf8586068069e1
Dan Cuellar <penguinho@aol.com>
2013-09-29 07:59:37 -0700

    Even Better reset.bat

--- reset.bat
+++ reset.bat
@@ -1,95 +1,128 @@
 @echo off
 
-:: go to directory containing batch file
+:: Go to directory containing batch file
 FOR /f %%i in ("%0") DO SET curpath=%%~dpi
-cd /d %curpath%
+CD /d %curpath%
 
+:: Flags to determine which parts will be executed
 SET doDev=0
 SET doSelendroid=0
 SET doAndroid=0
+SET doVerbose=0
+SET doForce=0
 
+:: Read in command line switches
 FOR %%A IN (%*) DO IF "%%A" == "--dev" SET doDev=1
 FOR %%A IN (%*) DO IF "%%A" == "--android" SET doAndroid=1
 FOR %%A IN (%*) DO IF "%%A" == "--selendroid" SET doSelendroid=1
+FOR %%A IN (%*) DO IF "%%A" == "--verbose" SET doVerbose=1
+FOR %%A IN (%*) DO IF "%%A" == "--force" SET doForce=1
+
+:: If nothing is flagged do only android
+IF %doDev% == 0 IF %doSelendroid% == 0 IF %doAndroid% == 0 SET doAndroid=1
 
 :: Install Package and Dependencies
-echo.
-echo npm install .
-call npm install .
+ECHO.
+ECHO =====Installing dependencies with npm=====
+ECHO.
+CALL :runCmd "npm install ."
 
-::Install Dev Dependencies
+:: Install Dev Dependencies
 if %doDev% == 1 (
-  echo.
-  echo npm install . --dev
-  call npm install . --dev
+  ECHO.
+  ECHO =====Installing development dependencies with npm=====
+  ECHO.
+  CALL :runCmd "npm install . --dev"
+  ECHO.
 )
 
-::Reset Android
+:: Reset Android
 if %doAndroid% == 1 (
-  echo.
-  echo =====Resetting Android=====
-  echo.
-  echo grunt configAndroidBootstrap
-  call node_modules\.bin\grunt configAndroidBootstrap
-  echo.
-  echo grunt buildAndroidBootstrap
-  call node_modules\.bin\grunt buildAndroidBootstrap
-  echo.
-  echo grunt setConfigVer:android
-  call node_modules\.bin\grunt setConfigVer:android
-  echo.
-  echo =====Reset Complete=====
-
-  echo.
-  echo =====Resetting Unlock.apk=====
-  echo.
-  call rd /S /Q build\unlock_apk
-  call mkdir build\unlock_apk
-  echo Building Unlock.apk
-  call git submodule update --init submodules\unlock_apk
-  call pushd submodules\unlock_apk
-  call ant clean
-  call ant debug
-  call popd
-  call copy submodules\unlock_apk\bin\unlock_apk-debug.apk build\unlock_apk\unlock_apk-debug.apk
-  echo =====Reset Complete=====
+  ECHO.
+  ECHO =====Resetting Android=====
+  ECHO.
+  CALL :runCmd "node_modules\.bin\grunt configAndroidBootstrap"
+  CALL :runCmd "node_modules\.bin\grunt buildAndroidBootstrap"
+  CALL :runCmd "node_modules\.bin\grunt setConfigVer:android"
+  ECHO.
+  ECHO =====Reset Android Complete=====
+  
+  ECHO.
+  ECHO =====Resetting Unlock.apk=====
+  ECHO.
+  CALL :runCmd "RD /S /Q build\unlock_apk"
+  CALL :runCmd "MKDIR build\unlock_apk"
+  ECHO Building Unlock.apk
+  CALL :runCmd "git submodule update --init submodules\unlock_apk"
+  CALL :runCmd "PUSHD submodules\unlock_apk"
+  CALL :runCmd "ant clean"
+  CALL :runCmd "ant debug"
+  CALL :runCmd "POPD"
+  CALL :runCmd "COPY submodules\unlock_apk\bin\unlock_apk-debug.apk build\unlock_apk\unlock_apk-debug.apk"
+  ECHO.
+  ECHO =====Reset Unlock.apk Complete=====
 
   :: Reset Android Dev
-  if %doDev% == 1 (
-    echo.
-    echo =====Resetting API Demos=====
-    echo.
-    echo Cloning/updating Android test app: ApiDemos
-    call git submodule update --init submodules\ApiDemos
-    call rd /S /Q sample-code\apps\ApiDemos
-    call mkdir sample-code\apps\ApiDemos
-    call xcopy submodules\ApiDemos sample-code\apps\ApiDemos /E /Q
-    echo.
-    echo grunt configAndroidApp:ApiDemos
-    call node_modules\.bin\grunt configAndroidApp:ApiDemos
-    echo.
-    echo grunt buildAndroidApp:ApiDemos
-    call node_modules\.bin\grunt buildAndroidApp:ApiDemos
+  IF %doDev% == 1 (
+    ECHO.
+    ECHO =====Resetting API Demos=====
+    ECHO.
+    ECHO Cloning/updating Android test app: ApiDemos
+    CALL :runCmd "git submodule update --init submodules\ApiDemos"
+    CALL :runCmd "RD /S /Q sample-code\apps\ApiDemos | VER > NUL"
+    CALL :runCmd "MKDIR sample-code\apps\ApiDemos"
+    CALL :runCmd "XCOPY submodules\ApiDemos sample-code\apps\ApiDemos /E /Q"
+    CALL :runCmd "node_modules\.bin\grunt configAndroidApp:ApiDemos"
+    CALL :runCmd "node_modules\.bin\grunt buildAndroidApp:ApiDemos"
+    ECHO.
+    ECHO =====Reset API Demos Complete=====
   )
 )
 
-::Reset Selendroid
+:: Reset Selendroid
 IF %doSelendroid% == 1 (
-  echo.
-  echo =====Resetting Selendroid=====
-  echo.
-  echo Clearing out any old modified server apks
-  call rd -/S /Q %windir%\Temp\selendroid*.apk
-  echo Cloning/updating selendroid
-  call rd -/S /Q submodules\selendroid\selendroid-server\target
-  call git submodule update --init submodules\selendroid
-  call rd /S /Q selendroid
-  echo Building selendroid server and supporting libraries
-  echo grunt buildSelendroidServer --force
-  call node_modules\.bin\grunt buildSelendroidServer --force
-  echo Setting Selendroid config to Appium's version
-  echo grunt setConfigVer:selendroid
-  call node_modules\.bin\grunt setConfigVer:selendroid
-  echo.
-  echo =====Reset Complete=====
+  ECHO.
+  ECHO =====Resetting Selendroid=====
+  ECHO.
+  ECHO Clearing out any old modified server apks
+  CALL :runCmd "RD -/S /Q %windir%\Temp\selendroid*.apk | VER > NUL"
+  ECHO Cloning/updating selendroid
+  CALL :runCmd "RD -/S /Q submodules\selendroid\selendroid-server\target | VER > NUL"
+  CALL :runCmd "git submodule update --init submodules\selendroid"
+  CALL :runCmd "RD /S /Q selendroid  | VER > NUL"
+  ECHO Building selendroid server and supporting libraries
+  CALL :runCmd "node_modules\.bin\grunt buildSelendroidServer"
+  ECHO Setting Selendroid config to Appium's version
+  CALL :runCmd "node_modules\.bin\grunt setConfigVer:selendroid"
+  ECHO.
+  ECHO =====Reset Selendroid Complete=====
 )
+ECHO.
+GOTO :EOF
+
+:: Function to run commands
+:runCmd - function to run a command
+  IF %doVerbose% == 1 ECHO %~1
+  CALL %~1
+  IF %ERRORLEVEL% NEQ 0 IF %doForce% == 0 (
+    CD /D %curpath%
+    ECHO.
+    ECHO Stopping because there was an error
+    CALL :halt 1
+  )
+GOTO :EOF
+
+:: Sets the errorlevel and stops the batch immediately
+:halt
+CALL :__SetErrorLevel %1
+CALL :__ErrorExit 2> NUL
+GOTO :EOF
+
+:__ErrorExit
+REM Creates a syntax error, stops immediately
+() 
+GOTO :EOF
+
+:__SetErrorLevel
+EXIT /B %TIME:~-2%
+GOTO :EOF

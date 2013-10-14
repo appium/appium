d0235a4d0ff97d34623adf7f7c1f781cc7cc0bee
Jonathan Lipps <jlipps@gmail.com>
2013-10-14 09:51:39 -0700

    make sure we publish compiling with xcode5

--- publish.sh
+++ publish.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 set +e
+
+XCODE_PATH=$(xcode-select -print-path)
+if test -d $XCODE_PATH/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator7.0.sdk; then
+    echo "Confirmed iOS7 SDK available"
+else
+    echo "You don't have iOS 7 SDK available. Switch to Xcode 5?"
+    exit 1
+fi
+
 git remote | grep "upstream" >/dev/null
 if [ $? -gt 0 ]; then
     echo "You need to have an 'upstream' remote to pull from / push tags to"
@@ -15,6 +24,7 @@ if [ $? -eq 0 ]; then
     echo "Your branch isn't in sync with master"
     exit 1
 fi
+
 set -e
 git pull upstream master
 ./reset.sh --hardcore

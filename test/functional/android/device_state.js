d0774ea4dce0a736571515e7fe2e1fda8498f859
Jonathan Lipps <jlipps@gmail.com>
2013-10-16 10:28:31 -0700

    make some files pass the linter

--- test/functional/android/device_state.js
+++ test/functional/android/device_state.js
@@ -1,3 +1,6 @@
+/*global describe:true, it:true, beforeEach:true */
+"use strict";
+
 var deviceState = require('../../../android/device_state')
   , should = require('should')
   , childProcess = require('child_process')
@@ -7,7 +10,7 @@ var deviceState = require('../../../android/device_state')
 describe('Android Device State module', function() {
   beforeEach(function(done) {
     // ensure a device or emu is connected
-    childProcess.exec("adb devices", function(err, stdout, stderr) {
+    childProcess.exec("adb devices", function(err, stdout) {
       should.not.exist(err);
       var device = /\n([A-Za-z0-9\-]+)\W+device\n/.exec(stdout);
       device = device[1];
@@ -19,14 +22,14 @@ describe('Android Device State module', function() {
   describe('isScreenLocked method', function() {
     it('should return true if screen is locked', function(done) {
       // Press POWER btn to lock screen first
-      childProcess.exec('adb shell input keyevent 26', function(err, stdout, stderr) {
+      childProcess.exec('adb shell input keyevent 26', function(err) {
         should.not.exist(err);
 
-        childProcess.exec('adb shell input keyevent 26', function(err, stdout, stderr) {
+        childProcess.exec('adb shell input keyevent 26', function(err) {
           should.not.exist(err);
           deviceState.isScreenLocked('adb', function(err, isLocked) {
             should.not.exist(err);
-            isLocked.should.be.true;
+            isLocked.should.equal(true);
             done();
           });
         });
@@ -37,12 +40,12 @@ describe('Android Device State module', function() {
       var adb = new ADB();
       adb.pushUnlock(function(err) {
         should.not.exist(err);
-        childProcess.exec('adb shell am start -n io.appium.unlock/.Unlock', function(err, stdout, stderr) {
+        childProcess.exec('adb shell am start -n io.appium.unlock/.Unlock', function(err) {
           should.not.exist(err);
           setTimeout(function() {
             deviceState.isScreenLocked('adb', function(err, isLocked) {
               should.not.exist(err);
-              isLocked.should.be.false;
+              isLocked.should.equal(false);
               done();
             });
           }, 2500);

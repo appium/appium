98667ea8f0b6b043138b540fae97999cc4f7b4d6
Dan Cuellar <penguinho@aol.com>
2013-11-30 18:11:56 -0800

    Adding the ability to run appium-doctor via the app using socket.io

--- bin/appium-doctor.js
+++ bin/appium-doctor.js
@@ -3,29 +3,37 @@
 var ios = require('../lib/doctor/ios.js')
   , android = require('../lib/doctor/android.js')
   , common = require("../lib/doctor/common.js")
-  , log = require('../lib/doctor/common.js').log
   , eol = require('os').EOL
   , async = require('async');
 
 var argv = process.argv
   , doAndroid = argv.indexOf('--android') > -1
   , doIOS = argv.indexOf('--ios') > -1
-  , verbose = argv.indexOf('--verbose') > -1;
+  , verbose = argv.indexOf('--verbose') > -1
+  , broadcast = argv.indexOf('--port') > -1
+  , port = null;
+
+if (broadcast) {
+  port = argv[argv.indexOf("--port") + 1];
+}
 
 if (!doIOS && !doAndroid) {
   doIOS = true;
   doAndroid = true;
 }
 
+var log = new common.Log(port);
+
 var runiOSChecks = function(cb) {
   if (doIOS) {
+    var iosChecker = new ios.IOSChecker(log);
     log.comment("Running iOS Checks");
-    ios.runAllChecks(function(err) {
+    iosChecker.runAllChecks(function(err) {
       if (!err) {
         log.pass("iOS Checks were successful." + eol);
         cb();
       } else {
-        common.exitDoctor();
+        log.exitDoctor();
       }
     });
   }
@@ -33,27 +41,38 @@ var runiOSChecks = function(cb) {
 
 var runAndroidChecks = function(cb) {
   if (doAndroid) {
+    var androidChecker = new android.AndroidChecker(log);
     log.comment("Running Android Checks");
-    android.runAllChecks(function(err) {
+    androidChecker.runAllChecks(function(err) {
       if (!err) {
         log.pass("Android Checks were successful." + eol);
         cb();
       } else {
-        common.exitDoctor();
+        log.exitDoctor();
       }
     });
   }
 };
 
 if (require.main === module) {
-  async.series([
-    runiOSChecks,
-    runAndroidChecks
-  ], function(err) {
-    if (!err) {
-      log.pass("All Checks were successful");
-    } else {
-      common.exitDoctor();
-    }
-  });
+
+  var mainMethod = function() {
+    async.series([
+      runiOSChecks,
+      runAndroidChecks
+    ], function(err) {
+      if (!err) {
+        log.pass("All Checks were successful");
+        log.stopBroadcast();
+      } else {
+        common.exitDoctor();
+      }
+    });
+  };
+
+  if (log.broadcast) {
+    log.startBroadcast(mainMethod);
+  } else {
+    mainMethod();
+  }
 }

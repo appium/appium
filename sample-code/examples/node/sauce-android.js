18917698a46b9db6e506adc60978c7ee203d0697
Jonathan Lipps <jlipps@gmail.com>
2013-12-09 15:12:41 -0800

    update sauce android example to not be dependent on driverblock

--- sample-code/examples/node/sauce-android.js
+++ sample-code/examples/node/sauce-android.js
@@ -1,33 +1,54 @@
-/*global it:true */
+/*global it:true, describe:true, beforeEach:true, afterEach:true */
 
 /* EXAMPLE APPIUM + SAUCE LABS INTEGRATION
    First: npm install mocha -g; npm install wd
-   Usage: SAUCE_USERNAME=xxx SAUCE_ACCESS_KEY=yyy mocha sauce-android.js */
+   Usage: SAUCE_USERNAME=xxx SAUCE_ACCESS_KEY=yyy mocha -t 300000 sauce-android.js */
 
 "use strict";
 
 var should = require("should")
-  , appUrl = 'http://appium.s3.amazonaws.com/NotesList.apk'
-  , dbPath = "../../../test/helpers/driverblock.js"
-  , describeSauce = require(dbPath).describeForSauce(appUrl, 'Android')
-  , extraCaps = {
-      "app-activity": ".NotesList"
-      , "app-package": "com.example.android.notepad"
-    };
+  , wd = require("wd")
+  , appUrl = 'http://appium.s3.amazonaws.com/NotesList.apk';
 
-describeSauce('notes app', function(h) {
+describe('notes app', function() {
+  var driver = null;
+
+  beforeEach(function(done) {
+    driver = wd.remote("ondemand.saucelabs.com", 80,
+      process.env.SAUCE_USERNAME, process.env.SAUCE_ACCESS_KEY);
+    driver.init({
+      device: 'Android',
+      version: '4.2',
+      app: appUrl,
+      name: 'Sauce Android test',
+      'app-activity': '.NotesList',
+      'app-package': 'com.example.android.notepad'
+    }, function(err) {
+      should.not.exist(err);
+      done();
+    });
+  });
+
+  afterEach(function(done) {
+    driver.quit(function(err) {
+      should.not.exist(err);
+      done();
+    });
+  });
+
   it('should save a note', function(done) {
-    h.driver.elementByName("New note", function(err, el) {
+    driver.elementByName("New note", function(err, el) {
       el.click(function() {
-        h.driver.elementByTagName("textfield", function(err, el) {
+        driver.elementByTagName("textfield", function(err, el) {
           el.sendKeys("This is a new note!", function() {
-            h.driver.elementByName("Save", function(err, el) {
+            driver.elementByName("Save", function(err, el) {
               el.click(function() {
-                h.driver.elementsByTagName("text", function(err, els) {
+                driver.elementsByTagName("text", function(err, els) {
                   els[2].text(function(err, text) {
                     text.should.equal("This is a new note!");
-                    els[2].click(function() {
-                      h.driver.quit(done);
+                    els[2].click(function(err) {
+                      should.not.exist(err);
+                      done();
                     });
                   });
                 });
@@ -38,4 +59,4 @@ describeSauce('notes app', function(h) {
       });
     });
   });
-}, extraCaps);
+});

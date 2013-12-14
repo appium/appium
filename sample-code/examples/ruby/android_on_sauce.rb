597b55bf6a2f865661cc3fc0b51812d12e5e024b
Dylan Lacey <github@dylanlacey.com>
2013-12-13 22:50:57 -0800

    Make ruby sauce examples follow the same structure.
    Make Android and Ruby examples have helper code at the bottom, similar
    method names

--- sample-code/examples/ruby/android_on_sauce.rb
+++ sample-code/examples/ruby/android_on_sauce.rb
@@ -24,7 +24,46 @@ require "rest_client"
 SAUCE_USERNAME = ENV['SAUCE_USERNAME']
 SAUCE_ACCESS_KEY = ENV['SAUCE_ACCESS_KEY']
 
-def desired_capabilites
+describe "Notepad" do
+  before :each do
+    http_client = ::Selenium::WebDriver::Remote::Http::Persistent.new
+    http_client.timeout = 300 # Allow for slow network or boot time
+    
+    @driver = Selenium::WebDriver.for(
+      :remote, 
+      :desired_capabilities => desired_caps,
+      :url => server_url,
+      :http_client => http_client
+    )
+    
+    http_client.timeout = 90
+  end
+
+  after(:each) do
+    # Get the success by checking for assertion exceptions,
+    # and log them against the job, which is exposed by the session_id
+    job_id = @driver.send(:bridge).session_id
+    update_job_success(job_id, example.exception.nil?)
+    @driver.quit
+  end
+
+  it "can create and save new notes" do
+    new_button = @driver.find_element(:name, "New note")
+    new_button.click
+
+    text_field = @driver.find_element(:tag_name, "textfield")
+    text_field.send_keys "This is a new note, from Ruby"
+
+    save_button = @driver.find_element(:name, "Save")
+    save_button.click
+
+    notes = @driver.find_elements(:tag_name, "text")
+    puts "The number of notes is: #{notes.length}"
+    notes[2].text.should eq "This is a new note, from Ruby"
+  end
+end
+
+def desired_caps
   {
     "device" => "Android",
     "browserName" => "",
@@ -69,43 +108,4 @@ end
 # Labs REST API to record job success or failure
 def update_job_success(job_id, success)
     RestClient.put "#{rest_jobs_url}/#{job_id}", {"passed" => success}.to_json, :content_type => :json
-end
-
-describe "Notepad" do
-  before :all do
-    http_client = ::Selenium::WebDriver::Remote::Http::Persistent.new
-    http_client.timeout = 300 # Allow for slow network or boot time
-    
-    @driver ||= Selenium::WebDriver.for(
-      :remote, 
-      :desired_capabilities => desired_capabilites,
-      :url => server_url,
-      :http_client => http_client
-    )
-    
-    http_client.timeout = 90
-  end
-
-  after(:each) do
-    # Get the success by checking for assertion exceptions,
-    # and log them against the job, which is exposed by the session_id
-    job_id = @driver.send(:bridge).session_id
-    update_job_success(job_id, example.exception.nil?)
-    @driver.quit
-  end
-
-  it "can create and save new notes" do
-    new_button = @driver.find_element(:name, "New note")
-    new_button.click
-
-    text_field = @driver.find_element(:tag_name, "textfield")
-    text_field.send_keys "This is a new note, from Ruby"
-
-    save_button = @driver.find_element(:name, "Save")
-    save_button.click
-
-    notes = @driver.find_elements(:tag_name, "text")
-    puts "The number of notes is: #{notes.length}"
-    notes[2].text.should eq "This is a new note, from Ruby"
-  end
 end

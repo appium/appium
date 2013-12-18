4a1cc2580c26d14d1ca609bdf49d778f805fba52
Jonathan Lipps <jlipps@gmail.com>
2013-12-18 13:29:02 -0800

    refactor instruments and add appium-internal flake support This commit constitutes a pretty big refactor of the instruments process handler. It also introduces a server flag, '--backend-retries', with a default of '3', which specifies how many times Appium will retry launching instruments before giving up and saying that it crashed or timed out. This works in concert with launchTimeout, of course.
    I think this refactor also fixes some longstanding callback reasoning
    errors and maybe some other bugs.
    
    It might also introduce some bugs of its own though :-)

--- test/functional/testapp/device.js
+++ test/functional/testapp/device.js
@@ -8,13 +8,12 @@ describeWd('device target actions', function(h) {
   it("should die in background and respond within (+/- 6 secs)", function(done) {
     var before = new Date().getTime() / 1000;
     h.driver.execute("mobile: background", [{seconds: 1}], function(err) {
-      should.not.exist(err);
+      should.exist(err);
+      err.status.should.equal(13);
+      err.cause.value.message.should.contain("Instruments died");
       var after = new Date().getTime() / 1000;
       should.ok((after - before) <= 10);
-      h.driver.elementByTagName('button', function(err) {
-        should.exist(err);
-        done();
-      });
+      done();
     });
   });
 });

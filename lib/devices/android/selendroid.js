appium/appium

commit 71ba778bd43e6d71cfab212275c2fb68e61fb8da

fix selendroid ECONNRESET crashes by making sure that activities come prepended with a dot. and add a test

lib/devices/android/selendroid.js
@@ -127,7 +127,11 @@ Selendroid.prototype.start = function(cb) {
 Selendroid.prototype.pushSelendroid = function(cb) {
   var instrumentWith = this.appPackage + ".selendroid/" +
                        "io.selendroid.ServerInstrumentation";
-  this.adb.instrument(this.appPackage, this.appActivity, instrumentWith, cb);
+  var act = this.appActivity;
+  if (act[0] !== '.') {
+    act = '.' + act;
+  }
+  this.adb.instrument(this.appPackage, act, instrumentWith, cb);
 };
 
 Selendroid.prototype.checkSelendroidCerts = function(cb) {

test/functional/selendroid/basic.js
@@ -4,8 +4,10 @@ var path = require('path')
   , appPath = path.resolve(__dirname, "../../../sample-code/apps/ApiDemos/bin/ApiDemos-debug.apk")
   , appPkg = "com.example.android.apis"
   , appAct = ".ApiDemos"
+  , appAct2 = "ApiDemos"
   , driverBlock = require("../../helpers/driverblock.js")
   , describeWd = driverBlock.describeForApp(appPath, "selendroid", appPkg, appAct)
+  , describeWd2 = driverBlock.describeForApp(appPath, "selendroid", appPkg, appAct2)
   , it = driverBlock.it
   , should = require('should');
 
@@ -102,3 +104,9 @@ describeWd('command timeouts', function(h) {
     find();
   });
 }, null, null, {newCommandTimeout: 3});
+
+describeWd2('app activities with no dot', function() {
+  it('should still launch app', function(done) {
+    done();
+  });
+});

commit f3c7f230e834babdc467699e6e499812c33ee244

Do not a dot to the activity name if it includes the package name
Commit 71ba778bd43e6d71cfab212275c2fb68e61fb8da introduced a regression
if the activity uses the full format like com.package.name.MainActivity

lib/devices/android/selendroid.js
@@ -132,7 +132,7 @@ Selendroid.prototype.pushSelendroid = function(cb) {
   var instrumentWith = this.appPackage + ".selendroid/" +
                        "io.selendroid.ServerInstrumentation";
   var act = this.appActivity;
-  if (act[0] !== '.') {
+  if (act[0] !== '.' && act.indexOf(this.appPackage) !== 0) {
     act = '.' + act;
   }
   this.adb.instrument(this.appPackage, act, instrumentWith, cb);
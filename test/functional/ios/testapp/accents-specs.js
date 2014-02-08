b5dbcecfaaede15e5f7dbadd2eac083f0c392da5
Isaac Murchie <isaac@saucelabs.com>
2014-02-07 17:07:44 -0800

    Add exceptions to accent check

--- test/functional/ios/testapp/accents-specs.js
+++ test/functional/ios/testapp/accents-specs.js
@@ -3,7 +3,7 @@
 var setup = require("../../common/setup-base"),
     desired = require('./desired');
 
-// TODO: check why this doesn't work 
+// TODO: check why this doesn't work
 describe('testapp - accented characters @skip-ios-all', function () {
   var driver;
   setup(this, desired).then(function (d) { driver = d; });
@@ -14,6 +14,16 @@ describe('testapp - accented characters @skip-ios-all', function () {
         .sendKeys("é Œ ù ḍ")
         .text()
         .should.become("é Œ ù ḍ")
+      .nodeify(done);
+  });
+
+  it('should send delete key', function (done) {
+    driver
+      .elementsByTagName('textField').at(1)
+        .sendKeys("abc")
+        .sendKeys('\uE003')
+        .text()
+        .should.become("")
       .nodeify(done);
   });
 });

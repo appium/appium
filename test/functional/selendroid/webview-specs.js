21c9242fb6a2ed61dfc71a14e664b0ea17b1b10e
Jonathan Lipps <jlipps@gmail.com>
2014-02-24 14:59:17 -0800

    finish android hybrid support and refactor android webview tests to work for android as well as selendroid

--- test/functional/selendroid/webview-specs.js
+++ test/functional/selendroid/webview-specs.js
@@ -1,75 +1,7 @@
-/*global beforeEach:true */
 "use strict";
 
 process.env.DEVICE = process.env.DEVICE || "selendroid";
-var env = require('../../helpers/env')
-  , setup = require("../common/setup-base")
-  , path = require('path');
-
-var desired = {
-  app: path.resolve(__dirname, "../../../sample-code/apps/selendroid-test-app.apk"),
-  'app-package': 'io.selendroid.testapp',
-  'app-activity': '.HomeScreenActivity'
-};
-
+var androidWebviewTests = require('../../helpers/android-webview');
 // if it doesn't work run: adb uninstall io.selendroid.testapp
 
-describe('selendroid - web_view -', function () {
-  var driver;
-  setup(this, desired).then(function (d) { driver = d; });
-
-  beforeEach(function (done) {
-    driver
-      .waitForElementById('buttonStartWebviewCD').click()
-      .window('WEBVIEW')
-      .nodeify(done);
-  });
-
-  if (env.FAST_TESTS) {
-    afterEach(function (done) {
-      driver
-        .window('NATIVE_APP')
-        .elementByIdOrNull('goBack').then(function (el) {
-          if (el) return el.click().sleep(1000);
-        }).nodeify(done);
-    });
-  }
-
-  it('should be web view', function (done) {
-    // todo: add some sort of check here
-    done();
-  });
-
-  it('should find and click an element', function (done) {
-    driver
-      .elementByCssSelector('input[type=submit]').click()
-      .sleep(1000)
-      .elementByTagName('h1').text()
-        .should.become("This is my way of saying hello")
-      .nodeify(done);
-  });
-
-  it('should clear input', function (done) {
-    driver
-      .elementById('name_input').clear().getValue().should.become("")
-      .nodeify(done);
-  });
-
-  it('should find and enter key sequence in input', function (done) {
-    driver
-      .elementById('name_input').clear()
-        .type("Mathieu").getValue().should.become("Mathieu")
-      .nodeify(done);
-  });
-
-  it('should be able to handle selendroid special keys', function (done) {
-    driver.keys('\uE102').nodeify(done);
-  });
-
-  it('should get web source', function (done) {
-    driver
-      .source().should.eventually.include("children")
-      .nodeify(done);
-  });
-
-});
+describe('selendroid - web_view -', androidWebviewTests);

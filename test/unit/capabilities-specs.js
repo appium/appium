8a08899580cc8f08cdb943c1bf92800d4317a538
Dylan Lacey <github@dylanlacey.com>
2014-03-11 07:07:35 -0700

    Emit deprecation warnings
    Capabilities are now an object, created by passing the capabilities
    to Capabilities.
    
    Properties on that object are copied into the Capabilities object so the
    API is identical.
    
    If a deprecated capability is used, logger.warn is sent a deprecation
    message.

---
+++ test/unit/capabilities-specs.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var Capabilities = require('../../lib/server/capabilities.js')
+  , loggerjs = require('../../lib/server/logger')
+  , logger = loggerjs.get('appium')
+  , sinon = require('sinon')
+  , chai = require('chai')
+  , should = chai.should()
+  , _ = require('Underscore');
+
+describe('capabilities', function () {
+  describe('#new', function () {
+    var warning = function (capability) {
+      return ("[DEPRECATED] The '" + capability + "' capability is " +
+              "deprecated, and will be removed in Appium 1.0");
+    };
+    var oldCapabilities = [
+      'device'
+    , 'version'
+    , 'app-package'
+    ];
+
+    beforeEach(function () {
+      sinon.spy(logger, "warn");
+    });
+    afterEach(function () {
+      logger.warn.restore();
+    });
+
+    _.each(oldCapabilities, function (item) {
+      var specName = "Should return a deprecation warning when given the " +
+                     JSON.stringify(item) + " capability";
+      it(specName, function () {
+        var fakeCaps = {};
+        fakeCaps[item] = 'dontcare';
+        new Capabilities(fakeCaps);
+        (logger.warn.args[0][0]).should.equal(warning(item));
+      });
+    });
+  });
+});

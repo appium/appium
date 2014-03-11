32c39a1f451e2592f6221aacb6c3da8b14c9b679
sebv <seb.vincent@gmail.com>
2014-03-11 21:47:16 +0800

    test fixes

--- test/helpers/reset.js
+++ test/helpers/reset.js
@@ -1,6 +1,7 @@
 "use strict";
 
-var Q = require('q'),
+var env = require('./env'),
+    Q = require('q'),
     exec = Q.denodeify(require('child_process').exec);
 
 exports.androidReset = function (appPackage, appActivity) {
@@ -10,7 +11,7 @@ exports.androidReset = function (appPackage, appActivity) {
 
 exports.androidUninstall = function (appPackage) {
   var cmd = 'adb uninstall ' + appPackage;
-  return exec(cmd)
+  return exec(cmd, {timeout: env.LAUNCH_TIMEOUT / 2})
     .catch(function () {})
     .then(function () { return Q.delay(500); });
 };

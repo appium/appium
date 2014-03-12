36c0e60e5d4b8674e58decff809b00d3cf7332b5
Dylan Lacey <github@dylanlacey.com>
2014-03-12 09:38:52 -0700

    Emit deprecation warnings at the end of sessions

--- test/unit/helper-specs.js
+++ test/unit/helper-specs.js
@@ -7,7 +7,7 @@ var helpers = require('../../lib/helpers.js')
   , sinon = require('sinon');
 
 describe("Helpers", function () {
-  describe("#generateDeprecationWarning", function () {
+  describe("#formatDeprecationWarning", function () {
     describe("with a function", function () {
       it("emits the function message", function () {
         var deprecated = "oldFunction";
@@ -15,9 +15,9 @@ describe("Helpers", function () {
         var expected = "[DEPRECATED] The " + deprecated + " function has " +
                        "been deprecated and will be removed.  Please use " +
                        "the " + replacement + " function instead.";
-        var warning = helpers.generateDeprecationWarning('function',
-                                                         deprecated,
-                                                         replacement);
+        var warning = helpers.formatDeprecationWarning('function',
+                                                        deprecated,
+                                                        replacement);
         warning.should.equal(expected);
       });
     });
@@ -29,9 +29,9 @@ describe("Helpers", function () {
         var expected = "[DEPRECATED] The " + deprecated + " capability has " +
                        "been deprecated and will be removed.  Please use " +
                        "the " + replacement + " capability instead.";
-        var warning = helpers.generateDeprecationWarning('capability',
-                                                         deprecated,
-                                                         replacement);
+        var warning = helpers.formatDeprecationWarning('capability',
+                                                        deprecated,
+                                                        replacement);
         warning.should.equal(expected);
       });
     });
@@ -45,12 +45,14 @@ describe("Helpers", function () {
       logger.warn.restore();
     });
     it('sends a message to warn', function () {
-      helpers.emitDeprecationWarning('function', 'old', 'new');
+      helpers.addDeprecationWarning('function', 'old', 'new');
+      helpers.emitDeprecationWarnings();
       logger.warn.called.should.equal(true);
     });
     it('is only called once per run', function () {
-      helpers.emitDeprecationWarning('function', 'old', 'new');
-      helpers.emitDeprecationWarning('function', 'old', 'new');
+      helpers.addDeprecationWarning('function', 'old', 'new');
+      helpers.addDeprecationWarning('function', 'old', 'new');
+      helpers.emitDeprecationWarnings();
       logger.warn.called.should.equal(true);
     });
   });

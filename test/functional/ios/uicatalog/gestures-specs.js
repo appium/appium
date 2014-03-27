148e5f3ba3992487c1528e9407cd277a421d451a
Linzhen Xuan <lxuan@linkedin.com>
2014-03-26 23:55:39 -0700

    Modify scroll to take element argument.
    Originally, scroll scrolls insider the first ScrollView. However, most of our ScrollViews is CollectionView, or TableView. So, we got error that the ScrollView could not found, although there are multiple "ScrollViews".
    
    We also need to able to scroll insider different scrollviews.

--- test/functional/ios/uicatalog/gestures-specs.js
+++ test/functional/ios/uicatalog/gestures-specs.js
@@ -448,6 +448,33 @@ describe('uicatalog - gestures -', function () {
       })
       .nodeify(done);
     });
+    it('should scroll down and up using element', function (done) {
+      var firstEl, location1, location2, table_view;
+      driver.elementByTagName('tableview').then(function (el) {
+        table_view = el;
+      })
+      .elementByTagName('tableCell')
+      .then(function (el) { firstEl = el; return el.getLocation(); })
+      .then(function (loc) { location1 = loc; })
+      .then(function () {
+        return driver.execute("mobile: scroll", [{element: table_view.value, direction: 'down'}]);
+      })
+      .then(function () { return firstEl.getLocation(); })
+      .then(function (loc2) {
+        location2 = loc2;
+        loc2.x.should.equal(location1.x);
+        loc2.y.should.not.equal(location1.y);
+      })
+      .then(function () {
+        return driver.execute("mobile: scroll", [{element: table_view.value, direction: 'up'}]);
+      })
+      .then(function () { return firstEl.getLocation(); })
+      .then(function (loc3) {
+        loc3.x.should.equal(location2.x);
+        loc3.y.should.not.equal(location2.y);
+      })
+      .nodeify(done);
+    });
   });
 
   describe('mobile shake', function () {

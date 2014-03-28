78f3a439822789a3dce163d1a8acc386ace47af2
Jonathan Lipps <jlipps@gmail.com>
2014-03-27 19:16:16 -0700

    add test for existing xml source behavior

--- test/functional/android/apidemos/source-specs.js
+++ test/functional/android/apidemos/source-specs.js
@@ -1,6 +1,8 @@
 "use strict";
 
 var setup = require("../../common/setup-base")
+  , xpath = require("xpath")
+  , XMLDom = require("xmldom").DOMParser
   , desired = require("./desired");
 
 describe("apidemos - source -", function () {
@@ -16,9 +18,19 @@ describe("apidemos - source -", function () {
         source.should.include('@class');
         var obj = JSON.parse(source);
         obj.should.exist;
-        // probably no need for so precise tests 
+        // probably no need for so precise tests
         //obj.hierarchy.node['@class'].should.equal("android.widget.FrameLayout");
         //obj.hierarchy.node.node.node[0].node['@class'].should.equal("android.widget.FrameLayout");
+      }).nodeify(done);
+  });
+  it('should return the page source as xml', function (done) {
+    driver
+      .elementByNameOrNull('Accessibility') // waiting for page to load
+      .execute("mobile: source", [{type: 'xml'}]).then(function (source) {
+        source.should.exist;
+        var dom = new XMLDom().parseFromString(source);
+        var nodes = xpath.select('//node[@content-desc="App"]', dom);
+        nodes.length.should.equal(1);
       }).nodeify(done);
   });
   it('should return the page source without crashing other commands', function (done) {
@@ -30,7 +42,7 @@ describe("apidemos - source -", function () {
         source.should.include('@class');
         var obj = JSON.parse(source);
         obj.should.exist;
-        // probably no need for so precise tests 
+        // probably no need for so precise tests
         //obj.hierarchy.node['@class'].should.equal("android.widget.FrameLayout");
         //obj.hierarchy.node.node.node[0].node['@class'].should.equal("android.widget.FrameLayout");
       }).execute("mobile: find", [[[[3, "Animation"]]]])

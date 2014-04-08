672533270796393486217066793ddeca331a9e28
Jonathan Lipps <jlipps@gmail.com>
2014-04-08 12:02:44 -0700

    don't use windowHandles for switching to/from webview

--- test/functional/android/webview-specs.js
+++ test/functional/android/webview-specs.js
@@ -3,7 +3,4 @@
 process.env.DEVICE = process.env.DEVICE || "android";
 var androidWebviewTests = require('../../helpers/android-webview');
 
-describe('android - web_view - contexts -', androidWebviewTests.contexts);
-
-// TODO: remove in Appium 1.0
-describe('android - web_view - windows -', androidWebviewTests.windows);
+describe('android - web_view - ', androidWebviewTests);

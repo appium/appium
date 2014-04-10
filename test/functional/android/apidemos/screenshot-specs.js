0312620d0904ebe4df2814482088188a80a93340
Jonathan Lipps <jlipps@gmail.com>
2014-04-09 18:43:02 -0700

    remove 'mobile: localScreenshot'

--- test/functional/android/apidemos/screenshot-specs.js
+++ test/functional/android/apidemos/screenshot-specs.js
@@ -1,26 +1,12 @@
 "use strict";
 
 var setup = require("../../common/setup-base")
-  , desired = require("./desired")
-  , fs = require('fs');
+  , desired = require("./desired");
 
 describe("apidemos - screenshot -", function () {
   var driver;
   setup(this, desired).then(function (d) { driver = d; });
 
-  //todo: fix that got: Command failed: remote object '/data/local/tmp/screenshot.png' does not exist  
-  it('should get a local screenshot @skip-android-all', function (done) {
-    var localScreenshotFile = '/tmp/test_screenshot_appium.png';
-    if (fs.existsSync(localScreenshotFile)) {
-      fs.unlinkSync(localScreenshotFile);
-    }
-    driver.execute("mobile: localScreenshot", [{file: localScreenshotFile}])
-      .then(function () {
-        var screenshot = fs.readFileSync(localScreenshotFile);
-        screenshot.should.exist;
-        screenshot.length.should.be.above(1000);
-      }).nodeify(done);
-  });
   it('should get an app screenshot', function (done) {
     driver.takeScreenshot()
       .should.eventually.have.length.above(1000)
@@ -33,7 +19,7 @@ describe("apidemos - screenshot -", function () {
         .should.eventually.have.length.above(1000)
       .execute("mobile: find", [[[[3, "Animation"]]]])
         .should.eventually.exist
-      .sleep(5000) // cooldown      
+      .sleep(5000) // cooldown
       .nodeify(done);
   });
 });

1c1e5a756ebde435e79564249122a0e2a6e17c1d
bootstraponline <code@bootstraponline.com>
2014-04-26 17:37:17 -0400

    Run appium_doc_lint on Travis CI

--- ci/script.sh
+++ ci/script.sh
@@ -7,6 +7,9 @@ source ./ci/env
 ./ci/show-env.sh
 
 if [[ $CI_CONFIG == 'unit' ]]; then
+    cd docs
+    appium_doc_lint || exit 1
+    cd -
     npm test
 elif [[ $CI_CONFIG == 'build' ]]; then
     ./reset.sh --hardcore --dev --ios --android --selendroid --gappium --verbose 

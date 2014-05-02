856739f5ca1372c3bd9d897b8e9fb5fca2273a0c
Jonathan Lipps <jlipps@gmail.com>
2014-05-02 10:52:30 -0700

    make sure commit hooks use local versions of grunt/mocha

--- test/pre-commit-hook.sh
+++ test/pre-commit-hook.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 set +e
 echo "Linting..."
-out=$(grunt lint 2>&1)
+grunt=$(npm bin)/grunt
+out=$($grunt lint 2>&1)
 status=$?
 if [ "$status" != "0" ]; then
     echo "$out"
     exit $status
 fi
 echo "Running unit tests..."
-out=$(mocha --recursive test/unit/ 2>&1)
+mocha=$(npm bin)/mocha
+out=$($mocha --recursive test/unit/ 2>&1)
 status=$?
 if [ "$status" != "0" ]; then
     echo "$out"

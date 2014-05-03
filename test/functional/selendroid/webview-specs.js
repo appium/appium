513e541a6cb0418e520eb0eee99bd37de6653e31
Jonathan Lipps <jlipps@gmail.com>
2014-05-02 18:16:27 -0700

    fix selendroid webview tests

--- test/functional/selendroid/webview-specs.js
+++ test/functional/selendroid/webview-specs.js
@@ -4,4 +4,4 @@ process.env.DEVICE = process.env.DEVICE || "selendroid";
 var androidWebviewTests = require('../../helpers/android-webview');
 // if it doesn't work run: adb uninstall io.selendroid.testapp
 
-describe('selendroid - web_view -', androidWebviewTests.windows);
+describe('selendroid - web_view -', androidWebviewTests);

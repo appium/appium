514fe9b01f41503a3284de8eb33a3abd793e2856
sebv <seb.vincent@gmail.com>
2014-05-31 14:14:42 +0800

    updated network connection tests

--- test/functional/selendroid/network-connection-spec.js
+++ test/functional/selendroid/network-connection-spec.js
@@ -9,35 +9,15 @@ describe('selendroid - network-connection -', function () {
     var driver;
     setup(this, desired).then(function (d) { driver = d; });
     it('should toggle the airplane mode', function (done) {
-      // first get the current mode:
-      driver._jsonWireCall({
-        method: 'GET'
-        , relPath: '/network_connection'
-        , cb: function (err, type) {
-            (err === null).should.be.true;
-            var newType = JSON.parse(type).value % 2 + 1;
-            // now set the network connection type
-            driver._jsonWireCall({
-              method: 'POST'
-              , relPath: '/network_connection'
-              , data: { "parameters": {"type": newType} }
-              , cb: function (err2, type2) {
-                (err2 === null).should.be.true;
-                (JSON.parse(type2).value % 2).should.equal(newType % 2);
-                // and really make sure it was set correctly
-                driver._jsonWireCall({
-                  method: 'GET'
-                  , relPath: '/network_connection'
-                  , cb: function (err3, type3) {
-                    (err3 === null).should.be.true;
-                    (JSON.parse(type3).value % 2).should.equal(newType % 2);
-                    done();
-                  }
-                });
-              }
-            });
-          }
-        });
+      driver
+        .getNetworkConnection()
+        .then(function (type) {
+          var newType = (type % 2) + 1;
+          return driver
+            .setNetworkConnection(newType)
+            .getNetworkConnection()
+              .should.become(newType);
+        }).nodeify(done);
     });
   });
 });

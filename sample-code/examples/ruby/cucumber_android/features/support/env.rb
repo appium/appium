a548d0a601f64504925fe4951f98f44f14d154fe
bootstraponline <code@bootstraponline.com>
2014-06-06 17:17:35 -0400

    Add cucumber example for cross platform env.rb

--- sample-code/examples/ruby/cucumber_android/features/support/env.rb
+++ sample-code/examples/ruby/cucumber_android/features/support/env.rb
@@ -24,3 +24,18 @@ end
 
 Before { $driver.start_driver }
 After { $driver.driver_quit }
+
+
+=begin
+# If you wanted one env.rb for both android and iOS, you could use logic similar to this:
+
+world_class = ENV['PLATFORM_NAME'] == 'iOS' ? IosWorld : AndroidWorld
+
+# each world class defines the `caps` method specific to that platform
+Appium::Driver.new world_class.caps
+Appium.promote_appium_methods world_class
+World { world_class.new }
+
+Before { $driver.start_driver }
+After { $driver.driver_quit }
+=end

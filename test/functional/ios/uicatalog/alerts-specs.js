9ccdcdfbaede50acbe1c8cacf842065e1f46082e
sebv <seb.vincent@gmail.com>
2014-06-12 17:38:46 +0800

    alert test fix

--- test/functional/ios/uicatalog/alerts-specs.js
+++ test/functional/ios/uicatalog/alerts-specs.js
@@ -12,12 +12,17 @@ var options = {
   'force new connection': true
 };
 
-//TODO waitForAlert not compatible with sauce, do it another way.
-describe('uicatalog - alerts @skip-ios6 @skip-ci', function () {
+describe('uicatalog - alerts @skip-ios6', function () {
 
   var alertTag = env.IOS7 ? '@label' : '@value';
 
+  var driver;
+  setup(this, desired).then(function (d) { driver = d; });
+
   function waitForAlert() {
+    // The socket.io logic below only works locally.
+    if (env.SAUCE) return new Q();
+
     var deferred = Q.defer();
     var client = io.connect('http://127.0.0.1:' + env.APPIUM_PORT, options);
     client.on('alert', function () {
@@ -27,47 +32,50 @@ describe('uicatalog - alerts @skip-ios6 @skip-ci', function () {
     return deferred.promise;
   }
 
-  describe('alert dialog detection', function () {
-    var driver;
-    setup(this, desired).then(function (d) { driver = d; });
-
-    it('should detect Simple', function (done) {
-      driver
-        .elementByXPath("//UIAStaticText[contains(@label,'Alert Views')]").click()
-        .waitForElementByXPath("//UIAStaticText[contains(" + alertTag + ",'Simple')]", 10000, 1000)
-        .elementByXPath("//UIAStaticText[contains(" + alertTag + ",'Simple')]")
-          .click()
-        .resolve(waitForAlert())
-        .nodeify(done);
-    });
+  afterEach(function (done) {
+    driver.back()
+      .nodeify(done);
   });
-  describe('alert dialog detection', function () {
-    var driver;
-    setup(this, desired).then(function (d) { driver = d; });
 
-    it('should detect Okay', function (done) {
-      driver
-        .elementByXPath("//UIAStaticText[contains(@label,'Alert Views')]").click()
-        .waitForElementByXPath("//UIAStaticText[contains(" + alertTag + ",'Okay')]", 10000, 1000)
-        .elementByXPath("//UIAStaticText[contains(" + alertTag + ",'Okay')]")
-          .click()
-        .resolve(waitForAlert())
-        .nodeify(done);
-    });
+  it('should detect Simple', function (done) {
+    var alertReceived;
+    driver
+      .elementByXPath("//UIAStaticText[contains(@label,'Alert Views')]").click()
+      .waitForElementByXPath("//UIAStaticText[contains(" + alertTag + ",'Simple')]", 10000, 1000)
+      .then(function () { alertReceived = waitForAlert(); })
+      .elementByXPath("//UIAStaticText[contains(" + alertTag + ",'Simple')]")
+        .click()
+      .alertText().should.eventually.include('A Short Title Is Best')
+      .resolve(alertReceived)
+      .dismissAlert()
+      .nodeify(done);
   });
-  describe('alert dialog detection', function () {
-    var driver;
-    setup(this, desired).then(function (d) { driver = d; });
 
-    it('should detect Other', function (done) {
-      driver
-        .elementByXPath("//UIAStaticText[contains(@label,'Alert Views')]").click()
-        //.sleep(60000)
-        .waitForElementByXPath("//UIAStaticText[contains(" + alertTag + ",'Other')]", 10000, 1000)
-        .elementByXPath("//UIAStaticText[contains(" + alertTag + ",'Other')]")
-          .click()
-        .resolve(waitForAlert())
-        .nodeify(done);
-    });
+  it('should detect Okay', function (done) {
+    var alertReceived;
+    driver
+      .elementByXPath("//UIAStaticText[contains(@label,'Alert Views')]").click()
+      .waitForElementByXPath("//UIAStaticText[contains(" + alertTag + ",'Okay')]", 10000, 1000)
+      .then(function () { alertReceived = waitForAlert(); })
+      .elementByXPath("//UIAStaticText[contains(" + alertTag + ",'Okay')]")
+        .click()
+      .alertText().should.eventually.include('A Short Title Is Best')
+      .resolve(alertReceived)
+      .acceptAlert()
+      .nodeify(done);
+  });
+
+  it('should detect Other', function (done) {
+    var alertReceived;
+    driver
+      .elementByXPath("//UIAStaticText[contains(@label,'Alert Views')]").click()
+      .waitForElementByXPath("//UIAStaticText[contains(" + alertTag + ",'Other')]", 10000, 1000)
+      .then(function () { alertReceived = waitForAlert(); })
+      .elementByXPath("//UIAStaticText[contains(" + alertTag + ",'Other')]")
+        .click()
+      .alertText().should.eventually.include('A Short Title Is Best')
+      .resolve(alertReceived)
+      .dismissAlert()
+      .nodeify(done);
   });
 });

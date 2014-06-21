4f8ef84a9eaa11fa811cea6fce00ae6e4a1c4864
sebv <seb.vincent@gmail.com>
2014-06-21 19:26:03 +0800

    test fix

--- test/functional/ios/uicatalog/clear-specs.js
+++ test/functional/ios/uicatalog/clear-specs.js
@@ -12,8 +12,9 @@ describe('uicatalog - clear @skip-ios6', function () {
     it('should be able to hide keyboard with the default strategy', function (done) {
       driver
         .elementByXPath("//UIAStaticText[contains(@name, 'Web View')]").moveTo(10, 10).click()
+        .waitForElementByXPath('//UIANavigationBar[@name="Web View"]', 5000, 500)
         .elementByXPath("//UIATextField").click()
-        .elementByAccessibilityIdOrNull("Go").should.eventually.exist
+        .waitForElementByAccessibilityId("Go", 5000, 500)
         .hideKeyboard()
         .elementByAccessibilityIdOrNull("Go").should.not.eventually.exist
         .back()
@@ -23,8 +24,9 @@ describe('uicatalog - clear @skip-ios6', function () {
     it('should be able to hide keyboard with the tapOutside strategy', function (done) {
       driver
         .elementByXPath("//UIAStaticText[contains(@name, 'Web View')]").moveTo(10, 10).click()
+        .waitForElementByXPath('//UIANavigationBar[@name="Web View"]', 5000, 500)
         .elementByXPath("//UIATextField").click()
-        .elementByAccessibilityIdOrNull("Go").should.eventually.exist
+        .waitForElementByAccessibilityId("Go", 5000, 500)
         .hideKeyboard({strategy: 'tapOutside'})
         .elementByAccessibilityIdOrNull("Go").should.not.eventually.exist
         .back()
@@ -34,8 +36,9 @@ describe('uicatalog - clear @skip-ios6', function () {
     it('should be able to hide keyboard with the tapOut strategy', function (done) {
       driver
         .elementByXPath("//UIAStaticText[contains(@name, 'Web View')]").moveTo(10, 10).click()
+        .waitForElementByXPath('//UIANavigationBar[@name="Web View"]', 5000, 500)
         .elementByXPath("//UIATextField").click()
-        .elementByAccessibilityIdOrNull("Go").should.eventually.exist
+        .waitForElementByAccessibilityId("Go", 5000, 500)
         .hideKeyboard({strategy: 'tapOut'})
         .elementByAccessibilityIdOrNull("Go").should.not.eventually.exist
         .back()

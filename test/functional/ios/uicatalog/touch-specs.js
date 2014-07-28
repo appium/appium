a246a63bf148e9da3b25d720322ce3ccc820a814
Isaac Murchie <isaac@saucelabs.com>
2014-07-28 09:58:35 -0700

    Fix handling of multiple taps

---
+++ test/functional/ios/uicatalog/touch-specs.js
@@ -0,0 +1,71 @@
+"use strict";
+
+var setup = require("../../common/setup-base")
+  , desired = require('./desired')
+  , wd = require("wd")
+  , TouchAction = wd.TouchAction;
+
+describe('uicatalog - touch', function () {
+  var driver;
+  setup(this, desired).then(function (d) { driver = d; });
+
+  it('should tap element with count', function (done) {
+    driver
+      .elementByXPath('//UIAStaticText[contains(@label, \'Steppers\')]')
+      .click()
+      .elementsByAccessibilityId('Increment')
+        .then(function (els) {
+          var action = new TouchAction(driver);
+          action.tap({
+            el: els[0],
+            count: 10
+          });
+          return action.perform();
+        })
+      .elementsByAccessibilityId('10')
+        .should.eventually.have.length(2)
+      .nodeify(done);
+  });
+
+  it('should tap element with offset and count', function (done) {
+    driver
+      .elementByXPath('//UIAStaticText[contains(@label, \'Steppers\')]')
+      .click()
+      .elementsByAccessibilityId('Increment')
+        .then(function (els) {
+          var action = new TouchAction(driver);
+          action.tap({
+            el: els[1],
+            x: 10,
+            y: 10,
+            count: 7
+          });
+          return action.perform();
+        })
+      .elementsByAccessibilityId('7')
+        .should.eventually.have.length(2)
+      .nodeify(done);
+  });
+
+  it('should tap offset with count', function (done) {
+    driver
+      .elementByXPath('//UIAStaticText[contains(@label, \'Steppers\')]')
+      .click()
+      .elementsByAccessibilityId('Increment')
+        .then(function (els) {
+          els[2].getLocation()
+            .then(function (loc) {
+              var action = new TouchAction(driver);
+              action.tap({
+                x: loc.x,
+                y: loc.y,
+                count: 3
+              });
+              return action.perform();
+            });
+        })
+      .elementsByAccessibilityId('3')
+        .should.eventually.have.length(2)
+      .nodeify(done);
+  });
+});

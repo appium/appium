b75beb29690b97b345499d7dc12bf82d3bc00dbb
Isaac Murchie <isaac@saucelabs.com>
2014-08-05 10:56:02 -0700

    Skip keystore tests in ci

--- test/functional/selendroid/keystore-specs.js
+++ test/functional/selendroid/keystore-specs.js
@@ -3,4 +3,4 @@
 process.env.DEVICE = process.env.DEVICE || "selendroid";
 var testBase = require('../common/keystore-base');
 
-describe("selendroid - keystore", testBase);
+describe("selendroid - keystore @skip-ci", testBase);

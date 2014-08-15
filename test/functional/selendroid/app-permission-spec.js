ae7394497c28e349fb0a66247546cb142e7a3367
Moiz Virani <moizjv@gmail.com>
2014-08-15 16:01:51 -0700

    Updating sample-code and adding test for selendroid contactmanager with internet permission

--- test/functional/selendroid/app-permission-spec.js
+++ test/functional/selendroid/app-permission-spec.js
@@ -5,14 +5,15 @@ require('../../helpers/setup-chai');
 var env = require('../../helpers/env')
   , initSession = require('../../helpers/session').initSession
   , _ = require('underscore')
-  , desired = require('./desired');
+  , desired = require('./desired')
+  , path = require('path');
 
 describe('should not launch app without internet permission', function () {
   this.timeout(env.MOCHA_INIT_TIMEOUT);
   var session;
   var name = this.title;
-  it('should not launch app', function (done) {
-    var newDesired = _.defaults({'app': 'sample-code/apps/ContactManager/ContactManager.apk'}, desired);
+  it('should not launch ContactManager app', function (done) {
+    var newDesired = _.defaults({'app': path.resolve(__dirname, '..', '..', '..', 'sample-code', 'apps', 'ContactManager', 'ContactManager.apk')}, desired);
     afterEach(function (done) {
       session
         .tearDown(this.currentTest.state === 'failed')
@@ -24,6 +25,24 @@ describe('should not launch app without internet permission', function () {
         err.data.should.include("INTERNET");
         throw err;
       }).should.be.rejectedWith(/The environment you requested was unavailable./)
+      .nodeify(done);
+  });
+});
+
+
+describe('should launch app with internet permission', function () {
+  this.timeout(env.MOCHA_INIT_TIMEOUT);
+  var session = null
+    , name = this.title;
+  afterEach(function (done) {
+    session
+      .tearDown(this.currentTest.state === 'passed')
+      .nodeify(done);
+  });
+  it('should launch ContactManager-selendroid app', function (done) {
+    var newDesired = _.defaults({'app': path.resolve(__dirname, '..', '..', '..', 'sample-code', 'apps', 'ContactManager', 'ContactManager-selendroid.apk')}, desired);
+    session = initSession(newDesired);
+    session.setUp(name)
       .nodeify(done);
   });
 });

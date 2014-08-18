b57e802fbc5e6b430e0c08efe32733e6aec36a65
Moiz Virani <moizjv@gmail.com>
2014-08-18 12:31:23 -0700

    Changing helper method to get application path dynamically

--- test/helpers/app.js
+++ test/helpers/app.js
@@ -1,15 +1,16 @@
 "use strict";
 
-var env = require('./env');
+var env = require('./env')
+  , path = require('path');
 
 module.exports.getAppPath = function (app) {
   if (env.IOS) {
-    return "sample-code/apps/" + app +
-      "/build/Release-iphonesimulator/" + app + ".app";
+    return path.resolve(__dirname, '..', '..', 'sample-code', 'apps', app,
+                        'build', 'Release-iphonesimulator', app + '.app');
   }
   if (env.ANDROID || env.SELENDROID) {
-    return "sample-code/apps/" + app +
-      "/bin/" + app + "-debug.apk";
+    return path.resolve(__dirname, '..', '..', 'sample-code', 'apps', app,
+                        'bin', app + '-debug.apk');
   }
   return app;
 };

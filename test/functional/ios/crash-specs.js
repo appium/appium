3c6a25b5ae0bf8c51146bb5b9919f709cebb7646
Jonathan Lipps <jlipps@gmail.com>
2014-08-29 14:05:39 -0700

    fix async bug in ios crash waterfall previously, we'd respond to the client's current callback and then start the shutdown sequence. this opened up a situation where the client could then call another command (say driver.quit) during shutdown, enabling the possibility of two simultaneous shutdown flows causing undefined behavior. this fix makes sure we don't respond to the client for a command that caused a crash until we're done shutting down.

--- test/functional/ios/crash-specs.js
+++ test/functional/ios/crash-specs.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var setup = require("../common/setup-base")
+  , _ = require("underscore")
   , getAppPath = require('../../helpers/app').getAppPath;
 
 describe('crash recovery', function () {
@@ -32,6 +33,30 @@ describe('crash recovery', function () {
       })
       .source() // will 404 because the session is gone
         .should.eventually.be.rejectedWith('6')
+    .nodeify(done);
+  });
+});
+
+describe('crash commands', function () {
+
+  var driver;
+  var desired = {
+    app: getAppPath('TestApp')
+  };
+
+  setup(this, desired, {}, {FAST_TESTS: false}).then(function (d) { driver = d; });
+
+  it('should not process new commands until after crash shutdown', function (done) {
+    driver
+      .execute("$.crash()") // this causes instruments to shutdown during
+                            // this command
+        .should.eventually.be.rejectedWith('13')
+      .status()
+      .then(function (s) {
+        if (_.has(s, 'isShuttingDown')) {
+          s.isShuttingDown.should.eql(false);
+        }
+      })
     .nodeify(done);
   });
 });

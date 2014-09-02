99c2d80dbf124e5a66d018329751a3012da703c3
Jonah Stiennon <jonahss@gmail.com>
2014-09-02 13:45:34 -0700

    tests run

--- test/functional/common/settings-specs.js
+++ test/functional/common/settings-specs.js
@@ -1,24 +1,30 @@
 "use strict";
 
-var setup = require("../setup-base");
+var setup = require("./setup-base")
+    , env = require('../../helpers/env')
+    , getAppPath = require('../../helpers/app').getAppPath;
 
-module.exports = function (desired) {
-
-  describe('settings', function () {
-    var driver;
-    setup(this, desired, {'no-reset': true}).then(function (d) { driver = d; });
-
-    it('should return a settings object even if none specified', function (done) {
-      driver
-        .settings().should.eventually.exist
-        .nodeify(done);
-    });
-
-    it('should be able to store a setting', function (done) {
-      driver
-        .settings({'settlers of': 'Catan'})
-        .settings().should.eventually.have.property('settlers of')
-        .nodeify(done);
-    });
-  });
+var desired = {
+  app: getAppPath('testapp')
 };
+if (env.SELENDROID) {
+  desired.automationName = 'selendroid';
+}
+
+describe('settings', function () {
+  var driver;
+  setup(this, desired, {'no-reset': true}).then(function (d) { driver = d; });
+
+  it('should return a settings object even if none specified', function (done) {
+    driver
+      .settings().should.eventually.exist()
+      .nodeify(done);
+  });
+
+  it('should be able to store a setting', function (done) {
+    driver
+      .settings({'settlers of': 'Catan'})
+      .settings().should.eventually.have.property('settlers of')
+      .nodeify(done);
+  });
+});

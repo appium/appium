13d56ef30264a6fb60daefb929c71cb8f0c2f3b2
sebv <seb.vincent@gmail.com>
2014-10-22 19:57:47 +0800

    fixed android lock tests

--- test/functional/android/apidemos/lock-specs.js
+++ test/functional/android/apidemos/lock-specs.js
@@ -1,36 +1,48 @@
 "use strict";
 
 var setup = require("../../common/setup-base")
-    , desired = require("./desired");
+    , desired = require("./desired")
+    , Asserter = require('wd').Asserter
+    , chai = require('chai');
 
 describe("apidemos - lock", function () {
   var driver;
   setup(this, desired).then(function (d) { driver = d; });
 
+  var isLockedAsserter = function (opts) {
+    return new Asserter(
+      function (driver) {
+        return driver
+          .isLocked()
+          .then(function (isLocked) {
+            isLocked.should.equal(opts.expected);
+            return isLocked;
+          })
+          .catch(function (err) {
+            err.retriable = err instanceof chai.AssertionError;
+            throw err;
+          });
+      }
+    );
+  };
+
   it('should lock the screen', function (done) {
     driver
-        .lockDevice(3)
-        .nodeify(done);
-  });
-  it('should know it\'s locked', function (done) {
-    driver
-        .lockDevice(3)
-        .isLocked()
-        .should.eventually.equal(true)
-        .nodeify(done);
-  });
+      .isLocked()
+      .should.not.eventually.be.ok
+      .lockDevice(3)
+      .waitFor(isLockedAsserter({expected: true}), 5000, 500)
+      .should.eventually.be.ok
+      .nodeify(done);
+      });
   it('should unlock the screen', function (done) {
     driver
-        .lockDevice(3)
-        .unlockDevice()
-        .nodeify(done);
-  });
-  it('should know it\'s unlocked', function (done) {
-    driver
-        .lockDevice(3)
-        .unlockDevice()
-        .isLocked()
-        .should.eventually.equal(false)
-        .nodeify(done);
-  });
+      .lockDevice(3)
+      .waitFor(isLockedAsserter({expected: true}), 5000, 500)
+      .should.eventually.be.ok
+      .unlockDevice()
+      .waitFor(isLockedAsserter({expected: false}), 5000, 500)
+      .should.not.eventually.be.ok
+      .nodeify(done);
+    });
 });

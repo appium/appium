c1118c60ca9d560d97243d9a557612194bce4ebb
Jonah Stiennon <jonahss@gmail.com>
2014-10-27 12:24:38 -0700

    cleaned up gps test

--- test/functional/android/gpsdemos/location-specs.js
+++ test/functional/android/gpsdemos/location-specs.js
@@ -3,16 +3,12 @@
 var setup = require("../../common/setup-base")
   , desired = require("./desired");
 
-var desired = {
-  app: 'sample-code/apps/gps-demo/bin/GPSTutorial1.apk'
-};
-
-// TODO: bring back when new wd is published with setGeoLocation
-describe("apidemo - location @skip-real-device", function () {
+describe("gpsDemo - location @skip-real-device", function () {
   var driver;
   setup(this, desired).then(function (d) { driver = d; });
 
   it('should set geo location', function (done) {
+
     var getText = function () {
       return driver
         .elementsByClassName("android.widget.TextView")
@@ -20,18 +16,24 @@ describe("apidemo - location @skip-real-device", function () {
           return els[1].text();
         });
     };
+
+
     var newLat = "27.17";
     var newLong = "78.04";
+
     driver
-      .resolve(getText()).then(function (text) {
+      .resolve(getText())
+      .then(function (text) {
         text.should.not.include("Latitude: " + newLat);
         text.should.not.include("Longitude: " + newLong);
       })
       .setGeoLocation(newLat, newLong)
-      .sleep(1000).then(getText).then(function (text) {
+      .sleep(1000)
+      .then(getText)
+      .then(function (text) {
         text.should.include("Latitude: " + newLat.substr(0, 4));
         text.should.include("Longitude: " + newLong.substr(0, 4));
-      }).nodeify(done);
+      })
+      .nodeify(done);
   });
 });
-

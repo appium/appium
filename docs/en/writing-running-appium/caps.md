690536f2d8481dcc66823ae5feae85321ee80c45
sebv <seb.vincent@gmail.com>
2014-10-29 11:48:20 +0800

    added sendKeyStrategy cap

--- docs/en/writing-running-appium/caps.md
+++ docs/en/writing-running-appium/caps.md
@@ -78,3 +78,4 @@
 |`processArguments`| Arguments to pass to the AUT using instruments|e.g., `-myflag`|
 |`interKeyDelay`| The delay, in ms, between keystrokes sent to an element when typing.|e.g., `100`|
 |`showIOSLog`| Whether to show any logs captured from a device in the appium logs. Default `false`|`true` or `false`|
+|`sendKeyStrategy`| strategy to use to type test into a test field. Simulator default: `oneByOne`. Real device default: 'grouped' |`oneByOne`, `grouped` or setValue|
--- test/functional/ios/testapp/keyboard-specs.js
+++ test/functional/ios/testapp/keyboard-specs.js
@@ -4,28 +4,68 @@ var setup = require("../../common/setup-base")
  ,  desired = require('./desired')
  ,  _ = require('underscore');
 
+describe('testapp', function () {
+  var ctx = this;
 
-describe("testapp - keyboard stability @skip-ci", function () {
-  var runs = 10
-    , text = 'Delhi is New @@@ BREAKFAST-FOOD-0001';
+  var test = function (strategy) {
+    describe('typing with strategy:' + (strategy || 'undefined'), function () {
+      var text = 'Appium Rocks';
 
-  var driver;
-  setup(this, _.defaults({
-    deviceName: 'iPad 2',
-  }, desired)).then(function (d) { driver = d; });
+      var driver;
+      var _desired = _.clone(desired);
+      if (strategy) _desired.sendKeyStrategy = strategy;
+      setup(ctx, _desired).then(function (d) { driver = d; });
 
-  var test = function () {
-    it("should send keys to a text field", function (done) {
-      driver
-        .elementByClassName('UIATextField')
-          .clear()
-          .sendKeys(text)
-          .text().should.become(text)
-        .nodeify(done);
+      it("should send keys to a text field", function (done) {
+        driver
+          .execute('env')
+          .then( function (env) {
+            if (strategy) env.sendKeyStrategy.should.equal(strategy);
+            else env.sendKeyStrategy.should.equal('oneByOne');
+          })
+          .elementByClassName('UIATextField')
+            .clear()
+            .sendKeys(text)
+            .text().then(function (text) {
+              if (strategy === 'grouped') {
+                text.length.should.be.above(0);
+              } else {
+                text.should.equal(text);
+              }
+            }).nodeify(done);
+      });
     });
   };
 
-  for (var n = 0; n < runs; n++) {
-    describe('sendKeys test ' + (n + 1), test);
-  }
+  test();
+  test('oneByOne');
+  test('grouped');
+  test('setValue');
+
+  describe("keyboard stability @skip-ci", function () {
+    var runs = 10
+      , text = 'Delhi is New @@@ BREAKFAST-FOOD-0001';
+
+    var driver;
+    setup(this, _.defaults({
+      deviceName: 'iPad 2',
+    }, desired)).then(function (d) { driver = d; });
+
+    var test = function () {
+      it("should send keys to a text field", function (done) {
+        driver
+          .elementByClassName('UIATextField')
+            .clear()
+            .sendKeys(text)
+            .text().should.become(text)
+          .nodeify(done);
+      });
+    };
+
+    for (var n = 0; n < runs; n++) {
+      describe('sendKeys test ' + (n + 1), test);
+    }
+  });
+
+
 });

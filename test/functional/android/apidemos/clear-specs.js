1daaf4d542bea900df38c1972c5ae3a7c9c9f9f0
sebv <seb.vincent@gmail.com>
2014-11-05 17:33:27 +0800

    added hideKeyboard Android tests

---
+++ test/functional/android/apidemos/clear-specs.js
@@ -0,0 +1,104 @@
+"use strict";
+
+var setup = require("../../common/setup-base")
+    , desired = require("./desired")
+    , _ = require('underscore')
+    , getAppPath = require('../../../helpers/app').getAppPath;
+
+
+describe("apidemos - clear", function () {
+  var driver;
+  var _desired = _.defaults({
+    app: getAppPath('ApiDemos'),
+    appActivity: '.view.Controls1',
+    newCommandTimeout: 90,
+    language: 'en',
+    locale: 'en_US'
+  }, desired);
+  setup(this, _desired).then(function (d) { driver = d; });
+
+  it('should hide the keyboard using the default strategy', function (done) {
+    driver
+      .waitForElementByClassName('android.widget.EditText')
+      .click()
+      .sleep(1000)
+      .hideKeyboard()
+      .sleep(1000)
+      .elementByClassName('android.widget.EditText')
+      .should.eventually.exist
+      .nodeify(done);
+  });
+
+  it('should hide the keyboard using the "Done" key', function (done) {
+    driver
+      .waitForElementByClassName('android.widget.EditText')
+      .click()
+      .sleep(1000)
+      .hideKeyboard('Done')
+      .sleep(1000)
+      .elementByClassName('android.widget.EditText')
+      .should.eventually.exist
+      .nodeify(done);
+  });
+
+  it('should hide the keyboard using the "press" strategy and "Done" key', function (done) {
+    driver
+      .waitForElementByClassName('android.widget.EditText')
+      .click()
+      .sleep(1000)
+      .hideKeyboard({strategy:'press', key: 'Done'})
+      .sleep(1000)
+      .elementByClassName('android.widget.EditText')
+      .should.eventually.exist
+      .nodeify(done);
+  });
+
+  it('should hide the keyboard using the "pressKey" strategy and "Done" key', function (done) {
+    driver
+      .waitForElementByClassName('android.widget.EditText')
+      .click()
+      .sleep(1000)
+      .hideKeyboard({strategy:'pressKey', key: 'Done'})
+      .sleep(1000)
+      .elementByClassName('android.widget.EditText')
+      .should.eventually.exist
+      .nodeify(done);
+  });
+
+  it('should hide the keyboard using the "pressKey" strategy and "Done" key', function (done) {
+    driver
+      .waitForElementByClassName('android.widget.EditText')
+      .click()
+      .sleep(1000)
+      .hideKeyboard({strategy:'swipeDown'})
+      .sleep(1000)
+      .elementByClassName('android.widget.EditText')
+      .should.eventually.exist
+      .nodeify(done);
+  });
+
+  it('should hide the keyboard using the "tapOutside" strategy', function (done) {
+    driver
+      .waitForElementByClassName('android.widget.EditText')
+      .click()
+      .sleep(1000)
+      .hideKeyboard({strategy:'tapOutside'})
+      .sleep(1000)
+      .elementByClassName('android.widget.EditText')
+      .should.eventually.exist
+      .nodeify(done);
+  });
+
+  it('should hide the keyboard using the "tapOut" strategy', function (done) {
+    driver
+      .waitForElementByClassName('android.widget.EditText')
+      .click()
+      .sleep(1000)
+      .hideKeyboard({strategy:'tapOut'})
+      .sleep(1000)
+      .elementByClassName('android.widget.EditText')
+      .should.eventually.exist
+      .nodeify(done);
+  });
+
+});

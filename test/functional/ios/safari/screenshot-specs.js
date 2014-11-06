b7542d3f97520e61aec8ff30ca9d36dab9bf6290
sebv <seb.vincent@gmail.com>
2014-11-06 18:08:47 +0800

    added screenshotWaitTimeout cap

--- test/functional/ios/safari/screenshot-specs.js
+++ test/functional/ios/safari/screenshot-specs.js
@@ -3,27 +3,43 @@
 var setup = require("../../common/setup-base");
 
 describe('safari - screenshots @skip-ios6', function () {
-  var driver;
-  setup(this, {browserName: 'safari'}).then(function (d) { driver = d; });
+  describe('default' ,function () {
+    var driver;
+    setup(this, {browserName: 'safari'}).then(function (d) { driver = d; });
 
-  it('should get an app screenshot', function (done) {
-    driver
-      .takeScreenshot()
-        .should.eventually.exist
+    it('should get an app screenshot', function (done) {
+      driver
+        .takeScreenshot()
+          .should.eventually.exist
+        .nodeify(done);
+    });
+    it('should get an app screenshot in landscape mode', function (done) {
+      driver.takeScreenshot().then(function (screenshot1) {
+        screenshot1.should.exist;
+        return driver
+          .setOrientation("LANDSCAPE")
+          // A useless error does often exist here, let's ignore it
+          .catch(function () {})
+          .takeScreenshot().then(function (screenshot2) {
+            screenshot2.should.exist;
+            screenshot2.should.not.eql(screenshot1);
+          });
+      }).sleep(3000) // cooldown
       .nodeify(done);
+    });
   });
-  it('should get an app screenshot in landscape mode', function (done) {
-    driver.takeScreenshot().then(function (screenshot1) {
-      screenshot1.should.exist;
-      return driver
-        .setOrientation("LANDSCAPE")
-        // A useless error does often exist here, let's ignore it
-        .catch(function () {})
-        .takeScreenshot().then(function (screenshot2) {
-          screenshot2.should.exist;
-          screenshot2.should.not.eql(screenshot1);
-        });
-    }).sleep(3000) // cooldown
-    .nodeify(done);
+
+  describe('setting screenshotWaitTimeout' ,function () {
+    var driver;
+    setup(this, {browserName: 'safari', screenshotWaitTimeout: 5})
+      .then(function (d) { driver = d; });
+
+    it('should get an app screenshot', function (done) {
+      driver
+        .takeScreenshot()
+          .should.eventually.exist
+        .nodeify(done);
+    });
   });
+
 });

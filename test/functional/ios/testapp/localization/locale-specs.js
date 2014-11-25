b3e8f97077e173eb149b9b804b16043f1dde2041
Isaac Murchie <isaac@saucelabs.com>
2014-11-24 16:32:29 -0800

    Fix getting sim dirs

--- test/functional/ios/testapp/localization/locale-specs.js
+++ test/functional/ios/testapp/localization/locale-specs.js
@@ -7,7 +7,7 @@ var env = require('../../../../helpers/env'),
     rimraf = require('rimraf'),
     path = require('path');
 
-describe('localization - locale @skip-ios6', function () {
+describe('localization - locale', function () {
   this.timeout(env.MOCHA_INIT_TIMEOUT);
 
   after(function () {

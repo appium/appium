a3a903893bc5b2b490a2c6b20623432941db991f
Jonathan Lipps <jlipps@gmail.com>
2014-12-01 14:21:25 -0800

    make sure our 'chromeOptions' overriding works for Chrome and not just Chromeviews (fix #4119)

--- test/functional/android/chrome/desired.js
+++ test/functional/android/chrome/desired.js
@@ -1,4 +1,5 @@
 module.exports = {
   browserName: process.env.BROWSER_NAME || 'chrome',
-  enablePerformanceLogging: true
+  enablePerformanceLogging: true,
+  chromeOptions: {args: ['--disable-translate']}
 };

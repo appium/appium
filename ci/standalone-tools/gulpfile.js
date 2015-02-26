a133d53a3fcf288c2538a4fe98fb532715a98182
sebv <seb.vincent@gmail.com>
2015-02-27 01:32:19 +0800

    keeps processing tap results on error.

--- ci/standalone-tools/gulpfile.js
+++ ci/standalone-tools/gulpfile.js
@@ -29,15 +29,22 @@ gulp.task('collect-downstream-tap-results', function () {
   var jobNameRaw = process.env.LAST_TRIGGERED_JOB_NAME;
   var jobName = jobNameRaw.replace(/_/g,' ');
   var builds = process.env['TRIGGERED_BUILD_NUMBERS_' + jobNameRaw].split(',');
+  var ok = true;
   var seq = _(builds).map(function (build) {
     return function () {
       var tapTgz = 'tapdata_' + build + '.tgz';
       return downloadS3Artifact(jobName, build, tapTgz).then(function () {
         return exec('tar xfz ' + tapTgz);
+      }).catch(function (err) {
+        console.error('error while retrieving ' + tapTgz + 'error: ' + err);
+        ok = false;
       });
     };
   });
-  return seq.reduce(Q.when, new Q());
+  return seq.reduce(Q.when, new Q())
+    .then(function () {
+      if (!ok) throw new Error('Tap file retrieval failed.');
+    });
 });
 
 gulp.task('download-build', function () {

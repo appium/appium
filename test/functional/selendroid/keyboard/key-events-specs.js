29dfe1f6f8221645fdb969b682c4f7c870a201c3
sebv <seb.vincent@gmail.com>
2015-05-14 16:39:42 +0800

    text fix

--- test/functional/selendroid/keyboard/key-events-specs.js
+++ test/functional/selendroid/keyboard/key-events-specs.js
@@ -1,5 +1,5 @@
 "use strict";
 
-var keyEventsTests = require('../common/android-keyboard-base').keyEventsTests;
+var keyEventsTests = require('../../common/android-keyboard-base').keyEventsTests;
 
 describe('selendroid - keyboard', keyEventsTests);

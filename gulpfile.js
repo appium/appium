03ae036a9f26e958d645658af44f1d3525237482
sebv <seb.vincent@gmail.com>
2015-05-21 02:38:44 -0700

    Configuring travis

--- gulpfile.js
+++ gulpfile.js
@@ -3,4 +3,8 @@
 var gulp = require('gulp'),
     boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
 
-boilerplate({build: 'appium-base-driver', jscs: false});
+boilerplate({
+  build: 'appium-base-driver',
+  jscs: false,
+  testReporter: process.env.TRAVIS ? 'spec' : 'nyan'
+});

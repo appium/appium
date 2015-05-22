3da51d05cf6aa8a29e11ed94b0495d1d510dabf2
Jonathan Lipps <jlipps@gmail.com>
2015-05-21 20:50:36 -0700

    move all old code out of git

--- lib/devices/android/bootstrap/src/io/appium/android/bootstrap/exceptions/PairCreationException.java
+++
@@ -1,8 +0,0 @@
-package io.appium.android.bootstrap.exceptions;
-
-/**
- * For trying to create a ClassInstancePair and something goes wrong.
- */
-public class PairCreationException extends Throwable {
-  public PairCreationException(String msg) { super(msg); }
-}

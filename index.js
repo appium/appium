2e57ce2d5262b8d75c4ee2d1f6ef4f923742a6c7
Jonathan Lipps <jlipps@gmail.com>
2015-05-29 14:54:42 -0700

    use better test exporting architecture

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // transpile:main
 
 import { BaseDriver } from './lib/driver';
-import { baseDriverUnitTests } from './test/driver-specs';
-import { baseDriverE2ETests } from './test/driver-e2e-specs';
+import baseDriverUnitTests from './test/driver-specs';
+import baseDriverE2ETests from './test/driver-e2e-specs';
 export { BaseDriver, baseDriverUnitTests, baseDriverE2ETests };
 

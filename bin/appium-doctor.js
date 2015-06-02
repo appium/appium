eeb4e67537012ea37f161461a5673bf09a0802e1
Jonathan Lipps <jlipps@gmail.com>
2015-06-02 19:10:37 +0100

    update all deps, remove unused deps, stub out binfiles, and add valid license

---
+++ bin/appium-doctor.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+"use strict";
+
+throw new Error("Appium doctor has not been ported yet");
---
+++ bin/authorize-ios.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+"use strict";
+
+throw new Error("iOS Authorization has not been ported yet");

9abacc61beb3546e1f9adfeb0c453a153d3cd4e2
sebv <seb.vincent@gmail.com>
2015-06-05 17:11:57 +0800

    using defaults in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,9 @@
 "use strict";
 
 var gulp = require('gulp'),
-    boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp),
-    DEFAULTS = require('appium-gulp-plugins').boilerplate.DEFAULTS,
-    _ = require('lodash');
+    boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
 
 boilerplate({
   build: 'appium-base-driver',
-  jscs: false,
-  testReporter: process.env.TRAVIS ? 'spec' : 'nyan',
-  coverage: _.defaults({verbose: true}, DEFAULTS.coverage)
+  jscs: false
 });

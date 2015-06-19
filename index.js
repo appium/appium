2041727ee53a165425e004302c58f677cf39a546
Jonathan Lipps <jlipps@gmail.com>
2015-06-19 10:17:53 -0700

    export tests from tests/index.js, not /index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // transpile:main
 
 import { BaseDriver } from './lib/driver';
-import baseDriverUnitTests from './test/driver-tests';
-import baseDriverE2ETests from './test/driver-e2e-tests';
-export { BaseDriver, baseDriverUnitTests, baseDriverE2ETests };
+export { BaseDriver };
 
---
+++ test/index.js
@@ -0,0 +1,6 @@
+// transpile:main
+
+import baseDriverUnitTests from './test/driver-tests';
+import baseDriverE2ETests from './test/driver-e2e-tests';
+
+export { baseDriverUnitTests, baseDriverE2ETests };

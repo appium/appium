742831107ed77b4e3a7ccf69b58881c454675b7f
Jonathan Lipps <jlipps@gmail.com>
2015-08-25 20:09:39 -0700

    add isPackageOrBundle helper function

---
+++ test/helpers-specs.js
@@ -0,0 +1,16 @@
+import { isPackageOrBundle } from '../lib/helpers';
+import chai from 'chai';
+chai.should();
+
+describe('helpers', () => {
+  describe('#isPackageOrBundle', () => {
+    it('should accept packages and bundles', () => {
+      isPackageOrBundle('io.appium.testapp').should.be.true;
+    });
+    it('should not accept non-packages or non-bundles', () => {
+      isPackageOrBundle('foo').should.be.false;
+      isPackageOrBundle('/path/to/an.app').should.be.false;
+      isPackageOrBundle('/path/to/an.apk').should.be.false;
+    });
+  });
+});

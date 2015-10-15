58ee94564e3646d24d4e51234e119885f325bd73
Scott Dixon <scott@howrefreshing.com.au>
2015-10-15 09:08:38 -0700

    appium-doctor CLI link

--- bin/appium-doctor.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-'use strict';
-
-// TODO: figure out how to use appium doctor now
-// var doctor = require('appium-doctor');
-
-throw new Error("Appium doctor has not been ported yet");

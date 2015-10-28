0643e6f12f93705d97bc07e63ad7723e7f67351b
moizjv <moizjv@gmail.com>
2015-10-28 08:34:49 -0400

    Fixing typo

--- bin/xcode_iwd.sh
+++ bin/xcode_iwd.sh
@@ -1,6 +1,5 @@
 plist_path=$1/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/Developer/Library/LaunchDaemons/com.apple.instruments.deviceservice.plist
-iwd_path=$2/node_modules/appium_instruments/thridparty/iwd7
+iwd_path=$2/node_modules/appium-instruments/thirdparty/iwd7
 /usr/libexec/PlistBuddy -c "Add :EnvironmentVariables dict" $plist_path
 /usr/libexec/PlistBuddy -c "Add :EnvironmentVariables:DYLD_INSERT_LIBRARIES string $iwd_path/DTMobileISShim.dylib" $plist_path
 /usr/libexec/PlistBuddy -c "Add :EnvironmentVariables:LIB_PATH string $iwd_path/" $plist_path
-

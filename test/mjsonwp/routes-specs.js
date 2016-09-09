d4e5b455447af4c9266595c0e8d01685eafa2073
Wen Fan <wfan@salesforce.com>
2016-09-08 17:17:20 -0700

    adding new endpoint

--- test/mjsonwp/routes-specs.js
+++ test/mjsonwp/routes-specs.js
@@ -38,7 +38,7 @@ describe('MJSONWP', () => {
       }
       var hash = shasum.digest('hex').substring(0, 8);
       // Modify the hash whenever the protocol has intentionally been modified.
-      hash.should.equal('4ea1ffc6');
+      hash.should.equal('14adc751');
     });
   });
 

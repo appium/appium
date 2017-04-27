0bf5acbd521909d9fcd839b6fe1da39f10d3e8ae
Dan Graham <dan@saucelabs.com>
2017-04-26 17:55:46 -0700

    Removed adm-zip (#118)
    * Added a test for unzipFile (which uses adm-zip)
    * Tested that it worked with adm-zip and then failed once adm-zip was taken out
    * Replaced 'adm-zip' with zip.extractAllTo from appium-supprt

--- test/basedriver/helpers-specs.js
+++ test/basedriver/helpers-specs.js
@@ -1,6 +1,10 @@
-import { isPackageOrBundle } from '../../lib/basedriver/helpers';
+import { isPackageOrBundle, unzipFile } from '../../lib/basedriver/helpers';
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
+import path from 'path';
+import sinon from 'sinon';
+import { system, fs } from 'appium-support';
+import mockFS from 'mock-fs';
 
 chai.use(chaiAsPromised);
 
@@ -13,6 +17,32 @@ describe('helpers', () => {
       isPackageOrBundle('foo').should.be.false;
       isPackageOrBundle('/path/to/an.app').should.be.false;
       isPackageOrBundle('/path/to/an.apk').should.be.false;
+    });
+  });
+
+  describe('#unzipFile', () => {
+    let mockDir = 'path/to/mock/dir';
+    before(async () => {
+      // Start mock filesystem
+      mockFS({
+        [mockDir]: {},
+      });
+
+      // Write the base64 contents of FakeIOSApp.app.zip to the mock filesystem
+      
+      const fakeIOSAppZip = 'UEsDBBQACAAIABF8/EYAAAAAAAAAAAAAAAAOABAARmFrZUlPU0FwcC5hcHBVWAwALwO4VQIDuFX1ARQAK8nILFYAorz8EoWi1MScnEqFxDyFxIICLgBQSwcIR93jPhoAAAAaAAAAUEsBAhUDFAAIAAgAEXz8Rkfd4z4aAAAAGgAAAA4ADAAAAAAAAAAAQKSBAAAAAEZha2VJT1NBcHAuYXBwVVgIAC8DuFUCA7hVUEsFBgAAAAABAAEASAAAAGYAAAAAAA==';
+      await fs.writeFile(path.resolve(mockDir, 'FakeIOSApp.app.zip'), fakeIOSAppZip, 'base64');
+    });
+
+    after(() => {
+      mockFS.restore();
+    });
+
+    it('should unzip a .zip file (force isWindows to be true so we can test the internal zip library)', async () => {
+      const forceWindows = sinon.stub(system, 'isWindows', () => true);
+      await unzipFile(path.resolve(mockDir, 'FakeIOSApp.app.zip'));
+      await fs.readFile(path.resolve(mockDir, 'FakeIOSApp.app'), 'utf8').should.eventually.deep.equal('this is not really an app\n');
+      forceWindows.restore();
     });
   });
 });

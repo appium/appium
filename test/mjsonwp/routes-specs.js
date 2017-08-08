8cd54941a392773b34549803148647e2005196e9
Bruno Alassia <vruno.alassia@gmail.com>
2017-08-08 22:47:25 +0200

    Add new endpoints (#129)
    * add routes for new emulator methods
    
    * add networkSpeed method
    
    * update hash

--- test/mjsonwp/routes-specs.js
+++ test/mjsonwp/routes-specs.js
@@ -38,7 +38,7 @@ describe('MJSONWP', () => {
       }
       var hash = shasum.digest('hex').substring(0, 8);
       // Modify the hash whenever the protocol has intentionally been modified.
-      hash.should.equal('354f9c79');
+      hash.should.equal('f5740d96');
     });
   });
 

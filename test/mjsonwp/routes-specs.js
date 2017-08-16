8481b9ac7cfa89563e86bc9f53e1b1d0127f6d6b
Mykola Mokhnach <nick+1@wire.com>
2017-08-16 20:35:52 +0200

    Add 'enabled' parameter to toggle_touch_id_enrollment endpoint (#130)
    * Add 'enabled' parameter to toggle_touch_id_enrollment endpoint
    
    * Update the hash

--- test/mjsonwp/routes-specs.js
+++ test/mjsonwp/routes-specs.js
@@ -40,7 +40,7 @@ describe('MJSONWP', () => {
       }
       let hash = shasum.digest('hex').substring(0, 8);
       // Modify the hash whenever the protocol has intentionally been modified.
-      hash.should.equal('f5740d96');
+      hash.should.equal('65a63512');
     });
   });
 

1a91bec967b68a82db3e454a873cf84102c56faa
Mykola Mokhnach <nick+1@wire.com>
2017-12-15 22:03:06 +0100

    Support pre-W3C element screenshot endpoint (#154)

--- test/mjsonwp/routes-specs.js
+++ test/mjsonwp/routes-specs.js
@@ -40,7 +40,7 @@ describe('MJSONWP', () => {
       }
       let hash = shasum.digest('hex').substring(0, 8);
       // Modify the hash whenever the protocol has intentionally been modified.
-      hash.should.equal('346c41c1');
+      hash.should.equal('a991dc46');
     });
   });
 

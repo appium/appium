f4b32d392693942bb92b7ccb2dbba52e6f31b06b
dan@saucelabs.com <dan@saucelabs.com>
2018-01-02 13:16:11 -0800

    Add apps; add create session test

---
+++ sample-code/apps/ContactManager/README.md
@@ -0,0 +1,4 @@
+# ContactManager
+
+ContactManager.apk only runs with UIAutomator and not with seledroid due to absence of
+INTERNET permission to use ContactManager with selendroid use ContactManger-selendroid.apk
---
+++ sample-code/apps/WebViewApp/WebViewApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WebViewApp
+//
+//  Created by Jonathan Lipps on 2/5/13.
+//  Copyright (c) 2013 Appium Committers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

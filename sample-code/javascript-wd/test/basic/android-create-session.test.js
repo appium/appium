e4cdf34d3645ffeae5ec2bc09b1fc67d7afdaeb3
dan@saucelabs.com <dan@saucelabs.com>
2018-01-02 13:26:55 -0800

    Android create session tests

--- sample-code/javascript-wd/test/basic/android-create-session.test.js
+++ sample-code/javascript-wd/test/basic/android-create-session.test.js
@@ -1,10 +1,10 @@
 import wd from 'wd';
 import chai from 'chai';
-import { iosCaps, serverConfig } from '../helpers/caps';
+import { iosCaps, androidCaps, serverConfig } from '../helpers/caps';
 
 const {assert} = chai;
 
-describe('Create session', function () {
+describe('Create Android session', function () {
   it('should create and destroy IOS sessions', async function () {
     // Connect to Appium server
     const driver = await wd.promiseChainRemote(serverConfig);
@@ -14,8 +14,8 @@ describe('Create session', function () {
 
     // Start the session
     await driver.init({
-      ...iosCaps,
-      app: require('../helpers/apps').iosTestApp
+      ...androidCaps,
+      app: require('../helpers/apps').androidApiDemos
     });
 
     // Now that session is running, check that 'sessions' length is one
--- sample-code/javascript-wd/test/helpers/caps.js
+++ sample-code/javascript-wd/test/helpers/caps.js
@@ -2,8 +2,16 @@ import path from 'path';
 
 const iosCaps = {
   platformName: 'iOS',
+  automationName: 'XCUITest',
   deviceName: 'iPhone SE' || process.env.IOS_DEVICE_NAME,
   platformVersion: '11.2' || process.env.IOS_PLATFORM_VERSION,
+  app: undefined, // Will be added in tests
+};
+
+const androidCaps = {
+  platformName: 'Android',
+  automationName: 'UiAutomator2',
+  deviceName: 'Android' || process.env.ANDROID_DEVICE_NAME,
   app: undefined, // Will be added in tests
 };
 
@@ -12,4 +20,4 @@ const serverConfig = {
   port: process.env.APPIUM_PORT || 4723
 };
 
-export { iosCaps, serverConfig };
+export { iosCaps, androidCaps, serverConfig };

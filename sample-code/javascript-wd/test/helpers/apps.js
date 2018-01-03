fe736e036bb50ca146219f0cdd27a5ba0a239be5
dan@saucelabs.com <dan@saucelabs.com>
2018-01-03 13:49:28 -0800

    Replace Apps with zips

--- sample-code/javascript-wd/test/helpers/apps.js
+++ sample-code/javascript-wd/test/helpers/apps.js
@@ -1,19 +1,8 @@
 if (process.env.DEV) {
-  exports.iosTestApp = "sample-code/apps/TestApp-iphonesimulator.app";
-  exports.iosUICatalogApp = "sample-code/apps/UICatalog-iphonesimulator.app";
-  exports.iosWebviewApp = "sample-code/apps/WebViewApp/build/release-iphonesimulator/WebViewApp.app";
-  exports.iosUICatalogApp = "sample-code/apps/UICatalog/build/release-iphonesimulator/UICatalog.app";
-  exports.androidApiDemos = "sample-code/apps/ApiDemos/bin/ApiDemos-debug.apk";
-  exports.selendroidTestApp = "sample-code/apps/selendroid-test-app.apk";
+  exports.iosTestApp = "sample-code/apps/TestApp.zip";
+  exports.androidApiDemos = "sample-code/apps/ApiDemos-debug.apk";
 } else {
   // TODO: Change thes URL's to updated locations
   exports.iosTestApp = "http://appium.github.io/appium/assets/TestApp7.1.app.zip";
-  exports.iosUICatalogApp = "http://appium.github.io/appium/assets/TestApp7.1.app.zip";
-  exports.iosWebviewApp = "http://appium.github.io/appium/assets/WebViewApp7.1.app.zip";
-  exports.iosUICatalogApp = "http://appium.github.io/appium/assets/UICatalog7.1.app.zip";
   exports.androidApiDemos = "http://appium.github.io/appium/assets/ApiDemos-debug.apk";
-  exports.selendroidTestApp = "http://appium.github.io/appium/assets/selendroid-test-app-0.10.0.apk";
-
-  exports.iosWebviewAppLocal = "http://localhost:3000/WebViewApp7.1.app.zip";
-  exports.androidApiDemosLocal = "http://localhost:3000/ApiDemos-debug.apk";
 }

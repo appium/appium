c8b9aa1d36034d2d5faab1e774f98713b2c3119a
Dan Graham <dan@saucelabs.com>
2018-01-03 10:34:58 -0800

    Sample code stub (#9887)
    * WD tests

---
+++ sample-code/javascript-wd/test/basic/ios-create-session.test.js
@@ -0,0 +1,9 @@
+import chai from 'chai';
+
+const {assert} = chai;
+
+describe('Dummy test', function () {
+  it('should run a temp test', async function () {
+    assert.equal(1+1, 2, 'One plus one should equal two');
+  });
+});

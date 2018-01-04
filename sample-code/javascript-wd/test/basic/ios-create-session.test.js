0008af64d3722475e4008642573ad179cd4ef925
Dan Graham <dan@saucelabs.com>
2018-01-04 13:59:55 -0800

    WD sample code (#9918)

--- sample-code/javascript-wd/test/basic/ios-create-session.test.js
+++ sample-code/javascript-wd/test/basic/ios-create-session.test.js
@@ -1,9 +1,26 @@
+import wd from 'wd';
 import chai from 'chai';
+import { iosCaps, serverConfig } from '../helpers/caps';
 
 const {assert} = chai;
 
-describe('Dummy test', function () {
-  it('should run a temp test', async function () {
-    assert.equal(1+1, 2, 'One plus one should equal two');
+describe('Create session', function () {
+  it('should create and destroy IOS sessions', async function () {
+    // Connect to Appium server
+    const driver = await wd.promiseChainRemote(serverConfig);
+
+    // Start the session
+    await driver.init({
+      ...iosCaps,
+      app: require('../helpers/apps').iosTestApp
+    });
+
+    // Check that the XCUIElementTypeApplication was what we expect it to be
+    const applicationElement = await driver.elementByClassName('XCUIElementTypeApplication');
+    const applicationName = await applicationElement.getAttribute('name');
+    assert.equal(applicationName, 'TestApp');
+
+    // Quit the session
+    await driver.quit();
   });
 });

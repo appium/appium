e9478318e8c413dc1ac41bc830c7d258f948747a
dan@saucelabs.com <dan@saucelabs.com>
2018-01-04 10:12:40 -0800

    PR fixes

--- sample-code/javascript-wd/test/helpers/apps.js
+++ sample-code/javascript-wd/test/helpers/apps.js
@@ -1,8 +1,8 @@
 if (process.env.DEV) {
-  exports.iosTestApp = "sample-code/apps/TestApp.app.zip";
-  exports.androidApiDemos = "sample-code/apps/ApiDemos-debug.apk";
-} else {
   // TODO: Change thes URL's to updated locations
   exports.iosTestApp = "http://appium.github.io/appium/assets/TestApp7.1.app.zip";
   exports.androidApiDemos = "http://appium.github.io/appium/assets/ApiDemos-debug.apk";
+} else {
+  exports.iosTestApp = "sample-code/apps/TestApp.app.zip";
+  exports.androidApiDemos = "sample-code/apps/ApiDemos-debug.apk";
 }
--- sample-code/javascript-wd/test/helpers/caps.js
+++ sample-code/javascript-wd/test/helpers/caps.js
@@ -3,15 +3,21 @@ import path from 'path';
 const iosCaps = {
   platformName: 'iOS',
   automationName: 'XCUITest',
-  deviceName: 'iPhone 6s' || process.env.IOS_DEVICE_NAME,
-  platformVersion: '11.2' || process.env.IOS_PLATFORM_VERSION,
+  deviceName:  process.env.IOS_DEVICE_NAME || 'iPhone 6s',
+  platformVersion: process.env.IOS_PLATFORM_VERSION || '11.1',
   app: undefined, // Will be added in tests
 };
+
+// Leave the Android platformVersion blank and set deviceName to a random string (Android deviceName is ignored by Appium but is still required)
+// If we're using SauceLabs, set the Android deviceName and platformVersion to the latest supported SauceLabs device and version
+const DEFAULT_ANDROID_DEVICE_NAME = process.env.SAUCE ? 'Android GoogleAPI Emulator' : 'My Android Device';
+const DEFAULT_ANDROID_PLATFORM_VERSION = process.env.SAUCE ? '7.1' : null;
 
 const androidCaps = {
   platformName: 'Android',
   automationName: 'UiAutomator2',
-  deviceName: 'Android' || process.env.ANDROID_DEVICE_NAME,
+  deviceName: process.env.ANDROID_DEVICE_NAME || DEFAULT_ANDROID_DEVICE_NAME,
+  platformVersion: process.env.ANDROID_PLATFORM_VERSION || DEFAULT_ANDROID_PLATFORM_VERSION,
   app: undefined, // Will be added in tests
 };
 

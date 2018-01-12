9229a58ea4576306ba5fc9863b93a0f699634889
Mykola Mokhnach <nick+1@wire.com>
2018-01-12 16:41:04 +0100

    Update parameters list for start_recording_screen/stop_recording_screen endpoints (#168)

--- test/mjsonwp/routes-specs.js
+++ test/mjsonwp/routes-specs.js
@@ -40,7 +40,7 @@ describe('MJSONWP', () => {
       }
       let hash = shasum.digest('hex').substring(0, 8);
       // Modify the hash whenever the protocol has intentionally been modified.
-      hash.should.equal('94cbb31c');
+      hash.should.equal('21ceab6b');
     });
   });
 

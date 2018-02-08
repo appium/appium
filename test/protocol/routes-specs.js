c679d534e4bba3c1d2728bf69b1b8d46b10713e5
Mykola Mokhnach <nick+1@wire.com>
2018-02-08 14:38:26 +0100

    Change the request type of queryAppState to POST (#185)
    * Change the request type of queryAppState to POST
    
    * Keep GET template

--- test/protocol/routes-specs.js
+++ test/protocol/routes-specs.js
@@ -40,7 +40,7 @@ describe('Protocol', function () {
       }
       let hash = shasum.digest('hex').substring(0, 8);
       // Modify the hash whenever the protocol has intentionally been modified.
-      hash.should.equal('eefd5dd0');
+      hash.should.equal('e4612508');
     });
   });
 

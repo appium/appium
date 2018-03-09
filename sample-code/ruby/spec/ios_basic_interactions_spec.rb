df3af263811f109b33d0e5ab0f37402f85f348b0
dan@saucelabs.com <dan@saucelabs.com>
2018-03-08 17:57:07 -0800

    Added Ruby IOS tests

--- sample-code/ruby/spec/ios_basic_interactions_spec.rb
+++ sample-code/ruby/spec/ios_basic_interactions_spec.rb
@@ -3,7 +3,7 @@ require 'appium_lib'
 require 'test/unit'
 extend Test::Unit::Assertions
 
-APP_PATH = '../apps/TestApp.app.zip'
+APP_PATH = ENV['SAUCE_LABS'] ? 'http://appium.github.io/appium/assets/TestApp7.1.app.zip' : '../apps/TestApp.app.zip'
 
 desired_caps = {
   caps: {
@@ -14,16 +14,20 @@ desired_caps = {
     automationName: 'XCUITest',
   },
   appium_lib: {
-    sauce_username:   nil, # TODO: Make Sauce option
-    sauce_access_key: nil,
+    sauce_username:   ENV['SAUCE_LABS'] ? ENV['SAUCE_USERNAME'] : nil,
+    sauce_access_key: ENV['SAUCE_LABS'] ? ENV['SAUCE_ACCESS_KEY'] : nil,
     wait: 60
   }
 }
 
 # Start the driver
 describe "IOS Basic Interactions" do
-  before do
+  before(:all) do
     @driver = Appium::Driver.new(desired_caps, true).start_driver
+  end
+
+  after(:all) do
+    @driver.quit
   end
 
   it "should send keys to inputs" do
@@ -31,5 +35,15 @@ describe "IOS Basic Interactions" do
     expect(@textFieldEl.attribute(:value)).to be_nil
     @textFieldEl.send_keys("Hello World!")
     expect(@textFieldEl.attribute(:value)).to eq("Hello World!")
+  end
+
+  it "should click a button that opens an alert" do
+    @buttonElementId = "show alert"
+    @buttonElement = @driver.find_element(:accessibility_id, @buttonElementId)
+    @buttonElement.click()
+    @alertTitleId = "Cool title"
+    @alertTitleElement = @driver.find_element(:accessibility_id, @alertTitleId)
+    @alertTitle = @alertTitleElement.attribute(:value)
+    expect(@alertTitle.attribute(:value)).to eq("Cool title")
   end
 end

59235da587cf05c514957be230d1d2c7731ec790
Mykola Mokhnach <nick+1@wire.com>
2018-03-26 14:46:37 +0200

    Add endpoints for system clipboard interaction (#199)
    * Add endpoints for system clipboard interaction
    
    * Fix get endpoint

--- test/protocol/routes-specs.js
+++ test/protocol/routes-specs.js
@@ -40,7 +40,7 @@ describe('Protocol', function () {
       }
       let hash = shasum.digest('hex').substring(0, 8);
       // Modify the hash whenever the protocol has intentionally been modified.
-      hash.should.equal('e4612508');
+      hash.should.equal('afd1f1e1');
     });
   });
 

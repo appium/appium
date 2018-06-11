f4b89448d6a3dca8b4a5950ed22615e27bf80c6a
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2018-06-12 07:59:39 +0900

    add format in deviveTime as a parameter (#225)

--- test/protocol/routes-specs.js
+++ test/protocol/routes-specs.js
@@ -40,7 +40,7 @@ describe('Protocol', function () {
       }
       let hash = shasum.digest('hex').substring(0, 8);
       // Modify the hash whenever the protocol has intentionally been modified.
-      hash.should.equal('77715dea');
+      hash.should.equal('0030c428');
     });
   });
 

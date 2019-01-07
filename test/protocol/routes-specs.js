a98f22607ef138f4bad42071dfac684ca7be9fdf
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-01-07 17:26:10 +0900

    add POST for system_time in order to set payload (#294)

--- test/protocol/routes-specs.js
+++ test/protocol/routes-specs.js
@@ -40,7 +40,7 @@ describe('Protocol', function () {
       }
       let hash = shasum.digest('hex').substring(0, 8);
       // Modify the hash whenever the protocol has intentionally been modified.
-      hash.should.equal('9e59ddee');
+      hash.should.equal('17fb263f');
     });
   });
 

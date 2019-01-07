8ae95a5a61ae4207c90e46fc015ee9f878fb53ec
Isaac Murchie <isaac.appium@gmail.com>
2019-01-07 11:32:02 -0600

    chore: fix linting

--- sample-code/javascript-webdriverio/test/basic/ios-create-session.test.js
+++ sample-code/javascript-webdriverio/test/basic/ios-create-session.test.js
@@ -1,24 +1,24 @@
-const webdriverio = require("webdriverio");
-const iosOptions = require("../../helpers/caps").iosOptions;
-const app = require("../../helpers/apps").iosTestApp;
-const assert = require("chai").assert;
+const webdriverio = require('webdriverio');
+const iosOptions = require('../../helpers/caps').iosOptions;
+const app = require('../../helpers/apps').iosTestApp;
+const assert = require('chai').assert;
 
 iosOptions.capabilities.app = app;
 
-describe("Create session", function () {
+describe('Create session', function () {
   let client;
 
   beforeEach(async function () {
     client = await webdriverio.remote(iosOptions);
   });
 
-  it("should create and destroy IOS sessions", async function () {
+  it('should create and destroy IOS sessions', async function () {
     const res = await client.status();
     assert.isObject(res.build);
 
-    const element = await client.findElement("class name", "XCUIElementTypeApplication");
-    await client.getElementAttribute(element.ELEMENT, "name").then((attr) => {
-      assert.equal(attr, "TestApp");
+    const element = await client.findElement('class name', 'XCUIElementTypeApplication');
+    await client.getElementAttribute(element.ELEMENT, 'name').then((attr) => {
+      assert.equal(attr, 'TestApp');
     });
 
     const destroySession = await client.deleteSession();

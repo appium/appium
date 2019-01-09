abf210e55f194a407efe34b56074f782e440d9f3
dpgraham <dpgraham.85@gmail.com>
2019-01-09 14:26:39 -0800

    PR fix... lodashify it

--- check-npm-pack-files.js
+++ check-npm-pack-files.js
@@ -3,13 +3,7 @@ const _ = require('lodash');
 
 const res = JSON.parse(childProcess.execSync('npm pack --dry-run --json --ignore-scripts', {encoding: 'utf8'}))[0];
 
-// Get list of the files being packed
-const fileNames = [];
-for (const file of res.files) {
-  fileNames.push(file.path);
-}
-
-// Get list of files we want to test for existence
+// List of files we are testing to make sure they are included in package
 const testFiles = [
   'npm-shrinkwrap.json', // Check that npm-shrinkwrap.json is being packed
   'LICENSE', // Check that license is included
@@ -17,7 +11,7 @@ const testFiles = [
 ];
 
 // Get list of files in `testFiles` that aren't in the list of packaged fileNames
-const missingFiles = _.without(testFiles, ...fileNames);
+const missingFiles = _.without(testFiles, ..._.map(res.files, 'path'));
 
 if (!_.isEmpty(missingFiles)) {
   throw new Error(`Files [${missingFiles.join(', ')}] are not included in package.json "files". ` +

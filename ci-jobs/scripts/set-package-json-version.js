dc216f87207840062c8a78d69d9566016763ea91
dpgraham <dpgraham.85@gmail.com>
2019-06-25 07:27:12 -0700

    Fix

--- ci-jobs/scripts/set-package-json-version.js
+++ ci-jobs/scripts/set-package-json-version.js
@@ -5,11 +5,14 @@ const packageJson = require('../../package.json');
 
 const log = new logger.getLogger('Create Release Branch:');
 
-async function setPackageJsonVersion (version) {
-  version = version || `${process.env.MINOR_BRANCH_NAME}.0-rc.0`;
+async function setPackageJsonVersion (version = `${process.env.MINOR_BRANCH_NAME}.0-rc.0`) {
   packageJson.version = version;
   log.info(`Setting version to: ${version}`);
-  await fs.writeFile(path.resolve(__dirname, '..', '..', 'package.json'), JSON.stringify(packageJson, null, 2));
+  await fs.writeFile(
+    path.resolve(__dirname, '..', '..', 'package.json'),
+    JSON.stringify(packageJson, null, 2),
+    'utf8',
+  );
 }
 
 if (require.main === module) {

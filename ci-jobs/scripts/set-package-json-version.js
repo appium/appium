6ac89714ebb03393c15d5060f7402931fc149c0a
dpgraham <dpgraham.85@gmail.com>
2019-06-27 14:18:09 -0700

    Make a build pipeline

--- ci-jobs/scripts/set-package-json-version.js
+++ ci-jobs/scripts/set-package-json-version.js
@@ -5,7 +5,14 @@ const packageJson = require('../../package.json');
 
 const log = new logger.getLogger('Create Release Branch:');
 
-async function setPackageJsonVersion (version = `${process.env.MINOR_BRANCH_NAME}.0-rc.0`) {
+async function setPackageJsonVersion (version) {
+  if (!version) {
+    if (process.env.MINOR_BRANCH_NAME) {
+      version = `${process.env.MINOR_BRANCH_NAME}.0-rc.0`;
+    } else if (process.env.APPIUM_VERSION) {
+      version = process.env.APPIUM_VERSION;
+    }
+  }
   packageJson.version = version;
   log.info(`Setting version to: ${version}`);
   await fs.writeFile(

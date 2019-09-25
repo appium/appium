c9ab4022828300977152f9ed64c5a9d2af4154cf
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-09-25 09:50:38 +0900

    Remove unused rependencies (#77)
    * thanks, ios-deploy
    
    * thanks, ifuse
    
    * thanks ios_webkit_debug_proxy and idevicelocation

--- test/ios-specs.js
+++ test/ios-specs.js
@@ -2,8 +2,7 @@
 
 import { fixes, XcodeCheck, XcodeCmdLineToolsCheck, DevToolsSecurityCheck,
          AuthorizationDbCheck, CarthageCheck, OptionalApplesimutilsCommandCheck,
-         OptionalIdbCommandCheck, OptionalIdevicelocationCommandCheck,
-         OptionalIOSDeployCommandCheck, OptionalIOSWebkitDebugProxyCommandCheck, OptionalIfuseCommandCheck } from '../lib/ios';
+         OptionalIdbCommandCheck } from '../lib/ios';
 import { fs, system } from 'appium-support';
 import * as utils from '../lib/utils';
 import * as tp from 'teen_process';
@@ -361,134 +360,6 @@ describe('ios', function () {
     });
     it('fix', async function () {
       removeColors(await check.fix()).should.equal('Why applesimutils is needed and how to install it: http://appium.io/docs/en/drivers/ios-xcuitest/');
-    });
-  }));
-
-  describe('OptionalIdevicelocationCommandCheck', withMocks({tp, utils}, (mocks) => {
-    let check = new OptionalIdevicelocationCommandCheck();
-    it('autofix', function () {
-      check.autofix.should.not.be.ok;
-    });
-    it('diagnose - success', async function () {
-      mocks.utils.expects('resolveExecutablePath').once().returns('path/to/idevicelocation');
-      (await check.diagnose()).should.deep.equal({
-        ok: true,
-        optional: true,
-        message: 'idevicelocation is installed at: path/to/idevicelocation'
-      });
-      mocks.verify();
-    });
-    it('diagnose - failure', async function () {
-      mocks.utils.expects('resolveExecutablePath').once().returns(false);
-      (await check.diagnose()).should.deep.equal({
-        ok: false,
-        optional: true,
-        message: 'idevicelocation cannot be found'
-      });
-      mocks.verify();
-    });
-    it('fix', async function () {
-      removeColors(await check.fix()).should.equal('idevicelocation is used to set geolocation for real device. Please read https://github.com/JonGabilondoAngulo/idevicelocation to install it');
-    });
-  }));
-
-  describe('OptionalIOSDeployCommandCheck', withMocks({tp, utils}, (mocks) => {
-    let check = new OptionalIOSDeployCommandCheck();
-    it('autofix', function () {
-      check.autofix.should.not.be.ok;
-    });
-    it('diagnose - success', async function () {
-      mocks.utils.expects('resolveExecutablePath').once().returns('path/to/ios-deploy');
-      mocks.tp.expects('exec').once().returns({stdout: '1.9.4', stderr: ''});
-      (await check.diagnose()).should.deep.equal({
-        ok: true,
-        optional: true,
-        message: 'ios-deploy is installed at: path/to/ios-deploy. Installed version is: 1.9.4'
-      });
-      mocks.verify();
-    });
-    it('diagnose - failure', async function () {
-      mocks.utils.expects('resolveExecutablePath').once().returns(false);
-      (await check.diagnose()).should.deep.equal({
-        ok: false,
-        optional: true,
-        message: 'ios-deploy cannot be found'
-      });
-      mocks.verify();
-    });
-    it('fix', async function () {
-      removeColors(await check.fix()).should.equal('ios-deploy is used to install iOS applications to real device. Please read http://appium.io/docs/en/drivers/ios-xcuitest-real-devices/ to install it');
-    });
-  }));
-
-  describe('OptionalIOSWebkitDebugProxyCommandCheck', withMocks({tp, utils}, (mocks) => {
-    let check = new OptionalIOSWebkitDebugProxyCommandCheck();
-    it('autofix', function () {
-      check.autofix.should.not.be.ok;
-    });
-    it('diagnose - success', async function () {
-      mocks.utils.expects('resolveExecutablePath').once().returns('path/to/ios_webkit_debug_proxy');
-      mocks.tp.expects('exec').once().returns({stdout: `ios_webkit_debug_proxy 1.8.3\ninfo AppiumDoctor Built with libimobiledevice v1.2.0, libplist v2.0.0`, stderr: ''});
-
-      (await check.diagnose()).should.deep.equal({
-        ok: true,
-        optional: true,
-        message: 'ios_webkit_debug_proxy is installed at: path/to/ios_webkit_debug_proxy. Installed version is: 1.8.3, info AppiumDoctor Built with libimobiledevice v1.2.0, libplist v2.0.0'
-      });
-      mocks.verify();
-    });
-    it('diagnose - failure', async function () {
-      mocks.utils.expects('resolveExecutablePath').once().returns(false);
-      (await check.diagnose()).should.deep.equal({
-        ok: false,
-        optional: true,
-        message: 'ios_webkit_debug_proxy cannot be found'
-      });
-      mocks.verify();
-    });
-    it('fix', async function () {
-      removeColors(await check.fix()).should.equal('ios_webkit_debug_proxy is used to proxy requests from Appium to MobileSafari running on real device. Please read https://github.com/google/ios-webkit-debug-proxy to install it');
-    });
-  }));
-
-  describe('OptionalIfuseCommandCheck', withMocks({tp, utils}, (mocks) => {
-    let check = new OptionalIfuseCommandCheck();
-    it('autofix', function () {
-      check.autofix.should.not.be.ok;
-    });
-    it('diagnose - success', async function () {
-      mocks.utils.expects('resolveExecutablePath').once().returns('path/to/ifuse');
-      mocks.tp.expects('exec').once().returns({stdout: '', stderr: 'ifuse 1.1.3\n'});
-
-      (await check.diagnose()).should.deep.equal({
-        ok: true,
-        optional: true,
-        message: 'ifuse is installed at: path/to/ifuse. Installed version is: 1.1.3'
-      });
-      mocks.verify();
-    });
-    it('diagnose - success - version is null', async function () {
-      mocks.utils.expects('resolveExecutablePath').once().returns('path/to/ifuse');
-      mocks.tp.expects('exec').once().returns({stdout: '', stderr: ''});
-
-      (await check.diagnose()).should.deep.equal({
-        ok: true,
-        optional: true,
-        message: 'ifuse is installed at: path/to/ifuse.'
-      });
-      mocks.verify();
-    });
-    it('diagnose - failure', async function () {
-      mocks.utils.expects('resolveExecutablePath').once().returns(false);
-      (await check.diagnose()).should.deep.equal({
-        ok: false,
-        optional: true,
-        message: 'ifuse cannot be found'
-      });
-      mocks.verify();
-    });
-    it('fix', async function () {
-      removeColors(await check.fix()).should.equal('ifuse is used to manage files/folders against a real device. Please read https://github.com/appium/appium/blob/master/docs/en/writing-running-appium/ios/ios-xctest-file-movement.md to install it');
     });
   }));
 });

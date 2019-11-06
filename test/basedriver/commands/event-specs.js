09a7bf81c9cbef0b5012521d8b0989a488b59a16
Jonathan Lipps <jlipps@gmail.com>
2019-11-05 16:38:30 -0800

    feat: add ability to retrieve log events as a new api route (#365)

--- test/basedriver/commands/event-specs.js
+++ test/basedriver/commands/event-specs.js
@@ -13,4 +13,12 @@ describe('logging custom events', function () {
     await d.logCustomEvent('myorg', 'myevent');
     _.keys(d._eventHistory).should.eql(['commands', 'myorg:myevent']);
   });
+  it('should get all events including custom ones', async function () {
+    const d = new BaseDriver();
+    d._eventHistory.should.eql({commands: []});
+    d.logEvent('appiumEvent');
+    await d.logCustomEvent('myorg', 'myevent');
+    const events = await d.getLogEvents();
+    _.keys(events).should.eql(['commands', 'appiumEvent', 'myorg:myevent']);
+  });
 });

260af285ac12a14dbd5cec5fa93968648627628d
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-11-13 16:44:36 +0900

    feat: add filtering log events with type (related to #365) (#368)
    * add filtering with type
    
    * use reduce, add converting to array
    
    * make anonymous

--- test/basedriver/commands/event-specs.js
+++ test/basedriver/commands/event-specs.js
@@ -22,3 +22,56 @@ describe('logging custom events', function () {
     _.keys(events).should.eql(['commands', 'appiumEvent', 'myorg:myevent']);
   });
 });
+
+describe('#getLogEvents', function () {
+  it('should allow to get all events', async function () {
+    const d = new BaseDriver();
+    d._eventHistory.should.eql({commands: []});
+    d._eventHistory.testCommand = ['1', '2', '3'];
+    await d.getLogEvents().should.eql({
+      commands: [], testCommand: ['1', '2', '3']
+    });
+  });
+
+  it('should filter with testCommand', async function () {
+    const d = new BaseDriver();
+    d._eventHistory.should.eql({commands: []});
+    d._eventHistory.testCommand = ['1', '2', '3'];
+    await d.getLogEvents('testCommand').should.eql({
+      testCommand: ['1', '2', '3']
+    });
+  });
+
+  it('should not filter with wrong but can be a part of the event name', async function () {
+    const d = new BaseDriver();
+    d._eventHistory.should.eql({commands: []});
+    d._eventHistory.testCommand = ['1', '2', '3'];
+    await d.getLogEvents('testCommandDummy').should.eql({});
+  });
+
+  it('should filter with multiple event keys', async function () {
+    const d = new BaseDriver();
+    d._eventHistory.should.eql({commands: []});
+    d._eventHistory.testCommand = ['1', '2', '3'];
+    d._eventHistory.testCommand2 = ['4', '5'];
+    await d.getLogEvents(['testCommand', 'testCommand2']).should.eql({
+      testCommand: ['1', '2', '3'], testCommand2: ['4', '5']
+    });
+  });
+
+  it('should filter with custom events', async function () {
+    const d = new BaseDriver();
+    d._eventHistory.should.eql({commands: []});
+    d._eventHistory['custom:appiumEvent'] = ['1', '2', '3'];
+    await d.getLogEvents(['custom:appiumEvent']).should.eql({
+      'custom:appiumEvent': ['1', '2', '3']
+    });
+  });
+
+  it('should not filter with no existed event name', async function () {
+    const d = new BaseDriver();
+    d._eventHistory.should.eql({commands: []});
+    d._eventHistory.testCommand = ['1', '2', '3'];
+    await d.getLogEvents(['noEventName']).should.eql({});
+  });
+});

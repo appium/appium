d22c01f7590aa83a2f09a0667f5c2a80f2a8a754
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2020-04-18 10:08:56 +0900

    feat: add calling chrome devtools endpoint for chromium (#405)
    * add calling chrome devtools for chromium
    
    * fill command and required parameter in
    
    * replace goog with :vendor

--- test/protocol/routes-specs.js
+++ test/protocol/routes-specs.js
@@ -40,7 +40,7 @@ describe('Protocol', function () {
       }
       let hash = shasum.digest('hex').substring(0, 8);
       // Modify the hash whenever the protocol has intentionally been modified.
-      hash.should.equal('86dc6c5b');
+      hash.should.equal('789b5ab2');
     });
   });
 

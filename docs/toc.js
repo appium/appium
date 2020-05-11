d4429154e7a809d5e0d518a0673c95be6584207e
Mykola Mokhnach <mokhnach@gmail.com>
2020-05-11 08:49:56 +0200

    docs: Add documentation for iOS audio capture feature (#14315)

--- docs/toc.js
+++ docs/toc.js
@@ -247,6 +247,7 @@ module.exports = {
         ['iOS Install Certificate', 'ios-xctest-install-certificate.md'],
         ['tvOS support', 'ios-tvos.md'],
         ['Pushing/Pulling files', 'ios-xctest-file-movement.md'],
+        ['Audio Capture', 'audio-capture.md'],
       ]],
       ['Android', ['android',
         ['Low-Level Insights on Android Input Events', 'actions.md'],

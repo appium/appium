da521fbf76353619657a3fd7899f193ab341001d
Jonathan Lipps <jlipps@gmail.com>
2020-06-10 16:49:56 -0700

    feat: add driver update cli command

---
+++ test/npm-specs.js
@@ -0,0 +1,16 @@
+// transpile:mocha
+
+import chai from 'chai';
+import NPM from '../lib/cli/npm';
+chai.should();
+
+describe('getLatestSafeUpgradeFromVersions', function () {
+  const versions1 = ['0.1.0', '0.1.1', '0.2.0', '0.2.5', '1.0.0', '1.0.1', '1.1.5', '1.2.7',
+    '2.0.0', '1.2.8-beta', '1.2.9-alpha', '1.3.0-rc', '2.0.1-beta'];
+  const npm = new NPM(null);
+  it('should get the latest minor upgrade in a list of versions', function () {
+    npm.getLatestSafeUpgradeFromVersions('0.1.0', versions1).should.eql('0.2.5');
+    npm.getLatestSafeUpgradeFromVersions('1.0.0', versions1).should.eql('1.2.7');
+    npm.getLatestSafeUpgradeFromVersions('0.2.0', versions1).should.eql('0.2.5');
+  });
+});

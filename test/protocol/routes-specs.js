14efcc973a4f78902f6b1a171fdf5242a0212118
Mykola Mokhnach <mokhnach@gmail.com>
2020-09-19 21:24:07 +0200

    feat: Add Webauthn routes (#433)

--- test/protocol/routes-specs.js
+++ test/protocol/routes-specs.js
@@ -40,7 +40,7 @@ describe('Protocol', function () {
       }
       let hash = shasum.digest('hex').substring(0, 8);
       // Modify the hash whenever the protocol has intentionally been modified.
-      hash.should.equal('e4576115');
+      hash.should.equal('52aacae0');
     });
   });
 

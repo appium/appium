e792d3fe58e67dbf06301c0f047077f00f58ac17
Jonathan Lipps <jlipps@gmail.com>
2021-01-14 22:53:31 -0800

    chore: require node v12

--- test/config-specs.js
+++ test/config-specs.js
@@ -146,6 +146,7 @@ describe('Config', function () {
         const unsupportedVersions = [
           'v0.1', 'v0.9.12', 'v0.10.36', 'v0.12.14',
           'v4.4.7', 'v5.7.0', 'v6.3.1', 'v7.1.1',
+          'v8.0.0', 'v9.2.3', 'v10.1.0',
         ];
         for (const version of unsupportedVersions) {
           it(`should fail if node is ${version}`, function () {
@@ -156,20 +157,16 @@ describe('Config', function () {
       });
 
       describe('supported nodes', function () {
-        it('should succeed if node is 8+', function () {
-          process.version = 'v8.1.2';
+        it('should succeed if node is 12+', function () {
+          process.version = 'v12.0.1';
           checkNodeOk.should.not.throw();
         });
-        it('should succeed if node is 9+', function () {
-          process.version = 'v9.1.2';
+        it('should succeed if node is 13+', function () {
+          process.version = 'v13.6.0';
           checkNodeOk.should.not.throw();
         });
-        it('should succeed if node is 10+', function () {
-          process.version = 'v10.0.1';
-          checkNodeOk.should.not.throw();
-        });
-        it('should succeed if node is 11+', function () {
-          process.version = 'v11.6.0';
+        it('should succeed if node is 14+', function () {
+          process.version = 'v14.0.0';
           checkNodeOk.should.not.throw();
         });
       });

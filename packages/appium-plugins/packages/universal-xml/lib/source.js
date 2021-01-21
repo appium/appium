d8ae2914641a5cf1df9d0ebecc2fbd3a155384c1
Jonathan Lipps <jlipps@gmail.com>
2021-01-20 17:12:01 -0800

    feat(xml): add xpath mapper so queries can be run on the new source

--- packages/appium-plugins/packages/universal-xml/lib/source.js
+++ packages/appium-plugins/packages/universal-xml/lib/source.js
@@ -16,13 +16,18 @@ const GEN_OPTS = {
 };
 
 export const ATTR_PREFIX = '@_';
+export const IDX_PATH_PREFIX = `${ATTR_PREFIX}indexPath`;
+export const IDX_PREFIX = `${ATTR_PREFIX}index`;
 
 const isAttr = (k) => k.substring(0, 2) === ATTR_PREFIX;
 const isNode = (k) => !isAttr(k);
 
-export function transformSourceXml (xmlStr, platform, metadata) {
+export function transformSourceXml (xmlStr, platform, {metadata, addIndexPath = false}) {
+  // first thing we want to do is modify the ios source root node, because it doesn't include the
+  // necessary index attribute, so we add it if it's not there
+  xmlStr = xmlStr.replace('<AppiumAUT>', '<AppiumAUT index="0">');
   const xmlObj = parser.parse(xmlStr, PARSE_OPTS);
-  const unknowns = transformNode(xmlObj, platform, metadata);
+  const unknowns = transformNode(xmlObj, platform, {metadata, addIndexPath, parentPath: ''});
   const jParser = new j2xParser(GEN_OPTS);
   const transformedXml = jParser.parse(xmlObj).trim();
   return {xml: transformedXml, unknowns};
@@ -49,21 +54,35 @@ function getUniversalAttrName (attrName, platform) {
   return getUniversalName(ATTR_MAP, attrName, platform);
 }
 
-function transformNode (nodeObj, platform, metadata) {
+function transformNode (nodeObj, platform, {metadata, addIndexPath, parentPath}) {
   const unknownNodes = [];
   const unknownAttrs = [];
   if (_.isPlainObject(nodeObj)) {
     const keys = Object.keys(nodeObj);
     const childNodeNames = keys.filter(isNode);
     const attrs = keys.filter(isAttr);
+    let thisIndexPath = parentPath;
+
+    if (attrs.length && addIndexPath) {
+      if (!attrs.includes(IDX_PREFIX)) {
+        throw new Error(`Index path is required but node found with no 'index' attribute`);
+      }
+
+      thisIndexPath = `${parentPath}/${nodeObj[IDX_PREFIX]}`;
+      nodeObj[IDX_PATH_PREFIX] = thisIndexPath;
+    }
 
     TRANSFORMS[platform]?.(nodeObj, metadata);
     unknownAttrs.push(...transformAttrs(nodeObj, attrs, platform));
-    unknownNodes.push(...transformChildNodes(nodeObj, childNodeNames, platform, metadata));
+    unknownNodes.push(...transformChildNodes(nodeObj, childNodeNames, platform, {
+      metadata, addIndexPath, parentPath: thisIndexPath
+    }));
 
   } else if (_.isArray(nodeObj)) {
     for (const childObj of nodeObj) {
-      const {nodes, attrs} = transformNode(childObj, platform, metadata);
+      const {nodes, attrs} = transformNode(childObj, platform, {
+        metadata, addIndexPath, parentPath
+      });
       unknownNodes.push(...nodes);
       unknownAttrs.push(...attrs);
     }
@@ -74,11 +93,11 @@ function transformNode (nodeObj, platform, metadata) {
   };
 }
 
-function transformChildNodes (nodeObj, childNodeNames, platform, metadata) {
+function transformChildNodes (nodeObj, childNodeNames, platform, {metadata, addIndexPath, parentPath}) {
   const unknownNodes = [];
   for (const nodeName of childNodeNames) {
     // before modifying the name of this child node, recurse down and modify the subtree
-    transformNode(nodeObj[nodeName], platform, metadata);
+    transformNode(nodeObj[nodeName], platform, {metadata, addIndexPath, parentPath});
 
     // now translate the node name and replace the subtree with this node
     const universalName = getUniversalNodeName(nodeName, platform);

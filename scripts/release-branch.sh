b1a1a3aae4fd0f4e5889ddddc98d1032e1c4c213
Christopher Hiller <boneskull@boneskull.com>
2021-05-18 13:20:20 -0700

    chore: revert removal of scripts folder

---
+++ scripts/release-branch.sh
@@ -0,0 +1,13 @@
+git fetch
+echo "* Creating release branch $1"
+git checkout -b releases/$1
+rm -rf node_modules/ npm-shrinkwrap.json package-lock.json
+npm install
+npm shrinkwrap
+echo "* git add ."
+git add npm-shrinkwrap.json package.json
+git commit -m 'add shrinkwrap'
+echo "* npm version $1.0-rc.0"
+npm version $1.0-rc.0
+git push origin releases/$1
+bash ./scripts/release.sh

9e77ff7d6630e2613657cf8ad1cf753af45c4c38
Mykola Mokhnach <mokhnach@gmail.com>
2021-08-05 14:46:03 +0200

    Use has

--- packages/appium/lib/appium.js
+++ packages/appium/lib/appium.js
@@ -166,7 +166,7 @@ class AppiumDriver extends BaseDriver {
 
       let runningDriversData, otherPendingDriversData;
       const parsedInnerDriverArgs = parseExtensionArgs(this.args.driverArgs, driverName);
-      if (!_.isEmpty(parsedInnerDriverArgs) && _.has(InnerDriver.argsConstraints)) {
+      if (!_.isEmpty(parsedInnerDriverArgs) && _.has(InnerDriver, 'argsConstraints')) {
         const driverArgsConstraints = InnerDriver.argsConstraints;
         if (!_.isEmpty(driverArgsConstraints)) {
           // TODO: parse/verify CLI args and merge them to this.args if OK or throw an exception

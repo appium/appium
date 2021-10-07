a91bec345697bf3f124aacb8dd3aca52e942fa42
Christopher Hiller <boneskull@boneskull.com>
2021-10-07 15:14:58 -0700

    chore(gulp-plugins): postTranspile task always runs after transpile task; add dev task
    - `postTranspile` would only run from certain _other_ tasks (like `test`).  This seemed incorrect, so now it runs after `transpile`, regardless of what task (or human) runs `transpile`.
    - Added a `dev` task, which is like `watch`, except does not run tests (only transpiles)

--- packages/gulp-plugins/lib/tasks/transpile.js
+++ packages/gulp-plugins/lib/tasks/transpile.js
@@ -11,18 +11,30 @@ const JSON_SOURCES = [
   'test/**/*.json',
 ];
 
+/**
+ * @param {import('gulp').Gulp} gulp - The gulp instance.
+ */
 const configure = function configure (gulp, opts, env) {
-  gulp.task('transpile', function () {
-    // json files can be copied as is, they don't need to be transpiled
-    const firstPath = gulp.src(JSON_SOURCES, {base: './'})
-      .pipe(gulp.dest(opts.transpileOut));
-    const secondPath = gulp.src(opts.files, {base: './'})
-      .pipe(gulpIf(isVerbose(), debug()))
-      .pipe(new Transpiler(opts).stream())
-      .on('error', env.spawnWatcher.handleError.bind(env.spawnWatcher))
-      .pipe(gulp.dest(opts.transpileOut));
-    return merge(firstPath, secondPath);
-  });
+  const tasks = [
+    function transpileSources () {
+      // json files can be copied as is, they don't need to be transpiled
+      const firstPath = gulp.src(JSON_SOURCES, {base: './'})
+        .pipe(gulp.dest(opts.transpileOut));
+      const secondPath = gulp.src(opts.files, {base: './'})
+        .pipe(gulpIf(isVerbose(), debug()))
+        .pipe(new Transpiler(opts).stream())
+        .on('error', env.spawnWatcher.handleError.bind(env.spawnWatcher))
+        .pipe(gulp.dest(opts.transpileOut));
+      return merge(firstPath, secondPath);
+    }
+  ];
+
+  if (opts.postTranspile && opts.postTranspile.length) {
+    gulp.task('post-transpile', gulp.parallel(opts.postTranspile));
+    tasks.push('post-transpile');
+  }
+
+  gulp.task('transpile', gulp.series(tasks));
 };
 
 module.exports = {

3dcf282166601596317b74925644a7fd82d0678f
Jonathan Lipps <jlipps@gmail.com>
2022-01-14 12:35:36 -0800

    refactor!(support): move opencv related functionality into its own package

---
+++ packages/opencv/test/opencv-specs.js
@@ -0,0 +1,9 @@
+import { initOpenCv } from '../lib';
+
+describe('OpenCV', function () {
+  it('should initialize opencv library', async function () {
+    await initOpenCv();
+    const buildInfo = require('opencv-bindings').getBuildInformation();
+    buildInfo.should.include('OpenCV');
+  });
+});

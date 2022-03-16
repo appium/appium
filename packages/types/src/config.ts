72085caa0a4030d8495fa1c66b092069aeebb20b
Christopher Hiller <boneskull@boneskull.com>
2022-03-16 14:49:00 -0700

    feat(types): add new @appium/types package
    This changes the TS configuration to to a) emit declarations for supported packages, and b) build declarations incrementally by package.
    
    To begin, we are targeting `appium`, `@appium/base-driver` and `@appium/support` for declarations; these three are intended to be published with declarations generated via their JS.
    
    Both `@appium/support` and `appium` are fully typechecked (sans implicit `any` types), but `@appium/base-driver` is mostly not.  Regardless, declarations are generated for all three.
    
    Each package will have its own `tsconfig.json` which "inherits" from `config/tsconfig.base.json`.  Each will also need to declare relative paths to dependencies. e.g., the `appium` package must configure its `tsconfig.json` so that it depends on the declarations of `@appium/support` and also where to find `@appium/support`. Essentially, a small dependency tree gets built, and those at the "root" get their declarations built first; then the consumers use those declarations.
    
    Also reorganized some scripts and added `npm-run-all` to help and added a "typecheck" job to CI.
    
    Since `npm install` will build, it's pretty inconvenient to have `npm` abort installation if the build fails.  In that case, if any of the build steps fail (see `build:loose`) the installation will continue.  Added a `prepublishOnly` which runs a build in "strict" mode where everything must pass.
    
    _Note: there's still a strong coupling of "what `BaseDriver` provides" and "what an external driver *may* provide".  It's unclear to me if this is a problem, though there seem to be a handful of methods that external drivers *must* implement.  The easiest way to get around this would probably be to extract these required properties and methods into their own interface (duplication) if we cannot otherwise declare methods as "abstract".  As it stands, the `Driver` is basically just `BaseDriver`._
    
    Also:
    
    - Moved some configuration types out of `appium` and into here, since they are used both by `appium` and `base-driver` via `DriverOpts`
    - Split `Driver` type into "the thing that BaseDriver is" and "all the other methods external drivers can implement"
    - Added missing `proxyCommand` method
    - Better generics for `LogType`
    - Remove unused `Constructor` type
    - Better types for `getSession`/`getSessions`
    - Added util `Class` type
    - Added many missing typings from DefinitelyTyped
    - Added `ts-node` for `@wdio/types` (I need to send a PR to webdriver to eliminate this dependency)
    - Type checks in their CI step

---
+++ packages/types/src/config.ts
@@ -0,0 +1,128 @@
+import appiumConfigSchema from './appium-config-schema';
+import type { AppiumConfiguration, ServerConfig } from './appium-config';
+
+/**
+ * The Appium configuration as it would be in a user-provided configuration file.
+ */
+export type AppiumConfig = Partial<AppiumConfiguration>;
+
+/**
+ * Derive the "constant" type of the server properties from the schema.
+ */
+ type AppiumServerSchema =
+ typeof appiumConfigSchema['properties']['server']['properties'];
+
+/**
+ * This type associates the types generated from the schema ({@linkcode AppiumConfiguration})
+ * with the schema itself (beginning with the `server` prop).
+ */
+type ServerConfigMapping = Associated<ServerConfig, AppiumServerSchema>;
+
+/**
+ * Camel-cased server config. _Not_ flattened.
+ */
+export type NormalizedServerConfig = {
+  [Prop in keyof ServerConfigMapping as AppiumServerSchema[Prop] extends WithDest
+    ? AppiumServerSchema[Prop]['appiumCliDest']
+    : KebabToCamel<Prop>]: ServerConfig[Prop];
+};
+
+/**
+ * "Normalized" config, which is camel-cased (instead of kebab-case, like the schema). It is _not_ flattened.
+ */
+export type NormalizedAppiumConfig = {
+  server: NormalizedServerConfig;
+};
+
+/**
+ * This type checks if `appiumCliDest` is present in the object via
+ * {@linkcode WithDest}, and uses the _value_ of that property for the key name;
+ * otherwise uses the camel-cased value of the key name.
+ */
+type SetKeyForProp<Prop extends keyof ServerConfigMapping> =
+  AppiumServerSchema[Prop] extends WithDest
+    ? AppiumServerSchema[Prop]['appiumCliDest']
+    : KebabToCamel<Prop>;
+
+/**
+ * Checks for the existence of default values, and ensures those properties will
+ * always be defined (eliminates `| undefined` from the type).
+ * If no default value, just a type.
+ */
+type KeyOrDefaultForProp<Prop extends keyof ServerConfigMapping> =
+  AppiumServerSchema[Prop] extends WithDefault
+    ? NonNullable<ServerConfig[Prop]>
+    : ServerConfig[Prop];
+
+/**
+ * The final shape of the parsed CLI arguments.
+ *
+ * These will be camel-cased unless overridden by `appiumCliDest` field in schema(s).
+ */
+export type DriverOpts = {
+  [Prop in keyof ServerConfigMapping as SetKeyForProp<Prop>]: KeyOrDefaultForProp<Prop>;
+};
+// begin utils
+
+/**
+ * Converts a kebab-cased string into a camel-cased string.
+ */
+ type KebabToCamel<S extends string> =
+ S extends `${infer P1}-${infer P2}${infer P3}`
+   ? `${Lowercase<P1>}${Uppercase<P2>}${KebabToCamel<P3>}`
+   : Lowercase<S>;
+
+/**
+* Converts an object with kebab-cased keys into camel-cased keys.
+*/
+type ObjectToCamel<T> = {
+ [K in keyof T as KebabToCamel<string & K>]: T[K] extends Record<string, any>
+   ? KeysToCamelCase<T[K]>
+   : T[K];
+};
+
+/**
+* Converts an object or array to have camel-cased keys.
+*/
+type KeysToCamelCase<T> = {
+ [K in keyof T as KebabToCamel<string & K>]: T[K] extends Array<any>
+   ? KeysToCamelCase<T[K][number]>[]
+   : ObjectToCamel<T[K]>;
+};
+
+/**
+ * Object `B` has all the keys as object `A` (even if those keys in `A` are otherwise optional). 
+ */
+type Associated<A extends object, B extends { [key in keyof Required<A>]: unknown }> = {
+  [Prop in keyof Required<A>]: B[Prop];
+}
+
+// end utils
+
+// begin conditionals
+
+// These types describe what a particular prop in the schema _could_ look like. We use them as conditionals in the above types.
+
+/**
+ * Certain properties have an `appiumCliDest` prop, which affects the shape of
+ * `ParsedArgs`. This type helps recognize these properties.
+ *
+ * See `appium/lib/schema/keywords` for definition of `appiumCliDest`.
+ */
+ interface WithDest {
+  appiumCliDest: string;
+}
+
+/**
+ * Some properties have a `default` prop, which means practically they will not
+ * be `undefined` upon parsing.
+ *
+ * We use this to ensure that the `ParsedArgs` makes guarantees
+ * about the presence of properties.
+ */
+interface WithDefault<T = any> {
+  default: T;
+}
+
+// end conditionals
+

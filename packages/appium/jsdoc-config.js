69663229fa2578d9238c50431df9a0ac8d060310
Jonathan Lipps <jlipps@gmail.com>
2022-03-29 14:39:17 -0700

    docs(appium): create new docs generator for appium 2

---
+++ packages/appium/jsdoc-config.js
@@ -0,0 +1,10 @@
+const baseConfig = require('../../jsdoc-config');
+
+const lang = process.env.APPIUM_DOCS_LANG || 'en';
+
+const config = {...baseConfig};
+config.opts.source.include = [];
+config.opts.tutorials = `./docs/${lang}`;
+config.opts.readme = `${config.opts.tutorials}/README.md`;
+
+module.exports = config;

882a5f4d5ef67bd99ebbdf64855cad70311ec21f
Christopher Hiller <boneskull@boneskull.com>
2022-04-07 12:56:37 -0700

    chore: unify test setup fixture
    Moved the stuff in `test/setup-babel` into `test/setup` and then removed some cruft in the `test:*` scripts of each package referencing `test/setup-babel`

--- test/setup-babel.js
+++
@@ -1,6 +0,0 @@
-// This configures @babel/register to look for a babel config in parent dir(s) of
-// wherever the tests are run.  This is required if running tests via `mocha` in a package dir
-// instead of the monorepo root. This does not affect `gulp-mocha`, since it has its own `.babelrc`.
-// This file is required _in addition to_ the monorepo root's `test/setup.js`.
-
-require('@babel/register')({rootMode: 'upward'});

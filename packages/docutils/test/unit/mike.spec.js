be1812faeef33195082fb3a6f082f2279d512d96
Jonathan Lipps <jlipps@gmail.com>
2022-04-07 14:04:28 -0700

    test(docutils): add a basic test so CI will do something

---
+++ packages/docutils/test/unit/mike.spec.js
@@ -0,0 +1,12 @@
+import { Mike } from '../..';
+import { expect } from 'chai';
+
+describe('Mike', function () {
+
+  it('should create args from params', function () {
+    const m = new Mike({configFile: '1', remote: '2', branch: '3', prefix: '4'});
+    expect(m.getMikeArgs('cmd', ['arg1', 'arg2'])).eql([
+      'cmd', 'arg1', 'arg2', '--config-file', '1', '--remote', '2', '--branch', '3', '--prefix', '4'
+    ]);
+  });
+});

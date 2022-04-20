b34b75a80e4b5d7037a5c93fef1b68fcb6d159ee
Christopher Hiller <boneskull@boneskull.com>
2022-04-20 13:17:21 -0700

    chore: add prettier
    As a pre-commit hook, `.js` files will be run thru `eslint --fix` and rewritten using `prettier`.
    Added `eslint-config-prettier` to disable rules incompatible with `prettier`.

--- packages/eslint-config-appium/index.js
+++ packages/eslint-config-appium/index.js
@@ -6,22 +6,18 @@ module.exports = {
     sourceType: 'module',
     ecmaFeatures: {
       impliedStrict: true,
-      experimentalObjectRestSpread: true
-    }
+      experimentalObjectRestSpread: true,
+    },
   },
   env: {
     node: true,
     mocha: true,
     es6: true,
   },
-  plugins: [
-    'import',
-    'mocha',
-    'promise'
-  ],
+  plugins: ['import', 'mocha', 'promise'],
   globals: {
     chai: true,
-    should: true
+    should: true,
   },
   rules: {
     'no-console': 2,
@@ -29,23 +25,31 @@ module.exports = {
     radix: [2, 'always'],
     'dot-notation': 2,
     eqeqeq: [2, 'smart'],
-    'brace-style': [2, '1tbs', {
-      allowSingleLine: true,
-    }],
-    indent: [2, 2, {
-      VariableDeclarator: {
-        var: 2,
-        let: 2,
-        const: 3,
+    'brace-style': [
+      2,
+      '1tbs',
+      {
+        allowSingleLine: true,
       },
-      ImportDeclaration: 'first',
-      CallExpression: {
-        arguments: 'off',
+    ],
+    indent: [
+      2,
+      2,
+      {
+        VariableDeclarator: {
+          var: 2,
+          let: 2,
+          const: 3,
+        },
+        ImportDeclaration: 'first',
+        CallExpression: {
+          arguments: 'off',
+        },
+        MemberExpression: 'off',
+        ObjectExpression: 'first',
+        SwitchCase: 1,
       },
-      MemberExpression: 'off',
-      ObjectExpression: 'first',
-      SwitchCase: 1,
-    }],
+    ],
     'comma-dangle': 0,
     'no-empty': 0,
     'object-shorthand': 2,
@@ -69,31 +73,44 @@ module.exports = {
     // enforce spacing
     'arrow-spacing': 2,
     'keyword-spacing': 2,
-    'comma-spacing': [2, {
-      before: false,
-      after: true
-    }],
+    'comma-spacing': [
+      2,
+      {
+        before: false,
+        after: true,
+      },
+    ],
     'array-bracket-spacing': 2,
     'no-trailing-spaces': 2,
     'no-whitespace-before-property': 2,
     'space-in-parens': [2, 'never'],
     'space-before-blocks': [2, 'always'],
     'space-before-function-paren': [2, 'always'],
-    'space-unary-ops': [2, {
-      words: true,
-      nonwords: false,
-    }],
+    'space-unary-ops': [
+      2,
+      {
+        words: true,
+        nonwords: false,
+      },
+    ],
     'space-infix-ops': 2,
-    'key-spacing': [2, {
-      mode: 'strict',
-      beforeColon: false,
-      afterColon: true,
-    }],
+    'key-spacing': [
+      2,
+      {
+        mode: 'strict',
+        beforeColon: false,
+        afterColon: true,
+      },
+    ],
     'no-multi-spaces': 2,
-    quotes: [2, 'single', {
-      avoidEscape: true,
-      allowTemplateLiterals: true,
-    }],
+    quotes: [
+      2,
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true,
+      },
+    ],
     'no-buffer-constructor': 1,
     'require-atomic-updates': 0,
     'no-prototype-builtins': 1,
@@ -110,7 +127,5 @@ module.exports = {
       }
     ]
   },
-  extends: [
-    'eslint:recommended',
-  ],
+  extends: ['eslint:recommended', 'prettier'],
 };

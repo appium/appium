4d91f76e4c14adc0231b5e170e1eceb062bcaf10
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2022-04-23 01:42:39 +0900

    docs(appium): add python.py (#16840)
    * add python.py
    
    * update the explanation
    
    * remove duplicated import
    
    * tweak, add en lang to make the find element stable
    
    * update review
    
    * make list

---
+++ packages/appium/sample-code/quickstarts/py/test.py
@@ -0,0 +1,30 @@
+import unittest
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+
+capabilities = dict(
+    platformName='Android',
+    automationName='uiautomator2',
+    deviceName='Android',
+    appPackage='com.android.settings',
+    appActivity='.Settings',
+    language='en',
+    locale='US'
+)
+
+appium_server_url = 'http://localhost:4723'
+
+class TestAppium(unittest.TestCase):
+    def setUp(self) -> None:
+        self.driver = webdriver.Remote(appium_server_url, capabilities)
+
+    def tearDown(self) -> None:
+        if self.driver:
+            self.driver.quit()
+
+    def test_find_battery(self) -> None:
+        el = self.driver.find_element(by=AppiumBy.XPATH, value='//*[@text="Battery"]')
+        el.click()
+
+if __name__ == '__main__':
+    unittest.main()

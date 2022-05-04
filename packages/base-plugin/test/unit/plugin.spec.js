5e96a6d2a7a79e96de03802b05406325765f15ab
Christopher Hiller <boneskull@boneskull.com>
2022-05-04 14:15:28 -0700

    feat(base-plugin): prepare for use by plugins
    This adds the requisite TS setup for base-plugin

--- packages/base-plugin/test/unit/plugin.spec.js
+++ packages/base-plugin/test/unit/plugin.spec.js
@@ -12,9 +12,6 @@ describe('base plugin', function () {
     const p = new BasePlugin('foo');
     should.exist(p.logger);
   });
-  it('should define no server update method', function () {
-    should.not.exist(BasePlugin.updateServer);
-  });
   it('should define a default list of no new methods', function () {
     BasePlugin.newMethodMap.should.eql({});
   });

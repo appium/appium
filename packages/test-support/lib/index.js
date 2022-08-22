743c764d5ea3211367fb2f338ecb5ec99af3ce86
Christopher Hiller <boneskull@boneskull.com>
2022-08-22 15:25:32 -0700

    chore(test-support): remove driver/plugin-specific methods
    BREAKING CHANGE:
    
    This removes plugin/driver specific methods from `@appium/test-support`, and also removes the peer dep on `appium`.
    The methods can now be found in `@appium/plugin-test-support` and `@appium/driver-test-support`.
    
    Closes #17398

--- packages/test-support/lib/index.js
+++ packages/test-support/lib/index.js
@@ -6,15 +6,3 @@ export {stubLog} from './log-utils';
 export {fakeTime} from './time-utils';
 export {withMocks, verifyMocks} from './mock-utils';
 export {withSandbox, verifySandbox} from './sandbox-utils';
-export {pluginE2EHarness} from './plugin-e2e-harness';
-export {driverUnitTestSuite} from './driver-unit-suite';
-export {driverE2ETestSuite, createSessionHelpers} from './driver-e2e-suite';
-
-export * from './helpers';
-
-/**
- * @typedef {import('./driver-e2e-suite').SessionHelpers} SessionHelpers
- */
-/**
- * @typedef {import('./plugin-e2e-harness').E2ESetupOpts} E2ESetupOpts
- */

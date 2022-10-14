6050abea17c9614837e580e171f15e999490abd6
Christopher Hiller <boneskull@boneskull.com>
2022-10-14 13:38:10 -0700

    fix(basedriver): ensure "opts" is defined at time of construction

--- packages/base-driver/test/unit/basedriver/driver.spec.js
+++ packages/base-driver/test/unit/basedriver/driver.spec.js
@@ -1,7 +1,18 @@
 import BaseDriver from '../../../lib';
 import {driverUnitTestSuite} from '@appium/driver-test-support';
 
+const {expect} = chai;
+
 driverUnitTestSuite(BaseDriver, {
   platformName: 'iOS',
   'appium:deviceName': 'Delorean',
+});
+
+describe('BaseDriver', function () {
+  describe('constructor', function () {
+    it('should initialize "opts"', function () {
+      const driver = new BaseDriver();
+      expect(driver.opts).to.exist;
+    });
+  });
 });

5a600c86e463d6105620e260b79ed6193d50c9c9
Christopher Hiller <boneskull@boneskull.com>
2022-11-02 13:44:01 -0700

    chore(): move lint-staged config to config file
    this allows us more granular control over what happens on pre-commit.
    
    Configures schema changes to trigger builds

---
+++ .lintstagedrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  '*.js': ['eslint --fix', 'prettier --write'],
+  '*.ts*': ['prettier --write'],
+  'appium-config-schema.js': () => [
+    'npm run --workspace=./packages/schema build',
+    'git add -A packages/schema/lib/appium-config.schema.json',
+    'npm run --workspace=./packages/types build',
+    'git add -A packages/types/lib/appium-config.ts',
+  ],
+};

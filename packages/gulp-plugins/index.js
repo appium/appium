f27ff3c86aef84c9bdc133af8aa47ba98ae4294b
Christopher Hiller <boneskull@boneskull.com>
2022-12-15 14:04:18 -0800

    chore: remove @appium/gulp-plugins
    Closes #17907

--- packages/gulp-plugins/index.js
+++
@@ -1,4 +0,0 @@
-exports.Transpiler = require('./lib/transpiler');
-exports.spawnWatcher = require('./lib/spawn-watcher');
-exports.boilerplate = require('./lib/boilerplate');
-exports.isVerbose = require('./lib/utils').isVerbose;

3b6b957ba6e5c1c2884485d61d06668c64d8423e
Jonathan Lipps <jlipps@gmail.com>
2022-12-20 14:33:59 -0800

    test(base-driver): remove appium:options test

--- packages/base-driver/test/e2e/basedriver/driver.e2e.spec.js
+++ packages/base-driver/test/e2e/basedriver/driver.e2e.spec.js
@@ -1,12 +1,5 @@
-import {BaseDriver, server, routeConfiguringFunction, PREFIXED_APPIUM_OPTS_CAP} from '../../../lib';
-import {
-  driverE2ETestSuite,
-  getTestPort,
-  TEST_HOST,
-  createSessionHelpers,
-} from '@appium/driver-test-support';
-
-const {expect} = chai;
+import {BaseDriver} from '../../../lib';
+import {driverE2ETestSuite} from '@appium/driver-test-support';
 
 const DEFAULT_CAPS = {
   platformName: 'iOS',
@@ -14,58 +7,6 @@ const DEFAULT_CAPS = {
 };
 
 driverE2ETestSuite(BaseDriver, DEFAULT_CAPS);
-
-describe('BaseDriver', function () {
-  // only run this test on basedriver, not other drivers which also use these tests, since we
-  // don't want them to try and start sessions with these random capabilities that are
-  // necessary to test the appium options logic
-  describe('special appium:options capability', function () {
-    /** @type {SessionHelpers['startSession']} */
-    let startSession;
-    /** @type {SessionHelpers['endSession']} */
-    let endSession;
-    /** @type {import('@appium/types').AppiumServer} */
-    let baseServer;
-    /** @type {BaseDriver} */
-    let d;
-
-    before(async function () {
-      const port = await getTestPort();
-      d = new BaseDriver({port, address: TEST_HOST});
-      baseServer = await server({
-        routeConfiguringFunction: routeConfiguringFunction(d),
-        port,
-        hostname: TEST_HOST,
-      });
-
-      ({startSession, endSession} = createSessionHelpers(port));
-    });
-
-    after(async function () {
-      await baseServer.close();
-    });
-
-    it('should be able to start a session with caps held in appium:options', async function () {
-      const ret = await startSession({
-        capabilities: {
-          alwaysMatch: {
-            platformName: 'iOS',
-            [PREFIXED_APPIUM_OPTS_CAP]: {
-              platformVersion: '11.4',
-              'appium:deviceName': 'iPhone 11',
-            },
-          },
-        },
-      });
-      try {
-        expect(d.opts.platformVersion).to.equal('11.4');
-        expect(d.opts.deviceName).to.equal('iPhone 11');
-      } finally {
-        await endSession(ret.sessionId);
-      }
-    });
-  });
-});
 
 /**
  * @typedef {import('@appium/driver-test-support').SessionHelpers} SessionHelpers

e8a8d95f301053e20be4861d13f1a257279c5647
Christopher Hiller <boneskull@boneskull.com>
2023-02-16 14:48:17 -0800

    feat(driver-test-support): add startStoppableAppium()
    This adds a function `startStoppableAppium()` which starts an Appium server (via its `main()` entry point), but resolves with an object having a `stop() => Promise<boolean>` method. This method force-quits the server, even if there are active connections.

--- packages/driver-test-support/lib/index.js
+++ packages/driver-test-support/lib/index.js
@@ -2,6 +2,9 @@ export {createSessionHelpers, driverE2ETestSuite} from './e2e-suite';
 export * from './unit-suite';
 export * from './helpers';
 
+// eslint-disable-next-line import/no-unresolved
+export * from './stoppable';
+
 /**
  * @typedef {import('@appium/types').DriverClass} DriverClass
  * @typedef {import('@appium/types').BaseNSCapabilities} BaseNSCapabilities

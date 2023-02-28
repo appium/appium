080eb8aa1b356974ce738c1ac6586819d2fd179d
Christopher Hiller <boneskull@boneskull.com>
2023-02-27 16:42:37 -0800

    chore(opencv): increase timeout for unit test this can take awhile when exercising many cores

--- packages/opencv/test/unit/opencv.spec.js
+++ packages/opencv/test/unit/opencv.spec.js
@@ -2,7 +2,7 @@ import {initOpenCv} from '../../lib';
 
 describe('OpenCV', function () {
   it('should initialize opencv library', async function () {
-    this.timeout('5s');
+    this.timeout('10s');
     await initOpenCv();
     const buildInfo = require('opencv-bindings').getBuildInformation();
     buildInfo.should.include('OpenCV');

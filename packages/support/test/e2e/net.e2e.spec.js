0ff735598a8f5658c6b8fc982e2af968b4a15c7c
Jonathan Lipps <jlipps@gmail.com>
2023-04-17 16:15:04 -0700

    test(support): update image path for download test

--- packages/support/test/e2e/net.e2e.spec.js
+++ packages/support/test/e2e/net.e2e.spec.js
@@ -16,7 +16,10 @@ describe('#net', function () {
   describe('downloadFile()', function () {
     it('should download file into the target folder', async function () {
       const dstPath = path.join(tmpRoot, 'download.tmp');
-      await downloadFile('https://appium.io/ico/apple-touch-icon-114x114-precomposed.png', dstPath);
+      await downloadFile(
+        'https://appium.io/docs/en/2.0/assets/images/appium-logo-white.png',
+        dstPath
+      );
       await fs.exists(dstPath).should.eventually.be.true;
     });
   });

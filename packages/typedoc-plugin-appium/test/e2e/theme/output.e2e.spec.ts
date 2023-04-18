ba77843e102ac329aac5c1aa5177b8e54ab48dc6
Christopher Hiller <boneskull@boneskull.com>
2023-04-18 16:35:54 -0700

    chore(typedoc-plugin-appium): add basic snapshot tests

---
+++ packages/typedoc-plugin-appium/test/e2e/theme/output.e2e.spec.ts
@@ -0,0 +1,49 @@
+import path from 'node:path';
+import {tempDir, fs} from 'appium/support';
+import {jestSnapshotPlugin} from 'mocha-chai-jest-snapshot';
+import {convert} from '../../../lib';
+import {reset} from '../helpers';
+
+chai.use(jestSnapshotPlugin());
+
+const {expect} = chai;
+
+describe('@appium/typedoc-plugin-appium', function () {
+  let tmpDir: string;
+  before(async function () {
+    const app = reset({
+      plugin: ['typedoc-plugin-markdown', 'typedoc-plugin-crossmodule-references'],
+    });
+    const promise = convert(app);
+    const project = app.convert();
+    await promise;
+    expect(project).to.exist;
+    tmpDir = await tempDir.openDir();
+    await app.generateDocs(project!, tmpDir);
+  });
+
+  after(async function () {
+    await fs.rimraf(tmpDir);
+  });
+
+  describe('theme', function () {
+    describe('command output', function () {
+      it('should generate expected markdown', async function () {
+        const baseDriverMd = await fs.readFile(
+          path.join(tmpDir, 'commands', 'base-driver.md'),
+          'utf8'
+        );
+        expect(baseDriverMd).toMatchSnapshot();
+      });
+    });
+    describe('execute method output', function () {
+      it('should generate expected markdown', async function () {
+        const fakeDriverMd = await fs.readFile(
+          path.join(tmpDir, 'commands', 'fake-driver.md'),
+          'utf8'
+        );
+        expect(fakeDriverMd).toMatchSnapshot();
+      });
+    });
+  });
+});

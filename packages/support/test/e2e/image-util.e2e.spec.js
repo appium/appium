1da9de4225ee177fa784bfaab4cf5fb91ec0f4f2
Mykola Mokhnach <mokhnach@gmail.com>
2023-05-02 22:02:07 +0200

    refactor(support)!: Drop jimp and pngjs in favour of sharp (#18584)
    BREAKING CHANGE: The following methods have been removed from imageUtil module: getJimpImage, base64ToImage, imageToBase64, cropImage
    BREAKING CHANGE: The following constants have been removed from imageUtil module: MIME_JPEG, MIME_PNG, MIME_BMP

--- packages/support/test/e2e/image-util.e2e.spec.js
+++ packages/support/test/e2e/image-util.e2e.spec.js
@@ -1,12 +1,6 @@
-import {
-  base64ToImage,
-  imageToBase64,
-  cropImage,
-  getJimpImage,
-  MIME_PNG,
-} from '../../lib/image-util';
+import { cropBase64Image } from '../../lib/image-util';
 import path from 'path';
-import _ from 'lodash';
+import sharp from 'sharp';
 import {fs} from '../../lib';
 
 const FIXTURES_ROOT = path.resolve(__dirname, 'fixture', 'images');
@@ -18,63 +12,25 @@ async function getImage(name) {
 
 describe('image-util', function () {
   describe('cropBase64Image', function () {
-    let originalImage = null;
+    let originalImageB64 = null;
 
     before(async function () {
-      const originalImage64 = await getImage('full-image.b64');
-      originalImage = await base64ToImage(originalImage64);
-
-      // verify original image size, to be sure that original image is correct
-      originalImage.width.should.be.equal(640, 'unexpected width');
-      originalImage.height.should.be.equal(1136, 'unexpected height');
+      originalImageB64 = await getImage('full-image.b64');
     });
 
     it('should verify that an image is cropped correctly', async function () {
-      const croppedImage = await cropImage(originalImage, {
+      const croppedImageB64 = await cropBase64Image(originalImageB64, {
         left: 35,
         top: 107,
         width: 323,
         height: 485,
       });
 
+      const croppedImage = sharp(Buffer.from(croppedImageB64, 'base64'));
+      const {width, height} = await croppedImage.metadata();
       // verify cropped image size, it should be less than original image according to crop region
-      croppedImage.width.should.be.equal(323, 'unexpected width');
-      croppedImage.height.should.be.equal(485, 'unexpected height');
-
-      // verify that image cropped, compare base64 representation
-      const croppedImageShouldBe = await getImage('cropped-image.b64');
-      const croppedImage64 = await imageToBase64(croppedImage);
-      croppedImage64.should.be.equal(croppedImageShouldBe);
-    });
-  });
-
-  describe('Jimp helpers', function () {
-    it('should get a jimp object using image buffer', async function () {
-      const base64Image = await getImage('cropped-image.b64');
-      const imageBuffer = Buffer.from(base64Image, 'base64');
-      const jimpImg = await getJimpImage(imageBuffer);
-      jimpImg.hash().should.eql('80000000000');
-      jimpImg.bitmap.height.should.eql(485);
-      jimpImg.bitmap.width.should.eql(323);
-    });
-    it('should get a jimp object using b64 string', async function () {
-      const base64Image = await getImage('cropped-image.b64');
-      const jimpImg = await getJimpImage(base64Image);
-      jimpImg.hash().should.eql('80000000000');
-      jimpImg.bitmap.height.should.eql(485);
-      jimpImg.bitmap.width.should.eql(323);
-    });
-    it('should error with incorrect data type', async function () {
-      await getJimpImage(1234).should.eventually.be.rejectedWith(/string or buffer/);
-    });
-    it('should error with incorrect image data', async function () {
-      await getJimpImage('foo').should.eventually.be.rejectedWith(/Could not find MIME for Buffer/);
-    });
-    it('should get an image buffer via the overridden getBuffer method', async function () {
-      const base64Image = await getImage('cropped-image.b64');
-      const jimpImg = await getJimpImage(base64Image);
-      const buf = await jimpImg.getBuffer(MIME_PNG);
-      _.isBuffer(buf).should.be.true;
+      width.should.be.equal(323, 'unexpected width');
+      height.should.be.equal(485, 'unexpected height');
     });
   });
 });

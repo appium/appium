a0198076d9d4889abce6821afcb7c96190410a6e
Christopher Hiller <boneskull@boneskull.com>
2023-05-19 15:08:26 -0700

    fix(docutils): fix broken executable

--- packages/docutils/bin/appium-docs.js
+++ packages/docutils/bin/appium-docs.js
@@ -2,4 +2,12 @@
 
 'use strict';
 
-module.exports = require('../build/lib/cli');
+const {main} = require('../build/lib/cli');
+const {getLogger} = require('../build/lib/logger');
+
+const log = getLogger('cli');
+
+// eslint-disable-next-line promise/prefer-await-to-then, promise/prefer-await-to-callbacks
+main().catch((err) => {
+  log.error('Caught otherwise-unhandled rejection (this is probably a bug):', err);
+});

ae8421f3fcaa09f4aa5f20e7ae6922c8c3ae10a3
Mykola Mokhnach <mokhnach@gmail.com>
2023-12-14 21:24:32 +0100

    chore(relaxed-caps-plugin): Enable typechecks

--- packages/relaxed-caps-plugin/lib/plugin.js
+++ packages/relaxed-caps-plugin/lib/plugin.js
@@ -1,7 +1,5 @@
-/* eslint-disable no-case-declarations */
-
 import _ from 'lodash';
-import BasePlugin from 'appium/plugin';
+import { BasePlugin } from 'appium/plugin';
 
 const STANDARD_CAPS = [
   'browserName',
@@ -19,7 +17,34 @@ const VENDOR_PREFIX = 'appium';
 const HAS_VENDOR_PREFIX_RE = /^.+:/;
 
 export class RelaxedCapsPlugin extends BasePlugin {
-  transformCaps(caps) {
+
+  /**
+   * @param {any} caps
+   * @returns {boolean}
+   */
+  isW3cCaps(caps) {
+    if (!_.isPlainObject(caps)) {
+      return false;
+    }
+
+    const isFirstMatchValid = () =>
+      _.isArray(caps.firstMatch) &&
+      !_.isEmpty(caps.firstMatch) &&
+      _.every(caps.firstMatch, _.isPlainObject);
+    const isAlwaysMatchValid = () => _.isPlainObject(caps.alwaysMatch);
+    if (_.has(caps, 'firstMatch') && _.has(caps, 'alwaysMatch')) {
+      return isFirstMatchValid() && isAlwaysMatchValid();
+    }
+    if (_.has(caps, 'firstMatch')) {
+      return isFirstMatchValid();
+    }
+    if (_.has(caps, 'alwaysMatch')) {
+      return isAlwaysMatchValid();
+    }
+    return false;
+  }
+
+  addVendorPrefix(caps) {
     const newCaps = {};
 
     // if this doesn't look like a caps object just return it
@@ -27,6 +52,7 @@ export class RelaxedCapsPlugin extends BasePlugin {
       return caps;
     }
 
+    /** @type {string[]} */
     const adjustedKeys = [];
     for (const key of Object.keys(caps)) {
       if (STANDARD_CAPS.includes(key) || HAS_VENDOR_PREFIX_RE.test(key)) {
@@ -47,14 +73,31 @@ export class RelaxedCapsPlugin extends BasePlugin {
     return newCaps;
   }
 
-  async createSession(next, driver, jwpDesCaps, jwpReqCaps, caps) {
-    const newCaps = {};
-    if (_.isArray(caps.firstMatch)) {
-      newCaps.firstMatch = caps.firstMatch.map(this.transformCaps.bind(this));
-    }
-    if (_.isPlainObject(caps.alwaysMatch)) {
-      newCaps.alwaysMatch = this.transformCaps(caps.alwaysMatch);
-    }
-    return await driver.createSession(jwpDesCaps, jwpReqCaps, newCaps);
+  fixCapsIfW3C(caps) {
+    const result = caps;
+    if (result && this.isW3cCaps(result)) {
+      if (_.isArray(result.firstMatch)) {
+        result.firstMatch = caps.firstMatch.map(this.addVendorPrefix.bind(this));
+      }
+      if (_.isPlainObject(caps.alwaysMatch)) {
+        result.alwaysMatch = this.addVendorPrefix(caps.alwaysMatch);
+      }
+    };
+    return result;
+  }
+
+  /**
+   * @param {Function} next
+   * @param {import('appium/driver').BaseDriver} driver
+   * @param {import('@appium/types').W3CDriverCaps} caps1
+   * @param {import('@appium/types').W3CDriverCaps} [caps2]
+   * @param {import('@appium/types').W3CDriverCaps} [caps3]
+   * @param {any[]} restArgs
+   * @returns
+   */
+  async createSession(next, driver, caps1, caps2, caps3, ...restArgs) {
+    const patchedCaps = [caps1, caps2, caps3].map(this.fixCapsIfW3C.bind(this));
+    // @ts-ignore We know what we are doing
+    return await driver.createSession(...[...patchedCaps, ...restArgs]);
   }
 }

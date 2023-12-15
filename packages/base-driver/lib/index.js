61d0bb72b61d4e3d1daa1da91f056419c89b1524
Jonathan Lipps <jlipps@gmail.com>
2023-12-15 13:28:44 -0800

    test(appium): wrap bluebird config in error handling for tests

--- packages/base-driver/lib/index.js
+++ packages/base-driver/lib/index.js
@@ -1,8 +1,13 @@
 import B from 'bluebird';
 
-B.config({
-  cancellation: true,
-});
+try {
+  B.config({
+    cancellation: true,
+  });
+} catch (ign) {
+  // sometimes during testing this somehow gets required twice and results in an error about
+  // cancellation not being able to be enabled after promise has been configured
+}
 
 // BaseDriver exports
 import {BaseDriver} from './basedriver/driver';

88cca05959b0a54aed5d2184a84a493bcc28ca06
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2024-05-16 15:10:59 -0700

    tweak types more

--- packages/appium/lib/cli/setup-command.js
+++ packages/appium/lib/cli/setup-command.js
@@ -42,51 +42,95 @@ export async function runSetupCommand(appiumHome, preConfigArgs, driverConfig, p
   }
 
   if (preConfigArgs.setupCommand === SUBCOMMAND_ALL) {
-    await setupDriverFull(preConfigArgs, driverConfig);
-    await setupPluginFull(preConfigArgs, pluginConfig);
+    await setupDriverFull(driverConfig);
+    await setupPluginFull(pluginConfig);
     return;
   }
 
-  await setupDriverDefault(preConfigArgs, driverConfig);
-  await setupPluginDefault(preConfigArgs, pluginConfig);
+  await setupDriverDefault(driverConfig);
+  await setupPluginDefault(pluginConfig);
 };
 
-async function setupDriverDefault(configArgs, driverConfig) {
+/**
+ * Install drivers listed in DEFAULT_DRIVERS.
+ * @template {import('appium/types').CliExtensionCommand} ExtCmd
+ * @param {import('../extension/extension-config').ExtensionConfig<ExtCmd>} driverConfig
+ */
+async function setupDriverDefault(driverConfig) {
   for (const driverName of DEFAULT_DRIVERS) {
-    await setupDriver(driverName, configArgs, driverConfig);
+    await setupDriver(driverName, driverConfig);
   }
 }
 
-async function setupDriverFull(configArgs, driverConfig) {
+/**
+ * Install all of known drivers listed in KNOWN_DRIVERS.
+ * @template {import('appium/types').CliExtensionCommand} ExtCmd
+ * @param {import('../extension/extension-config').ExtensionConfig<ExtCmd>} driverConfig
+ */
+async function setupDriverFull(driverConfig) {
   for (const driver of _.keys(KNOWN_DRIVERS)) {
-    await setupDriver(driver, configArgs, driverConfig);
+    await setupDriver(driver, driverConfig);
   }
 }
 
-async function setupDriver(driverName, configArgs, driverConfig) {
-  const driverConfigArgs = configArgs;
-  driverConfigArgs.subcommand = 'driver';
-  driverConfigArgs.driverCommand = 'install';
-  driverConfigArgs.driver = driverName;
+/**
+ * Install the given driver name.
+ * @param {string} driverName
+ * @param {import('../extension/extension-config').ExtensionConfig<CliExtensionCommand>} driverConfig
+ */
+async function setupDriver(driverName, driverConfig) {
+  /** @type {Args} */
+  const driverConfigArgs = {
+    'subcommand': 'driver',
+    'driverCommand': 'install',
+    'driver': driverName,
+    'extraArgs': []
+  };
   await runExtensionCommand(driverConfigArgs, driverConfig);
 }
 
-async function setupPluginDefault(configArgs, pluginConfig) {
+/**
+ * Install drivers listed in DEFAULT_PLUGINS.
+ * @template {import('appium/types').CliExtensionCommand} ExtCmd
+ * @param {import('../extension/extension-config').ExtensionConfig<ExtCmd>} pluginConfig
+ */
+async function setupPluginDefault(pluginConfig) {
   for (const pluginName of DEFAULT_PLUGINS) {
-    await setupPlugin(pluginName, configArgs, pluginConfig);
+    await setupPlugin(pluginName, pluginConfig);
   }
 }
 
-async function setupPluginFull(configArgs, driverConfig) {
+/**
+ * Install all of known plugins listed in driverConfig.
+ * @template {import('appium/types').CliExtensionCommand} ExtCmd
+ * @param {import('../extension/extension-config').ExtensionConfig<ExtCmd>} pluginConfig
+ */
+async function setupPluginFull(pluginConfig) {
   for (const pluginName of _.keys(KNOWN_PLUGINS)) {
-    await setupPlugin(pluginName, configArgs, driverConfig);
+    await setupPlugin(pluginName, pluginConfig);
   }
 }
 
-async function setupPlugin(pluginName, configArgs, pluginConfig) {
-  const pluginConfigArgs = configArgs;
-  pluginConfigArgs.subcommand = 'plugin';
-  pluginConfigArgs.pluginCommand = 'install';
-  pluginConfigArgs.plugin = pluginName;
-  await runExtensionCommand(pluginConfigArgs, pluginConfig);
+/**
+ * Install the given plugin name.
+ * @param {string} pluginName
+ * @param {import('../extension/extension-config').ExtensionConfig<CliExtensionCommand>} pluginConfig
+ */
+async function setupPlugin(pluginName, pluginConfig) {
+  const pluginConfigArgs = {
+    'subcommand': 'plugin',
+    'pluginCommand': 'install',
+    'plugin': pluginName,
+    'extraArgs': []
+  };
+  await runExtensionCommand(/** @type {Args} */(pluginConfigArgs), pluginConfig);
 }
+
+/**
+ * @typedef {import('appium/types').CliExtensionCommand} CliExtensionCommand
+ * @typedef {import('appium/types').CliExtensionSubcommand} CliExtensionSubcommand
+
+ */
+/**
+ * @typedef {import('appium/types').Args<CliExtensionCommand, CliExtensionSubcommand>} Args
+ */

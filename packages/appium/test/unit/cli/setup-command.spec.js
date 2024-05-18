56119c59afec5064ee9d023276a4407987aea06c
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2024-05-18 01:20:13 -0700

    add test

---
+++ packages/appium/test/unit/cli/setup-command.spec.js
@@ -0,0 +1,13 @@
+import { system } from '@appium/support';
+import {getPresetDrivers} from '../../../lib/cli/setup-command';
+
+const expect = chai.expect;
+
+describe('SetupCommand', function () {
+  describe('getPresetDrivers', function () {
+    it('for macos environment', function () {
+      const expected = system.isMac() ? ['uiautomator2', 'xcuitest', 'espresso'] : ['uiautomator2', 'espresso']
+      expect(getPresetDrivers('mobile')).eql(expected)
+    });
+  });
+});

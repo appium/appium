f4831558a09c211c0f58bb52a286deffa762235c
Mykola Mokhnach <mokhnach@gmail.com>
2024-06-06 22:46:52 +0200

    feat(appium): Add session signature to all logs (#20202)

--- packages/base-driver/lib/express/middleware.js
+++ packages/base-driver/lib/express/middleware.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import log from './logger';
 import {errors} from '../protocol';
-import {handleIdempotency} from './idempotency';
+export {handleIdempotency} from './idempotency';
 import {pathToRegexp} from 'path-to-regexp';
 
 /**
@@ -66,6 +66,26 @@ export function fixPythonContentType(basePath) {
     }
     next();
   };
+}
+
+/**
+ *
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ * @returns {any}
+ */
+export function handleLogContext(req, res, next) {
+  const contextStorage = global._global_npmlog?.asyncStorage;
+  if (!contextStorage) {
+    return next();
+  }
+
+  const sessionIdMatch = SESSION_ID_PATTERN.exec(req.url);
+  contextStorage.enterWith(
+    sessionIdMatch ? {sessionSignature: sessionIdMatch[1].substring(0, 8)} : {}
+  );
+  return next();
 }
 
 /**
@@ -168,5 +188,3 @@ function patchWithSessionId(req, body) {
   }
   return body;
 }
-
-export { handleIdempotency };
--- packages/base-driver/lib/express/server.js
+++ packages/base-driver/lib/express/server.js
@@ -16,6 +16,7 @@ import {
   handleIdempotency,
   handleUpgrade,
   catch404Handler,
+  handleLogContext,
 } from './middleware';
 import {guineaPig, guineaPigScrollable, guineaPigAppBanner, welcome, STATIC_DIR} from './static';
 import {produceError, produceCrash} from './crash';
@@ -146,6 +147,7 @@ function configureServer({
   basePath = normalizeBasePath(basePath);
 
   app.use(endLogFormatter);
+  app.use(handleLogContext);
 
   // set up static assets
   app.use(favicon(path.resolve(STATIC_DIR, 'favicon.ico')));

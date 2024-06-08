dbce31871a04d79204b58a32ef9e883b033881cb
Mykola Mokhnach <mokhnach@gmail.com>
2024-06-08 15:06:20 +0200

    chore(support): Simplify the logic for the AppiumLogger instance retrieval

--- packages/support/lib/logging.js
+++ packages/support/lib/logging.js
@@ -1,5 +1,3 @@
-// @ts-check
-
 import globalLog from '@appium/logger';
 import _ from 'lodash';
 import moment from 'moment';
@@ -7,17 +5,17 @@ import moment from 'moment';
 /** @type {import('@appium/types').AppiumLoggerLevel[]} */
 export const LEVELS = ['silly', 'verbose', 'debug', 'info', 'http', 'warn', 'error'];
 const MAX_LOG_RECORDS_COUNT = 3000;
-
 const PREFIX_TIMESTAMP_FORMAT = 'HH-mm-ss:SSS';
-
-// mock log object used in testing mode to silence the output
+// export a default logger with no prefix
+export const log = getLogger();
+// mock log object is used in testing mode to silence the output
 const MOCK_LOG = {
   unwrap: () => ({
     loadSecureValuesPreprocessingRules: () => ({
       issues: [],
       rules: [],
     }),
-    level: '',
+    level: 'verbose',
     prefix: '',
     log: _.noop,
   }),
@@ -26,45 +24,10 @@ const MOCK_LOG = {
 
 /**
  *
- * @returns {[import('@appium/logger').Logger, boolean]}
- */
-function _getLogger() {
-  // check if the user set the `_TESTING` or `_FORCE_LOGS` flag
-  const testingMode = process.env._TESTING === '1';
-  const forceLogMode = process.env._FORCE_LOGS === '1';
-
-  // if is possible that there is a logger instance that is already around,
-  // in which case we want t o use that
-  const usingGlobalLog = !!global._global_npmlog;
-  let logger;
-  if (testingMode && !forceLogMode) {
-    // in testing mode, use a mock logger object that we can query
-    logger = MOCK_LOG;
-  } else {
-    // otherwise, either use the global, or a new `npmlog` object
-    logger = global._global_npmlog || globalLog;
-    // The default value is 10000, which causes excessive memory usage
-    logger.maxRecordSize = MAX_LOG_RECORDS_COUNT;
-  }
-  return [logger, usingGlobalLog];
-}
-
-/**
- * @param {AppiumLoggerPrefix?} prefix
- * @param {boolean} logTimestamp whether to include timestamps into log prefixes
- * @returns {string}
- */
-function getActualPrefix(prefix, logTimestamp = false) {
-  const result = (_.isFunction(prefix) ? prefix() : prefix) ?? '';
-  return logTimestamp ? `[${moment().format(PREFIX_TIMESTAMP_FORMAT)}] ${result}` : result;
-}
-
-/**
- *
  * @param {AppiumLoggerPrefix?} prefix
  * @returns {AppiumLogger}
  */
-function getLogger(prefix = null) {
+export function getLogger(prefix = null) {
   const [logger, usingGlobalLog] = _getLogger();
 
   // wrap the logger so that we can catch and modify any logging
@@ -72,8 +35,27 @@ function getLogger(prefix = null) {
     unwrap: () => logger,
     levels: LEVELS,
     prefix,
+    /**
+     * Logs given arguments at the error level and returns
+     * the error object.
+     *
+     * @param  {...any} args
+     * @returns {Error}
+     */
+    errorWithException (/** @type {any[]} */ ...args) {
+      this.error(...args);
+      // make sure we have an `Error` object. Wrap if necessary
+      return _.isError(args[0]) ? args[0] : new Error(args.join('\n'));
+    },
+    /**
+     * @deprecated Use {@link errorWithException} instead
+     * @param {...any} args
+     * @throws {Error}
+     */
+    errorAndThrow (/** @type {any[]} */ ...args) {
+      throw this.errorWithException(args);
+    },
   };
-
   // allow access to the level of the underlying logger
   Object.defineProperty(wrappedLogger, 'level', {
     get() {
@@ -85,28 +67,15 @@ function getLogger(prefix = null) {
     enumerable: true,
     configurable: true,
   });
-
-  const logTimestamp = process.env._LOG_TIMESTAMP === '1';
-
+  const isDebugTimestampLoggingEnabled = process.env._LOG_TIMESTAMP === '1';
   // add all the levels from `npmlog`, and map to the underlying logger
   for (const level of LEVELS) {
-    wrappedLogger[level] = /** @param {...any} args */ function (...args) {
-      const actualPrefix = getActualPrefix(this.prefix, logTimestamp);
+    wrappedLogger[level] = /** @param {any[]} args */ function (...args) {
+      const finalPrefix = getFinalPrefix(this.prefix, isDebugTimestampLoggingEnabled);
       // @ts-ignore This is OK
-      logger[level](actualPrefix, ...args);
+      logger[level](finalPrefix, ...args);
     };
   }
-  wrappedLogger.errorWithException = function (/** @type {any[]} */ ...args) {
-    this.error(...args);
-    // make sure we have an `Error` object. Wrap if necessary
-    return _.isError(args[0]) ? args[0] : new Error(args.join('\n'));
-  };
-  /**
-   * @deprecated Use {@link errorWithException} instead
-   */
-  wrappedLogger.errorAndThrow = function (/** @type {any[]} */ ...args) {
-    throw this.errorWithException(args);
-  };
   if (!usingGlobalLog) {
     // if we're not using a global log specified from some top-level package,
     // set the log level to a default of verbose. Otherwise, let the top-level
@@ -116,10 +85,41 @@ function getLogger(prefix = null) {
   return /** @type {AppiumLogger} */ (wrappedLogger);
 }
 
-// export a default logger with no prefix
-const log = getLogger();
+/**
+ *
+ * @returns {[import('@appium/logger').Logger, boolean]}
+ */
+function _getLogger() {
+  // check if the user set the `_TESTING` or `_FORCE_LOGS` flag
+  const testingMode = process.env._TESTING === '1';
+  const forceLogMode = process.env._FORCE_LOGS === '1';
+  // if is possible that there is a logger instance that is already around,
+  // in which case we want t o use that
+  const useGlobalLog = !!global._global_npmlog;
+  const logger = testingMode && !forceLogMode
+    // in testing mode, use a mock logger object that we can query
+    ? MOCK_LOG
+    // otherwise, either use the global, or a new `npmlog` object
+    : (global._global_npmlog || globalLog);
+  // The default value is 10000, which causes excessive memory usage
+  logger.maxRecordSize = MAX_LOG_RECORDS_COUNT;
+  return [logger, useGlobalLog];
+}
 
-export {log, getLogger};
+/**
+ * @param {AppiumLoggerPrefix?} prefix
+ * @param {boolean} [shouldLogTimestamp=false] whether to include timestamps into log prefixes
+ * @returns {string}
+ */
+function getFinalPrefix(prefix, shouldLogTimestamp = false) {
+  const result = (_.isFunction(prefix) ? prefix() : prefix) ?? '';
+  if (!shouldLogTimestamp) {
+    return result;
+  }
+  const formattedtimestamp = `[${moment().format(PREFIX_TIMESTAMP_FORMAT)}]`;
+  return result ? `${formattedtimestamp} ${result}` : formattedtimestamp;
+}
+
 export default log;
 
 /**

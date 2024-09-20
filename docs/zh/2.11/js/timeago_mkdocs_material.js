e72d34a12574670afd5cff878b0d15859dfd4107
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2024-09-19 22:59:07 -0800

    docs(appium): auto-build docs for appium@2.11, language zh

--- docs/zh/2.11/js/timeago_mkdocs_material.js
+++ docs/zh/2.11/js/timeago_mkdocs_material.js
@@ -1,18 +1,33 @@
-// Script to ensure timeago keeps working when 
-// used with mkdocs-material's instant loading feature 
+// Script to ensure timeago keeps working when
+// used with mkdocs-material's instant loading feature
+
+function getLocale(element) {
+    var raw_locale = element.getAttribute('locale');
+    var locale = {
+        bn: 'bn_IN',
+        en: 'en_US',
+        hi: 'hi_IN',
+        id: 'id_ID',
+        nb: 'nb_NO',
+        nn: 'nn_NO',
+        pt: 'pt_BR',
+        zh: 'zh_CN'
+    }[raw_locale];
+    return locale ? locale : raw_locale;
+}
 
 if (typeof document$ !== "undefined") {
     document$.subscribe(function() {
         var nodes = document.querySelectorAll('.timeago');
         if (nodes.length > 0) {
-          var locale = nodes[0].getAttribute('locale');
-          timeago.render(nodes, locale);
+            var locale = getLocale(nodes[0]);
+            timeago.render(nodes, locale);
         }
     })
 } else {
     var nodes = document.querySelectorAll('.timeago');
     if (nodes.length > 0) {
-      var locale = nodes[0].getAttribute('locale');
-      timeago.render(nodes, locale);
+        var locale = getLocale(nodes[0]);
+        timeago.render(nodes, locale);
     }
 }

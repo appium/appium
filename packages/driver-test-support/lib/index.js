9aa9807b904c80b41b84c6eef066391eb475df5b
Mykola Mokhnach <mokhnach@gmail.com>
2024-11-29 21:30:34 +0100

    chore(driver-test-support): Remove stoppable utils (#20791)

--- packages/driver-test-support/lib/index.js
+++ packages/driver-test-support/lib/index.js
@@ -2,9 +2,6 @@ export {createSessionHelpers, driverE2ETestSuite} from './e2e-suite';
 export * from './unit-suite';
 export * from './helpers';
 
-// eslint-disable-next-line import/no-unresolved
-export * from './stoppable';
-
 /**
  * @typedef {import('@appium/types').DriverClass} DriverClass
  * @typedef {import('@appium/types').BaseNSCapabilities} BaseNSCapabilities

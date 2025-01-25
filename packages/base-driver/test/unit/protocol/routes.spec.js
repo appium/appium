7a60c08d9871198ffefc6c4676b78c67ea82a8ea
Mykola Mokhnach <mokhnach@gmail.com>
2025-01-25 16:10:28 +0100

    update sha

--- packages/base-driver/test/unit/protocol/routes.spec.js
+++ packages/base-driver/test/unit/protocol/routes.spec.js
@@ -41,7 +41,7 @@ describe('Protocol', function () {
       }
       let hash = shasum.digest('hex').substring(0, 8);
       // Modify the hash whenever the protocol has intentionally been modified.
-      hash.should.equal('d4fdd7fe');
+      hash.should.equal('392df577');
     });
   });
 

f8b57999e5a4a13a089cff12954de5d99c8a35fc
Mykola Mokhnach <mokhnach@gmail.com>
2025-02-06 08:56:13 +0100

    feat(base-driver): Print the closest match if the given script did not match (#20956)

--- packages/base-driver/lib/basedriver/commands/execute.ts
+++ packages/base-driver/lib/basedriver/commands/execute.ts
@@ -10,6 +10,7 @@ import {
 } from '@appium/types';
 import {mixin} from './mixin';
 import {BaseDriver} from '../driver';
+import {distance} from 'fastest-levenshtein';
 
 declare module '../driver' {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -26,9 +27,33 @@ const ExecuteCommands: IExecuteCommands = {
     const commandMetadata = {...Driver.executeMethodMap?.[script]};
     if (!commandMetadata.command) {
       const availableScripts = _.keys(Driver.executeMethodMap);
+      if (_.isEmpty(availableScripts)) {
+        throw new errors.UnsupportedOperationError(
+          `Unsupported execute method '${script}'. ` +
+          `Make sure the installed ${Driver.name} is up-to-date. ` +
+          `The current driver version does not define any execute methods.`
+        );
+      }
+      const matchesMap: StringRecord<string[]> = availableScripts
+        .map((name) => [distance(script, name), name])
+        .reduce((acc, [key, value]) => {
+          if (key in acc) {
+            acc[key].push(value);
+          } else {
+            acc[key] = [value];
+          }
+          return acc;
+        }, {});
+      const sortedMatches = _.flatten(
+        _.keys(matchesMap)
+          .sort((a, b) => parseInt(a, 10) - parseInt(b, 10))
+          .map((x) => matchesMap[x])
+      );
       throw new errors.UnsupportedOperationError(
-        `Unsupported execute method '${script}'. Available methods ` +
-          `are: ${availableScripts.join(', ')}`
+        `Unsupported execute method '${script}', did you mean '${sortedMatches[0]}'? ` +
+        `Make sure the installed ${Driver.name} is up-to-date. ` +
+        `Execute methods available in the current driver version are: ` +
+        sortedMatches.join(', ')
       );
     }
     const args = validateExecuteMethodParams(protoArgs, commandMetadata.params);

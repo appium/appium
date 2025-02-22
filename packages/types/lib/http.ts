ae5c4b744b229196a8a6327004e798505e2bb20c
Mykola Mokhnach <mokhnach@gmail.com>
2025-02-22 20:00:09 +0100

    chore(base-driver): Add types to proxy options

--- packages/types/lib/http.ts
+++ packages/types/lib/http.ts
@@ -1,3 +1,5 @@
+import type { AppiumLogger } from './logger';
+
 /**
  * An object of HTTP headers.
  */
@@ -34,6 +36,61 @@ export interface ProxyResponse<T = any> {
   statusCode: number;
   headers: HTTPHeaders;
   body: HTTPBody<T>;
+}
+
+export interface ProxyOptions {
+  /**
+   * Downstream URL scheme
+   *
+   * @default 'http'
+   */
+  scheme?: string;
+  /**
+   * Downstream server hostname
+   *
+   * @default 'localhost''
+   */
+  server?: string;
+  /**
+   * Downstream server port number
+   *
+   * @default 4444
+   */
+  port?: number;
+  /**
+   * Downstream server pathname prefix
+   *
+   * @default ''
+   */
+  base?: string;
+  /**
+   * Upstream server pathname prefix
+   *
+   * @default ''
+   */
+  reqBasePath?: string;
+  /**
+   * Initial downstream session identifier value
+   *
+   * @default null
+   */
+  sessionId?: string | null;
+  /**
+   * Downstream server timeout in milliseconds
+   *
+   * @default 240000
+   */
+  timeout?: number;
+  /**
+   * Proxy logger instance. If unset then a default logger is used
+   */
+  log?: AppiumLogger;
+  /**
+   * Whether to apply HTTP Keep-Alive to the downstream server
+   *
+   * @default true
+   */
+  keepAlive?: boolean;
 }
 
 export type HTTPBody<T = any> = T;

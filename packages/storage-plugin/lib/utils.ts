df8588f57704df6ca028f6bc378ea86f9b355c23
Mykola Mokhnach <mokhnach@gmail.com>
2025-03-07 22:59:23 +0100

    moar

--- packages/storage-plugin/lib/utils.ts
+++ packages/storage-plugin/lib/utils.ts
@@ -3,7 +3,7 @@ import { errors } from 'appium/driver';
 
 
 export function toW3cResponseError(err: any): [number, Record<string, any>] {
-  const protocolError = _.has('w3cStatus', err) ? err : new errors.UnknownError(err);
+  const protocolError = _.has(err, 'w3cStatus') ? err : new errors.UnknownError(err);
   return [protocolError.w3cStatus, {
     value: {
       error: protocolError.error,

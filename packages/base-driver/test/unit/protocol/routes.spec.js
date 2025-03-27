2cb3a31252cf0190180acf0c353112880d586b31
Edgars Eglītis <37242620+eglitise@users.noreply.github.com>
2025-03-27 21:23:27 +0200

    fix: adjust parameter for W3C element send keys endpoint (#21149)
    * fix: adjust parameter for W3C element send keys endpoint
    
    * fix: update hash

--- packages/base-driver/test/unit/protocol/routes.spec.js
+++ packages/base-driver/test/unit/protocol/routes.spec.js
@@ -41,7 +41,7 @@ describe('Protocol', function () {
       }
       let hash = shasum.digest('hex').substring(0, 8);
       // Modify the hash whenever the protocol has intentionally been modified.
-      hash.should.equal('dadd642a');
+      hash.should.equal('61e4a9e2');
     });
   });
 

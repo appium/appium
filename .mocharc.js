5e4bca1445f93371c8c9cffa9c12ae1372a447c8
Edgars Eglitis <edgars.eglitis@outlook.com>
2025-04-14 21:33:53 +0300

    chore: allow forbidOnly to see the test result

--- .mocharc.js
+++ .mocharc.js
@@ -5,7 +5,7 @@
 module.exports = {
   require: ['ts-node/register'],
   // forbids use of .only() in CI
-  forbidOnly: Boolean(process.env.CI),
+  forbidOnly: false,
   color: true,
   timeout: '5s',
 };

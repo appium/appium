97c62676fc1f3be26a23970a10e9904db4b29716
Mykola Mokhnach <mokhnach@gmail.com>
2025-04-16 19:43:57 +0200

    chore(base-driver): Mark /session/:sessionId/network_connection deprecated (#21209)

--- packages/base-driver/lib/protocol/routes.js
+++ packages/base-driver/lib/protocol/routes.js
@@ -345,10 +345,11 @@ export const METHOD_MAP = /** @type {const} */ ({
     GET: {command: 'getContexts'},
   },
   '/session/:sessionId/network_connection': {
-    GET: {command: 'getNetworkConnection'},
+    GET: {command: 'getNetworkConnection', deprecated: true},
     POST: {
       command: 'setNetworkConnection',
       payloadParams: {unwrap: 'parameters', required: ['type']},
+      deprecated: true,
     },
   },
   '/session/:sessionId/receive_async_response': {

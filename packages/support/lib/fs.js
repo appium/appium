596fa29e10e23788a04705e821bb174f1220e36e
Mykola Mokhnach <mokhnach@gmail.com>
2025-08-02 09:38:33 +0200

    feat(support): Drop the usage of the obsolete "mv" package

--- packages/support/lib/fs.js
+++ packages/support/lib/fs.js
@@ -16,7 +16,6 @@ import {
 import { glob } from 'glob';
 import klaw from 'klaw';
 import _ from 'lodash';
-import mv from 'mv';
 import ncp from 'ncp';
 import path from 'path';
 import pkgDir from 'pkg-dir';
@@ -144,9 +143,51 @@ const fs = {
   },
 
   /**
-   * Move a file
+   * Move a file or a folder
+   *
+   * @param {string} from Source file/folder
+   * @param {string} to Destination file/folder
+   * @param {mv} [opts] Move options
+   * @returns {Promise<void>}
    */
-  mv: /** @type {(from: string, to: string, opts?: mv.Options) => B<void>} */ (B.promisify(mv)),
+  async mv(from, to, opts) {
+    const ensureDestination = async (/** @type {import('fs').PathLike} */ p) => {
+      if (opts?.mkdirp && !(await this.exists(p))) {
+        await fsPromises.mkdir(p, { recursive: true });
+      }
+    };
+
+    /** @type {import('fs').Stats} */
+    let fromStat;
+    try {
+      fromStat = await fsPromises.stat(from);
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        throw new Error(`The source path '${from}' does not exist or is not accessible`);
+      }
+      throw err;
+    }
+    if (fromStat.isFile()) {
+      await ensureDestination(path.dirname(to));
+      await fsPromises.rename(from, to);
+    } else if (fromStat.isDirectory()) {
+      await ensureDestination(to);
+      const items = await fsPromises.readdir(from, { withFileTypes: true });
+      for (const item of items) {
+        const srcPath = path.join(from, item.name);
+        const destPath = path.join(to, item.name);
+        if (item.isDirectory()) {
+          await this.mv(srcPath, destPath, opts);
+        } else if (item.isFile()) {
+          await fsPromises.rename(srcPath, destPath);
+        }
+      }
+    } else {
+      return;
+    }
+
+    await this.rimraf(from);
+  },
 
   /**
    * Find path to an executable in system `PATH`
@@ -404,8 +445,14 @@ export default fs;
 
 /**
  * @typedef {import('glob')} glob
- * @typedef {import('mv')} mv
  * @typedef {import('fs').PathLike} PathLike
+ */
+
+/**
+ * @typedef {Object} mv
+ * @property {boolean} [mkdirp=false] Whether to automatically create the destination folder structure
+ * @deprecated @property {boolean} [clobber=false] Legacy deprecated property, not used anymore
+ * @deprecated @property {number} [limit=16] Legacy deprecated property, not used anymore
  */
 
 /**

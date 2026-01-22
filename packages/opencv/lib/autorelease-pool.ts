4d48e157576c17b249bdf941c8c80eb6d2c22e42
Mykola Mokhnach <mokhnach@gmail.com>
2026-01-22 19:17:16 +0100

    feat(opencv): Migrate to typescript

--- packages/opencv/lib/autorelease-pool.ts
+++ packages/opencv/lib/autorelease-pool.ts
@@ -1,30 +1,29 @@
+interface Deletable {
+  delete(): void;
+}
+
 export class OpenCvAutoreleasePool {
-  /**
-   * @type {Set}
-   */
-  _items;
+  private readonly _items: Set<Deletable>;
 
   constructor() {
     this._items = new Set();
   }
 
-  /**
-   * @template T
-   * @param {...T} items
-   * @return {T[]|T} the same items
-   */
-  add(...items) {
+  add<T extends Deletable>(item: T): T;
+  add<T extends Deletable>(...items: T[]): T | T[] {
     for (const item of items) {
       this._items.add(item);
     }
     return items.length === 1 ? items[0] : items;
   }
 
-  drain() {
+  drain(): void {
     for (const item of this._items) {
       try {
         item.delete();
-      } catch {}
+      } catch {
+        // Ignore errors during cleanup
+      }
     }
     this._items.clear();
   }

6e8c19c83e2d28adbaaac7a476f16d0f30c57149
Mykola Mokhnach <mokhnach@gmail.com>
2026-02-11 07:11:59 +0100

    test(logger): Migrate tests to typescript (#21948)

--- packages/logger/test/unit/display-specs.ts
+++ packages/logger/test/unit/display-specs.ts
@@ -1,33 +1,33 @@
-import { Log } from '../../lib/log';
-import { waitForCondition } from 'asyncbox';
+import {Log} from '../../lib/log';
+import {waitForCondition} from 'asyncbox';
 
 describe('display', function () {
-  let log;
+  let log: Log;
 
   describe('explicitly set new log level display to empty string', function () {
-    let actual;
+    let actual: string;
 
     beforeEach(function () {
       actual = '';
       log = new Log();
-      log.write = (msg) => {
+      (log as any).write = (msg: string) => {
         actual += msg;
       };
     });
 
     it('explicitly set new log level display to empty string', async function () {
       log.addLevel('explicitNoLevelDisplayed', 20000, {}, '');
-      log.explicitNoLevelDisplayed('1', '2');
+      (log as any).explicitNoLevelDisplayed('1', '2');
       await waitForCondition(() => actual.trim() === '1 2', {waitMs: 1000, intervalMs: 50});
 
       actual = '';
-      log.explicitNoLevelDisplayed('', '1');
+      (log as any).explicitNoLevelDisplayed('', '1');
       await waitForCondition(() => actual.trim() === '1', {waitMs: 1000, intervalMs: 50});
     });
 
     it('explicitly set new log level display to 0', async function () {
-      log.addLevel('explicitNoLevelDisplayed', 20000, {}, 0);
-      log.explicitNoLevelDisplayed('', '1');
+      log.addLevel('explicitNoLevelDisplayed', 20000, {}, '0');
+      (log as any).explicitNoLevelDisplayed('', '1');
       await waitForCondition(() => actual.trim() === '0 1', {waitMs: 1000, intervalMs: 50});
     });
   });

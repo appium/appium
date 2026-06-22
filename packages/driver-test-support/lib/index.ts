2b2f70af59e29fa2a244504111787c46d086b8d0
Mykola Mokhnach <mokhnach@gmail.com>
2026-06-22 17:25:58 +0200

    ditch the module

--- packages/driver-test-support/lib/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @deprecated This package has been deprecated and will be removed.
- * Copy over tests from this package to your own driver.
- */
-export {createSessionHelpers, driverE2ETestSuite} from './e2e-suite';
-export * from './unit-suite';
-export * from './helpers';
-export type {NewSessionData, NewSessionResponse, SessionHelpers} from './types';
